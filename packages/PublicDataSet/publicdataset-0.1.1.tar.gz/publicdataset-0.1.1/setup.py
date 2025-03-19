521c8d356fd64221ec2ea2783976cbfa0e73707e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:26:26 +0000

    Add package PublicDataSet/0.1.1/publicdataset-0.1.1.tar.gz

---
+++ packages/PublicDataSet/publicdataset-0.1.1.tar.gz/publicdataset-0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="PublicDataSet",
+    version="0.1.1",
+    packages=find_packages(),
+    install_requires=[
+    ],
+    author="YanXinle",
+    author_email="1020121123@qq.com",
+)
