a0482a8101d3b9b2b188c19b228c8301d4d66192
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 23:04:17 +0000

    Add package check-spyder/0.1/check_spyder-0.1.tar.gz

---
+++ packages/check-spyder/check_spyder-0.1.tar.gz/check_spyder-0.1/check_spyder/check_spyder.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri March 14 18:28:36 2025
+
+@author: Javier S. Zurdo
+@module: Check if Spyder is enabled or not.
+"""
+
+#%% ------------ Libraries -----------------
+# ------------------------------------------
+import os
+
+#%% ------------ Classes -------------------
+# ------------------------------------------
+class Check_Spyder:
+    def check(self, test_type:str='environment_variables'):
+        is_spyder = False
+        # Checking environment variables
+        if test_type == 'environment_variables':
+            is_spyder = any('SPYDER' in name for name in os.environ)
+        else:  # Using spyder kernel values
+            try: # fails when not ipython
+                ip_name = get_ipython().__class__.__name__
+                is_spyder = ip_name == 'SpyderShell'
+            except:
+                is_spyder = False
+        return is_spyder
+
+    def __init__(self, test_type:str='environment_variables'):
+        self.test_type = test_type
+        self.is_spyder = self.check()
