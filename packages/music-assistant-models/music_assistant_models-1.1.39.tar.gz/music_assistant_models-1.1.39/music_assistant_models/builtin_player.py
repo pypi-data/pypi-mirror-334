d579fbaace022cba88af51acf334aae377bb77cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 23:04:04 +0000

    Add package music-assistant-models/1.1.39/music_assistant_models-1.1.39.tar.gz

---
+++ packages/music-assistant-models/music_assistant_models-1.1.39.tar.gz/music_assistant_models-1.1.39/music_assistant_models/builtin_player.py
@@ -0,0 +1,27 @@
+"""Models for the builtin player provider."""
+
+from dataclasses import dataclass
+
+from mashumaro import DataClassDictMixin
+
+from .enums import BuiltinPlayerEventType
+
+
+@dataclass
+class BuiltinPlayerEvent(DataClassDictMixin):
+    """Model for events sent to the builtin (web) player."""
+
+    type: BuiltinPlayerEventType
+    volume: int | None = None  # set if action is SET_VOLUME
+    media_url: str | None = None  # set if action is PLAY_MEDIA
+
+
+@dataclass
+class BuiltinPlayerState(DataClassDictMixin):
+    """Model for state updates from the builtin (web) player."""
+
+    playing: bool
+    paused: bool
+    position: int
+    volume: int
+    muted: bool
