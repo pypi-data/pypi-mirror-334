f219c365ce1991f7596a000d2286b0e9460947d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 23:04:36 +0000

    Add package git-draft/0.28.1/git_draft-0.28.1.tar.gz

---
+++ packages/git-draft/git_draft-0.28.1.tar.gz/git_draft-0.28.1/src/git_draft/__init__.py
@@ -0,0 +1,13 @@
+import logging
+
+from .bots import Action, Bot, Goal, Toolbox
+
+
+__all__ = [
+    "Action",
+    "Bot",
+    "Goal",
+    "Toolbox",
+]
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
