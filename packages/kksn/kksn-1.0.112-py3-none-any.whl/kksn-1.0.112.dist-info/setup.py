ed2043219202c542d2f14e62bf374802df64e3d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 21:04:06 +0000

    Add package kksn/1.0.112/kksn-1.0.112-py3-none-any.whl

---
+++ packages/kksn/kksn-1.0.112-py3-none-any.whl/kksn-1.0.112.dist-info/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='kksn',
+    version='1.0.112',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        'kksn': ['*.exe']  # 指定要包含的文件模式
+    },
+    description='kksn序列号生成器',
+    # long_description=open('README.md').read(),
+    # python3，readme文件中文报错
+    long_description=open('README.md', 'r', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    author='Kuizi',
+    author_email='123751307@qq.com',
+    license='MIT',
+    license_files="LICEN[CS]E*",
+    install_requires=[
+        'wmi',
+        'ntplib'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Microsoft :: Windows',
+        'Intended Audience :: Developers',
+        'Topic :: Utilities'
+    ]
+)
