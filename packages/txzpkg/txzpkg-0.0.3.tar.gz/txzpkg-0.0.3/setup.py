df810563944623e19f7ad0062a2bcc2dba058986
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 21:03:58 +0000

    Add package txzpkg/0.0.3/txzpkg-0.0.3.tar.gz

---
+++ packages/txzpkg/txzpkg-0.0.3.tar.gz/txzpkg-0.0.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='txzpkg',
+    version='0.0.3',
+    description='download pkg as txz, install as txz',
+    author='du7ec',
+    author_email='dutec6834@gmail.com',
+    url='https://github.com/FarAway6834/txzpkg',
+    packages=find_packages(exclude=[]),
+    keywords=['txz', 'pkg', 'txzpkg'],
+    python_requires='>=3.4',
+    package_data={},
+    classifiers=[
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
+    ],
+)
