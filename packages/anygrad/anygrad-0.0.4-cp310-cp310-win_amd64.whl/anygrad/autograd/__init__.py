7fb33f604a52655d76020b00fee99de7cd5e3d47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 23:04:04 +0000

    Add package anygrad/0.0.4/anygrad-0.0.4-cp310-cp310-win_amd64.whl

---
+++ packages/anygrad/anygrad-0.0.4-cp310-cp310-win_amd64.whl/anygrad/autograd/__init__.py
@@ -0,0 +1,5 @@
+from anygrad.autograd.autograd import GradMode, no_grad
+from anygrad.autograd.helper import BuildGraph
+from anygrad.autograd.helper import GradientCal
+
+__all__ = ["GradMode", "GradientCal", "BuildGraph", "no_grad"]
