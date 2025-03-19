7469b674f3b80a37855f4faf168789782f4183e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 23:04:32 +0000

    Add package sik-stochastic-tests/0.0.1/sik_stochastic_tests-0.0.1.tar.gz

---
+++ packages/sik-stochastic-tests/sik_stochastic_tests-0.0.1.tar.gz/sik_stochastic_tests-0.0.1/src/sik_stochastic_tests/__init__.py
@@ -0,0 +1,8 @@
+"""Public facing functions and classes for models."""
+
+def my_function() -> str:  # noqa: D103
+    return 'hello'
+
+__all__ = [
+    'my_function',
+]
---
+++ packages/sik-stochastic-tests/sik_stochastic_tests-0.0.1.tar.gz/sik_stochastic_tests-0.0.1/tests/test_the.py
@@ -0,0 +1,5 @@
+"""Tests for the sik_stochastic_tests module."""
+from sik_stochastic_tests import my_function
+
+def test_this_____function():
+    assert my_function() == 'hello'
