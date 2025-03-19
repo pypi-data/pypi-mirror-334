71f590f3aedf750a2663a982dea1a7adf2275d21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 21:04:30 +0000

    Add package conventionalrp/0.2.3/conventionalrp-0.2.3-cp39-abi3-win_amd64.whl

---
+++ packages/conventionalrp/conventionalrp-0.2.3-cp39-abi3-win_amd64.whl/conventionalrp/renderers/__init__.py
@@ -0,0 +1,4 @@
+# FILE: /trpg-log-processor/trpg-log-processor/src/conventionalrp/renderers/__init__.py
+"""
+This file initializes the renderers module.
+"""
---
+++ packages/conventionalrp/conventionalrp-0.2.3-cp39-abi3-win_amd64.whl/conventionalrp/utils/__init__.py
@@ -0,0 +1,2 @@
+# FILE: /trpg-log-processor/trpg-log-processor/src/conventionalrp/utils/__init__.py
+"""This file initializes the utils module."""
