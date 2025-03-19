2dee5cc1dea5f1c7a390929e8d8e0cba6a696cef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:26:34 +0000

    Add package upathtools/0.4.4/upathtools-0.4.4-py3-none-any.whl

---
+++ packages/upathtools/upathtools-0.4.4-py3-none-any.whl/upathtools/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "0.4.4"
+
+from upathtools.async_ops import read_path, read_folder, list_files, read_folder_as_text
+
+__all__ = ["list_files", "read_folder", "read_folder_as_text", "read_path"]
