2313d1f8e0961813fcc525195a0f90f021674938
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 22:04:53 +0000

    Add package fbgemm-gpu-nightly-cpu/2025.3.15/fbgemm_gpu_nightly_cpu-2025.3.15-cp312-cp312-manylinux_2_28_aarch64.whl

---
+++ packages/fbgemm-gpu-nightly-cpu/fbgemm_gpu_nightly_cpu-2025.3.15-cp312-cp312-manylinux_2_28_aarch64.whl/fbgemm_gpu/tbe/bench/__init__.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+# pyre-unsafe
+
+from .bench_config import (  # noqa F401
+    TBEBenchmarkingConfig,
+    TBEBenchmarkingConfigLoader,
+)
+from .bench_runs import (  # noqa F401
+    bench_warmup,
+    benchmark_cpu_requests,
+    benchmark_pipelined_requests,
+    benchmark_requests,
+    benchmark_requests_refer,
+    benchmark_vbe,
+)
+from .embedding_ops_common_config import EmbeddingOpsCommonConfigLoader  # noqa F401
+from .eval_compression import (  # noqa F401
+    benchmark_eval_compression,
+    EvalCompressionBenchmarkOutput,
+)
+from .reporter import BenchmarkReporter  # noqa F401
+from .tbe_data_config import TBEDataConfig  # noqa F401
+from .tbe_data_config_loader import TBEDataConfigLoader  # noqa F401
+from .tbe_data_config_param_models import (  # noqa F401
+    BatchParams,
+    IndicesParams,
+    PoolingParams,
+)
+from .utils import fill_random_scale_bias  # noqa F401
