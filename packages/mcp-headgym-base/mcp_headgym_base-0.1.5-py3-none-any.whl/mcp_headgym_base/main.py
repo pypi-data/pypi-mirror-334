ae4b2283e6badc20977d96ea486d7395bb767aa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:41:13 +0000

    Add package mcp-headgym-base/0.1.5/mcp_headgym_base-0.1.5-py3-none-any.whl

---
+++ packages/mcp-headgym-base/mcp_headgym_base-0.1.5-py3-none-any.whl/mcp_headgym_base/main.py
@@ -0,0 +1,29 @@
+"""
+FastMCP Screenshot Example
+
+Give Claude a tool to capture and view screenshots.
+"""
+
+import io
+
+from mcp.server.fastmcp import FastMCP
+
+
+# Create server
+mcp = FastMCP("HeadGym")
+
+
+
+@mcp.tool()
+async def list_experts() -> str:
+    """Lists all experts in the HeadGym directory
+
+    """
+    return "Expert 1: @murat Dr. John Doe\nExpert 2: @nikola Dr. Jane Doe"
+
+def main():
+    # Initialize and run the server
+    mcp.run(transport='stdio')
+
+if __name__ == "__main__":
+    main()
