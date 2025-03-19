955df2cf5be11ab8ce30f26354532956952e2c41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 23:04:16 +0000

    Add package hp1p2/0.3.0/hp1p2-0.3.0.tar.gz

---
+++ packages/hp1p2/hp1p2-0.3.0.tar.gz/hp1p2-0.3.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hp1p2",
+    version="0.3.0",
+    description="A very hp package",
+    author="compinfun",
+    author_email="compinfun@gmail.com",
+    packages=find_packages(),
+    package_data={
+        "hp1p2": ["data/*.mp3"],
+    },
+    include_package_data=True,
+)
