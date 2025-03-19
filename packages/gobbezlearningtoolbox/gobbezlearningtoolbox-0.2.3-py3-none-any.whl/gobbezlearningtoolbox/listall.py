6982665635a43228fd67508b1ee52d4973f05208
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 23:05:54 +0000

    Add package gobbezlearningtoolbox/0.2.3/gobbezlearningtoolbox-0.2.3-py3-none-any.whl

---
+++ packages/gobbezlearningtoolbox/gobbezlearningtoolbox-0.2.3-py3-none-any.whl/gobbezlearningtoolbox/listall.py
@@ -0,0 +1,11 @@
+from .print_fonts import PrintFonts
+
+class ListAll():
+    def __init__(self):
+        pf = PrintFonts()
+        text = """
+        # Here you can find all the resources of the module:
+        datascience = Data Science with Pandas, Plots with Matplotlib and Seaborn and Machine-Learning with Scikit-learn
+        deeplearning = Deep Learning models with Tensorflow Keras
+        """
+        pf.format(text)
