ecdc64859c29a5b7e427502491e196ad99e15d8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 23:04:22 +0000

    Add package rich-string-builder/0.0.1/rich_string_builder-0.0.1.tar.gz

---
+++ packages/rich-string-builder/rich_string_builder-0.0.1.tar.gz/rich_string_builder-0.0.1/README.md
@@ -0,0 +1,82 @@
+# rich-string-builder
+Text preprocessor for building strings for the [rich library](https://github.com/Textualize/rich)
+
+
+## Background
+This came about because I was annoyed at the way we needed to insert text into rich to make it reflect our intended
+stylistic choices. Take the following example:
+
+```python
+console.print(
+    f"Subnet: [{COLOR_PALETTE['GENERAL']['SUBHEADING']}]{netuid_}[/{COLOR_PALETTE['GENERAL']['SUBHEADING']}] "
+    f"Stake:\n"
+    f"  [blue]{current_stake}[/blue] "
+    f":arrow_right: "[{COLOR_PALETTE['STAKE']['STAKE_AMOUNT']}]{new_stake}\n"
+)
+```
+
+With this library, it's a bit more readable as:
+```python
+console.print(
+    rs.str(
+        "Subnet: ", rs.GENERAL.SUBHEADING(netuid_),
+        " Stake:\n  ",
+        rs.c.blue(current_stake),
+        " :arrow_right: ", rs.STAKE.STAKE_AMOUNT(new_stake, "\n")
+    )
+)
+```
+
+The ideas are largely derived from Tailwind and other utility-style frameworks/libraries.
+
+## Current State
+Currently this is just proof of concept. I will likely add to this over time.
+
+
+## Examples
+See `examples` directory for examples of using this.
+
+One of the fundamental aspects of this library is the ability to preload your own custom styles from a dictionary, like so:
+
+```python
+from rich.console import Console
+from rich_string_builder.string_builder import RichStyles
+
+console = Console()
+
+my_styles = {
+    "alert": "blink yellow",
+    "headers": {
+        "main": "bold",
+        "sub": "#4196D6"
+    }
+}
+rs = RichStyles(my_styles)
+console.print(rs.custom.alert("WARNING!"))  # '[blink yellow]WARNING![/blink yellow]'
+```
+
+Though you are able to combine these styles ad-hoc:
+
+```python
+bright_blue = rs.Styles.bright + rs.Colors.blue
+console.print(bright_blue("Coldkey"))  # '[bright blue]Coldkey[/bright blue]'
+```
+
+Say you have a complicated custom style, and you wish to only remove an element or two and reuse it:
+```python
+my_styles = {
+    "foo": "blink bright yellow"
+}
+rs = RichStyles(my_styles)
+console.print((rs.custom.foo - "blink")("Hello!"))  # '[bright yellow]Hello![/bright yellow]'
+# or define a new type:
+bar = rs.custom.foo - "blink"
+console.print(bar("Hello!"))  # '[bright yellow]Hello![/bright yellow]'
+```
+
+
+## Who is this for?
+I am personally colourblind. It's nice to have someone who is not colourblind define a colour palette, and I simply
+build from this. While colourblind people are not the only intended users, I think it will help them (us) substantially
+in terms of readability of the styles. 
+It's also very helpful for keeping easier track of your styles and reusability.
---
+++ packages/rich-string-builder/rich_string_builder-0.0.1.tar.gz/rich_string_builder-0.0.1/rich_string_builder/string_builder.py
@@ -0,0 +1,109 @@
+from typing import Union, Optional
+
+
+class _ApplyStyle:
+    def __init__(self, style=None):
+        self.style = style
+
+    def __call__(self, *strings):
+        return self.apply_style(*strings)
+
+    def __repr__(self):
+        return f"_ApplyStyle<{self.style}>"
+
+    def __str__(self):
+        return self.style
+
+    def __add__(self, other):
+        if not isinstance(other, (str, _ApplyStyle)):
+            raise TypeError
+        elif isinstance(other, _ApplyStyle):
+            return _ApplyStyle(f"{self.style} {other.style}")
+        else:
+            return _ApplyStyle(f"{self.style} {other}")
+
+    def __sub__(self, other):
+        if not isinstance(other, (str, _ApplyStyle)):
+            raise TypeError
+        elif isinstance(other, _ApplyStyle):
+            return _ApplyStyle(self.style.replace(other.style, "").strip())
+        else:
+            return _ApplyStyle(self.style.replace(other, "").strip())
+
+    def apply_style(self, *strings):
+        braces = "{}" * len(strings)
+        if self.style is not None:
+            return f"[{self.style}]" + braces.format(*strings) + f"[/{self.style}]"
+        else:
+            return braces.format(*strings)
+
+
+class _StyleBuilder:
+    def __str__(self):
+        return str(
+            {
+                attr: str(getattr(self, attr))
+                for attr in dir(self)
+                if not attr.startswith("_")
+            }
+        )
+
+    def __repr__(self):
+        return f"_StyleBuilder<{self.__str__()}>"
+
+
+class RichStyles:
+    String = _ApplyStyle()
+
+    class Styles:
+        dim = _ApplyStyle("dim")
+        bright = _ApplyStyle("bright")
+        bold = _ApplyStyle("bold")
+        strong = _ApplyStyle("strong")
+        code = _ApplyStyle("code")
+        italic = _ApplyStyle("italic")
+        emphasize = _ApplyStyle("emphasize")
+        underline = _ApplyStyle("underline")
+        blink = _ApplyStyle("blink")
+        blink2 = _ApplyStyle("blink2")
+
+    class Colors:
+        black = _ApplyStyle("black")
+        red = _ApplyStyle("red")
+        green = _ApplyStyle("green")
+        yellow = _ApplyStyle("yellow")
+        magenta = _ApplyStyle("magenta")
+        cyan = _ApplyStyle("cyan")
+        white = _ApplyStyle("white")
+        blue = _ApplyStyle("blue")
+
+    def __init__(
+        self, custom_definitions: Optional[dict[str, Union[str, dict]]] = None
+    ):
+        self.custom = (
+            self._objectify(custom_definitions) if custom_definitions else None
+        )
+        self.s = self.Styles
+        self.c = self.Colors
+        self.str = self.String
+
+    @staticmethod
+    def _objectify(custom_definition: dict[str, Union[str, dict]]):
+        def _create_object(value_: Union[str, dict]):
+            if isinstance(value_, str):
+                return _ApplyStyle(value_)
+            elif isinstance(value_, dict):
+                inner_object = _StyleBuilder()
+                for inner_key_, inner_value_ in value_.items():
+                    setattr(inner_object, inner_key_, _create_object(inner_value_))
+                return inner_object
+            else:
+                raise TypeError(
+                    f"Unexpected type {type(value_)} {value_}. "
+                    f"Dictionary must only consist of strings or inner dictionaries."
+                )
+
+        custom = _StyleBuilder()
+        for key, value in custom_definition.items():
+            setattr(custom, key, _create_object(value))
+        return custom
