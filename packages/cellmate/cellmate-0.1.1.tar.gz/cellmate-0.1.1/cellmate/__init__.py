de9dfd0635e80fb58cef773727d7e73d7df92ff4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 22:04:32 +0000

    Add package cellmate/0.1.1/cellmate-0.1.1.tar.gz

---
+++ packages/cellmate/cellmate-0.1.1.tar.gz/cellmate-0.1.1/cellmate/__init__.py
@@ -0,0 +1,14 @@
+from .components import Column, Sheet, Style, Workbook
+from .defaults import (
+    CURRENCY,
+    DATE,
+    DEFAULT,
+    INTEGER,
+    PERCENTAGE,
+    PERCENTAGE_POINTS,
+    TITLE1,
+    TITLE2,
+)
+
+
+__version__ = "0.1.1"
