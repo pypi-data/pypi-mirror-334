bddc315f499bd611547319047b0d5827a089021a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 22:04:38 +0000

    Add package iduedu/0.3.1a0/iduedu-0.3.1a0-py3-none-any.whl

---
+++ packages/iduedu/iduedu-0.3.1a0-py3-none-any.whl/iduedu/__init__.py
@@ -0,0 +1,12 @@
+"""
+IduEdu
+========
+
+IduEdu is a Python package for the creation and manipulation of complex city networks from OpenStreetMap.
+
+Homepage https://github.com/DDonnyy/IduEdu.
+"""
+
+from ._config import config
+from ._api import *
+from ._version import VERSION as __version__
