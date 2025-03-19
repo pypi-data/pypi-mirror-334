67c144e100c21c493622cf1b5eb4817752b678b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 21:04:00 +0000

    Add package PySDL3/0.9.6b0/pysdl3-0.9.6b0-py3-none-any.whl

---
+++ packages/PySDL3/pysdl3-0.9.6b0-py3-none-any.whl/sdl3/SDL_error.py
@@ -0,0 +1,17 @@
+from .__init__ import ctypes, typing, abc, \
+    SDL_FUNC, SDL_SET_CURRENT_BINARY, SDL_GET_BINARY, SDL_BINARY
+
+SDL_SET_CURRENT_BINARY(SDL_BINARY)
+
+SDL_SetError: abc.Callable[..., typing.Any] = SDL_FUNC["SDL_SetError", ctypes.c_bool, [ctypes.c_char_p]]
+SDL_SetErrorV: abc.Callable[..., typing.Any] = SDL_FUNC["SDL_SetErrorV", ctypes.c_bool, [ctypes.c_char_p, ctypes.c_void_p]]
+SDL_OutOfMemory: abc.Callable[..., typing.Any] = SDL_FUNC["SDL_OutOfMemory", ctypes.c_bool, []]
+SDL_GetError: abc.Callable[..., typing.Any] = SDL_FUNC["SDL_GetError", ctypes.c_char_p, []]
+SDL_ClearError: abc.Callable[..., typing.Any] = SDL_FUNC["SDL_ClearError", ctypes.c_bool, []]
+
+def SDL_Unsupported() -> ctypes.c_int:
+    return SDL_GET_BINARY(SDL_BINARY).SDL_SetError("That operation is not supported".encode())
+
+def SDL_InvalidParamError(param: ctypes.c_char_p) -> ctypes.c_int:
+    if isinstance(param, bytes): param = param.decode()
+    return SDL_GET_BINARY(SDL_BINARY).SDL_SetError(f"Parameter '{param}' is invalid".encode())
