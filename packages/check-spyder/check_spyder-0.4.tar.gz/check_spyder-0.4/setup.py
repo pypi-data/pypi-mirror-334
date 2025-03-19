5c4150465a72a26aee7c6b90c4e86aa8886b4bb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 23:04:20 +0000

    Add package check-spyder/0.4/check_spyder-0.4.tar.gz

---
+++ packages/check-spyder/check_spyder-0.4.tar.gz/check_spyder-0.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+# python setup.py sdist
+# twine upload dist/*
+
+#packages=find_packages(),
+#install_requires=['os'],
+setup( 
+    name='check_spyder',
+    version='0.4',
+    packages=['check_spyder'],
+    description='Library to detect if you are using Spyder IDE or not.',
+    long_description=open('check_spyder/Readme.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    author='Javier S. Zurdo',
+    url='https://github.com/javierzurdo/Check_Spyder'
+)
