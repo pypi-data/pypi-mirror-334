9e799a709b640878376c0196faef66b653d6328a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:41:15 +0000

    Add package Geode-Hybrid-Geosciences/1.0.0/geode_hybrid_geosciences-1.0.0-cp310-cp310-win_amd64.whl

---
+++ packages/Geode-Hybrid-Geosciences/geode_hybrid_geosciences-1.0.0-cp310-cp310-win_amd64.whl/geode_hybrid_geosciences/implicit.py
@@ -0,0 +1,10 @@
+#
+# Copyright (c) 2019 - 2025 Geode-solutions. All rights reserved.
+#
+
+import opengeode
+import geode_common
+
+from .bin.geode_hybrid_geosciences_py_implicit import *
+
+HybridGeosciencesImplicitLibrary.initialize()
