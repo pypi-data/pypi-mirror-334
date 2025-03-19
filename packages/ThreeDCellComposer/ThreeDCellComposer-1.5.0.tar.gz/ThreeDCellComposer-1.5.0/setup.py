748deb2260a0f235e158278418d907f04411ed47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 23:04:27 +0000

    Add package ThreeDCellComposer/1.5.0/ThreeDCellComposer-1.5.0.tar.gz

---
+++ packages/ThreeDCellComposer/ThreeDCellComposer-1.5.0.tar.gz/ThreeDCellComposer-1.5.0/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='ThreeDCellComposer',
+    version='1.5.0',    
+    description='3D cell segmentation by composing 2D segmentations',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/murphylab/3DCellComposer/',
+    author='Haoran Chen and Ted Zhang and Robert F. Murphy',
+    author_email='murphy@cmu.edu',
+    license='MIT',
+    packages=['ThreeDCellComposer'],
+    install_requires=[
+        'numpy',
+        'matplotlib',
+        'scipy',
+        'scikit-image',
+        'deepcell',
+        'pandas',
+        'aicsimageio',
+        'CellSegmentationEvaluator',
+        'hubmap-ome-utils>=0.3.3',
+    ],
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',  
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS :: MacOS X',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Scientific/Engineering :: Image Processing',
+    ],
+)
