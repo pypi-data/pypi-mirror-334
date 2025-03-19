b669d53d7e23282aeb44de095f240bc0e44d8cda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 22:04:35 +0000

    Add package qena-shared-lib/0.1.9/qena_shared_lib-0.1.9.tar.gz

---
+++ packages/qena-shared-lib/qena_shared_lib-0.1.9.tar.gz/qena_shared_lib-0.1.9/tests/conftest.py
@@ -0,0 +1,23 @@
+from pytest_asyncio import fixture as fixture_asyncio
+
+from qena_shared_lib.logstash import (
+    BaseLogstashSender,
+    LogstashLogRecord,
+    SenderResponse,
+)
+
+
+class MockLogstashSender(BaseLogstashSender):
+    async def _send(self, _: LogstashLogRecord) -> SenderResponse:
+        return SenderResponse(sent=True)
+
+
+@fixture_asyncio(scope="session")
+async def logstash():
+    logstash = MockLogstashSender(service_name="test")
+
+    await logstash.start()
+
+    yield logstash
+
+    await logstash.stop()
