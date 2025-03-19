146e679d669b8eed3dd87a7d7b17155d470142a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 23:04:20 +0000

    Add package agent-mont/0.1.1/agent_mont-0.1.1.tar.gz

---
+++ packages/agent-mont/agent_mont-0.1.1.tar.gz/agent_mont-0.1.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="agent-mont",
+    version="0.1.1",
+    author="Faisal (BizTech)",
+    author_email="faisalazeeii786@gmail.com",
+    description="A next-level AgentOps monitoring library for Crew AI applications with enhanced metrics.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ansarifaisal12/Agent_Monts.git",  
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    install_requires=[
+        "psutil",
+        "matplotlib",
+        "streamlit",
+        "tiktoken"
+    ],
+    include_package_data=True,
+)
