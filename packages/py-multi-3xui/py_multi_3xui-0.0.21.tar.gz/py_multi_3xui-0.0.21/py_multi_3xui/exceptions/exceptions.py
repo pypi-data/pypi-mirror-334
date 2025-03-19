caede06aa331c7d1946a9c0c94309d990c863cfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 22:04:53 +0000

    Add package py-multi-3xui/0.0.21/py_multi_3xui-0.0.21.tar.gz

---
+++ packages/py-multi-3xui/py_multi_3xui-0.0.21.tar.gz/py_multi_3xui-0.0.21/py_multi_3xui/exceptions/exceptions.py
@@ -0,0 +1,6 @@
+class HostAlreadyExistException(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+class ClientNotFoundException(Exception):
+    def __init__(self, message):
+        super().__init__(message)
