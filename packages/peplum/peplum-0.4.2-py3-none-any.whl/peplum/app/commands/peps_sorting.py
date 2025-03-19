17eb3435c1c9ebba9dd986aae5b0e1379a0914ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 23:04:36 +0000

    Add package peplum/0.4.2/peplum-0.4.2-py3-none-any.whl

---
+++ packages/peplum/peplum-0.4.2-py3-none-any.whl/peplum/app/commands/peps_sorting.py
@@ -0,0 +1,29 @@
+"""Provides command-oriented messages that relate to sorting PEPs."""
+
+##############################################################################
+# Textual enhanced imports.
+from textual_enhanced.commands import Command
+
+
+##############################################################################
+class SortByNumber(Command):
+    """Sort PEPs by their number"""
+
+    BINDING_KEY = "1"
+
+
+##############################################################################
+class SortByCreated(Command):
+    """Sort PEPs by their created date"""
+
+    BINDING_KEY = "2"
+
+
+##############################################################################
+class SortByTitle(Command):
+    """Sort PEPs by their title"""
+
+    BINDING_KEY = "3"
+
+
+### peps_sorting.py ends here
