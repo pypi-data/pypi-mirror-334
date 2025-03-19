d0499c347ea3a156262442e6b673a658d58fb355
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 22:04:21 +0000

    Add package scalign-umap/0.1.0/scalign_umap-0.1.0.tar.gz

---
+++ packages/scalign-umap/scalign_umap-0.1.0.tar.gz/scalign_umap-0.1.0/setup.py
@@ -0,0 +1,33 @@
+
+import platform
+from setuptools import setup
+
+configuration = {
+    "name": "scalign-umap",
+    "version": "0.1.0",
+    "description": 
+        "fork from package umap-learn 0.5.8 for usage from scalign package. "
+        "contains additional bug fixes",
+    "license": "BSD",
+    "packages": ["scalign_umap"],
+    "install_requires": [
+        "numpy >= 1.23",
+        "scipy >= 1.3.1",
+        "scikit-learn >= 1.6",
+        "numba >= 0.51.2",
+        "pynndescent >= 0.5",
+        "tqdm",
+    ],
+    "extras_require": {
+        "parametric": [
+            "tensorflow >= 2.1",
+            "keras >= 3.0"
+        ]
+    },
+    "ext_modules": [],
+    "cmdclass": {},
+    "data_files": (),
+    "zip_safe": False,
+}
+
+setup(**configuration)
