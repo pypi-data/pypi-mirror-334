12efa2159a0476a4449c661dd5c015b00f7960d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:41:15 +0000

    Add package windows-terminal/0.1.0/windows_terminal-0.1.0.tar.gz

---
+++ packages/windows-terminal/windows_terminal-0.1.0.tar.gz/windows_terminal-0.1.0/README.md
@@ -0,0 +1,6 @@
+# Windows Terminal Package
+This is a placeholder package for Windows Terminal enhancements.
+
+## Installation
+```sh
+pip install windows-terminal
---
+++ packages/windows-terminal/windows_terminal-0.1.0.tar.gz/windows_terminal-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="windows-terminal",
+    version="0.1.0",
+    description="A test package",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[],
+)
