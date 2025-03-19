eadc368785e542be2bb86262271751d9c81ca711
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 22:04:54 +0000

    Add package iaskai/0.1.3/iaskai-0.1.3.tar.gz

---
+++ packages/iaskai/iaskai-0.1.3.tar.gz/iaskai-0.1.3/iask/_internal.py
@@ -0,0 +1,15 @@
+import re
+from markdownify import markdownify as md
+from typing import Optional, Union
+
+
+def cache_find(diff: Union[dict, list]) -> Optional[str]:
+    values = diff if isinstance(diff, list) else diff.values()
+    for value in values:
+        if isinstance(value, (list, dict)):
+            if cache := cache_find(value):
+                return cache
+        if isinstance(value, str) and re.search(r"<p>.+?</p>", value):
+            return md(value).strip()
+
+    return None
