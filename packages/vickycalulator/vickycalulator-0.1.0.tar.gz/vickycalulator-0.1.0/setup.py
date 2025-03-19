f19b13f1ded63555ad372078ad104f6fd3730f08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 23:04:04 +0000

    Add package vickycalulator/0.1.0/vickycalulator-0.1.0.tar.gz

---
+++ packages/vickycalulator/vickycalulator-0.1.0.tar.gz/vickycalulator-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vickycalulator',                   # Package name (must be unique on PyPI)
+    version='0.1.0',                # Initial release version
+    description='A simple calculation package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Vicky',
+    author_email='vickyvijay069@gmail.com',
+    # url='https://github.com/yourusername/vicky',  # Optional, your repo URL
+    packages=find_packages(),       # Automatically find package directories
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',        # Specify minimum Python version if needed
+)
