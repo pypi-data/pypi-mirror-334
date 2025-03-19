1720e0bbc997cddaa608f2c66179f03a1c4694f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 23:04:04 +0000

    Add package anygrad/0.0.4/anygrad-0.0.4-cp313-cp313-win32.whl

---
+++ packages/anygrad/anygrad-0.0.4-cp313-cp313-win32.whl/anygrad/tensor/base/__init__.py
@@ -0,0 +1,6 @@
+from anygrad.tensor.base.basetensor import BaseTensor
+from anygrad.tensor.base.booltensor import BoolTensor
+from anygrad.tensor.base.floattensor import FloatTensor
+from anygrad.tensor.base.inttensor import IntTensor
+
+__all__ = ["BaseTensor", "BoolTensor", "FloatTensor", "IntTensor"]
