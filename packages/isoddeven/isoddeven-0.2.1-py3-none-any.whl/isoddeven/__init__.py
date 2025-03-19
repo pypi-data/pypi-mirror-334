ce15a6c786e6db5f1614e3cd1ad45ad7c3d4ab36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:26:29 +0000

    Add package isoddeven/0.2.1/isoddeven-0.2.1-py3-none-any.whl

---
+++ packages/isoddeven/isoddeven-0.2.1-py3-none-any.whl/isoddeven/__init__.py
@@ -0,0 +1,9 @@
+"""
+isoddeven - A fun Python package to find out if a number is odd or even vouched at https://mewtru.com/is-odd
+"""
+
+from .isoddeven import is_odd
+from .isoddeven import is_odd
+from .isoddeven import state
+from .extras import is_up
+from .extras import about
