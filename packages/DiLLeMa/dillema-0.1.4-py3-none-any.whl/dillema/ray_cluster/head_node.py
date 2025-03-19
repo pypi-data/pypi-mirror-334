34c5a080c676ca0afe15d6292cf75aafb0aa8702
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 21:04:03 +0000

    Add package DiLLeMa/0.1.4/dillema-0.1.4-py3-none-any.whl

---
+++ packages/DiLLeMa/dillema-0.1.4-py3-none-any.whl/dillema/ray_cluster/head_node.py
@@ -0,0 +1,27 @@
+import time
+from dillema.common import get_ip_address
+import ray
+import argparse
+import logging
+
+
+def start_ray_head_node(parse_arg: argparse.ArgumentParser):
+    """
+    Start a Ray head node on the given port.
+    """
+    logging.basicConfig(level=logging.INFO)
+
+    try:
+        ray.init(
+            ignore_reinit_error=True,
+            include_dashboard=True,
+            object_store_memory=10**9,
+            dashboard_host="0.0.0.0",
+            _node_name="head_node",
+        )
+        while True:
+            time.sleep(20)
+
+    except Exception as e:
+        logging.error(f"Failed to start Ray head node: {e}")
+        raise
