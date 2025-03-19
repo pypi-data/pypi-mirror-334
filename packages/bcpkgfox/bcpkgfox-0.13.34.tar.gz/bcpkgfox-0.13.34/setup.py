3fad6ea07379262e2082771bf873d918a2318182
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 22:04:52 +0000

    Add package bcpkgfox/0.13.34/bcpkgfox-0.13.34.tar.gz

---
+++ packages/bcpkgfox/bcpkgfox-0.13.34.tar.gz/bcpkgfox-0.13.34/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bcpkgfox",
+    version="0.13.34",
+    author="Guilherme Neri",
+    author_email="guilherme.neri@bcfox.com.br",
+    description="Biblioteca BCFOX",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/robotsbcfox/PacotePythonBCFOX",
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "bc-find-imports=bcpkgfox.cli:main",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        'setuptools',
+        'selenium'
+    ],
+    extras_require={
+        "full": [
+            'undetected-chromedriver',
+            'webdriver-manager',
+            'opencv-python',
+            'pygetwindow',
+            'pyscreeze',
+            'pyautogui',
+            'requests',
+            'pymupdf',
+            'Pillow',
+            'psutil'
+        ],
+    },
+)
+
