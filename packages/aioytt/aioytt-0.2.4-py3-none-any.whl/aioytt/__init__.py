01d809fe301e21f9a597b806a07e7f58084bf778
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 22:04:46 +0000

    Add package aioytt/0.2.4/aioytt-0.2.4-py3-none-any.whl

---
+++ packages/aioytt/aioytt-0.2.4-py3-none-any.whl/aioytt/__init__.py
@@ -0,0 +1,11 @@
+import os
+import sys
+from typing import Final
+
+from loguru import logger
+
+from .transcript import get_transcript_from_url
+from .transcript import get_transcript_from_video_id
+
+LOGURU_LEVEL: Final[str] = os.getenv("LOGURU_LEVEL", "INFO")
+logger.configure(handlers=[{"sink": sys.stderr, "level": LOGURU_LEVEL}])
