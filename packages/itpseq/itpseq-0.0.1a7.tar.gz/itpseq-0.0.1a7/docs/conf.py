5ba1276f71a98dd81c2ec0bd997291e9f051c16f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 23:04:32 +0000

    Add package itpseq/0.0.1a7/itpseq-0.0.1a7.tar.gz

---
+++ packages/itpseq/itpseq-0.0.1a7.tar.gz/itpseq-0.0.1a7/docs/conf.py
@@ -0,0 +1,50 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+
+import sys, os
+sys.path.insert(0, os.path.abspath('..'))
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'iTP-Seq'
+copyright = '2025, Thibaud Renault'
+author = 'Thibaud Renault'
+release = '0.0.1a7'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.napoleon',
+    'numpydoc',
+    'nbsphinx',
+    'sphinx_gallery.load_style',
+    #'myst_nb',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+source_suffix = ['.rst', '.md']
+nbsphinx_execute = 'never'
+
+autosummary_generate = True
+autodoc_member_order = 'groupwise'
+
+nbsphinx_thumbnails = {
+    'notebooks/itoeprint_tutorial': '_static/itoeprint_tutorial.png',
+    #'notebooks/report_tutorial': '_static/report_tutorial.png',
+    'notebooks/report_tutorial': '_static/report_page.png',
+}
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
