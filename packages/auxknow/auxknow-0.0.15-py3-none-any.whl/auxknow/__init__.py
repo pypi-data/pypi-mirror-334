89cb6022ffbe0e192e6fee5125953dc6b93ec0a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 22:04:16 +0000

    Add package auxknow/0.0.15/auxknow-0.0.15-py3-none-any.whl

---
+++ packages/auxknow/auxknow-0.0.15-py3-none-any.whl/auxknow/__init__.py
@@ -0,0 +1,12 @@
+import warnings
+from .auxknow import AuxKnow, AuxKnowConfig, AuxKnowAnswer, AuxKnowSession
+
+warnings.filterwarnings(
+    "ignore",
+    message="Pydantic serializer warnings:",
+    category=UserWarning,
+    module="pydantic.main",
+)
+
+__version__ = "0.0.15"
+__all__ = ["AuxKnow", "AuxKnowConfig", "AuxKnowAnswer", "AuxKnowSession"]
