fbb080e33d37f2260ceb3a25f466415387802afe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 21:04:05 +0000

    Add package yta-api/0.0.33/yta_api-0.0.33.tar.gz

---
+++ packages/yta-api/yta_api-0.0.33.tar.gz/yta_api-0.0.33/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+
+VERSION = '0.0.33'
+DESCRIPTION = 'Youtube Autonomous API Module.'
+LONG_DESCRIPTION = 'This is the Youtube Autonomous API module'
+
+setup(
+    name = "yta_api", 
+    version = VERSION,
+    author = "Daniel Alcalá",
+    author_email = "<danielalcalavalera@gmail.com>",
+    description = DESCRIPTION,
+    long_description = LONG_DESCRIPTION,
+    packages = find_packages(),
+    install_requires = [
+        'fastapi',
+        'yta_audio',
+        'yta_image',
+        'yta_general_utils',
+        #'pyliblzma' # Only for vercel deployment
+    ],
+    
+    keywords = [
+        'youtube autonomous api module software'
+    ],
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
