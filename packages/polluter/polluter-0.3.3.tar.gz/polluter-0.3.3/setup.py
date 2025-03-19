21d545f99a60d45da221762119df896c1f0d519d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:26:28 +0000

    Add package polluter/0.3.3/polluter-0.3.3.tar.gz

---
+++ packages/polluter/polluter-0.3.3.tar.gz/polluter-0.3.3/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="polluter",
+    version="0.3.3",
+    author="j@ack",
+    packages=find_packages(),  # Automatically find packages in the root directory
+)
