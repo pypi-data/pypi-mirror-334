d8cd377b56367e379321c086048eaabfa2cbf48b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:26:33 +0000

    Add package para-attn/0.3.27/para_attn-0.3.27.tar.gz

---
+++ packages/para-attn/para_attn-0.3.27.tar.gz/para_attn-0.3.27/src/para_attn/parallel_vae/diffusers_adapters/__init__.py
@@ -0,0 +1,17 @@
+import importlib
+
+
+def parallelize_vae(vae, *args, **kwargs) -> None:
+    vae_cls_name = vae.__class__.__name__
+    if False:
+        pass
+    elif vae_cls_name == "AutoencoderKL":
+        adapter_name = "autoencoder_kl"
+    elif vae_cls_name == "AutoencoderKLHunyuanVideo":
+        adapter_name = "autoencoder_kl_hunyuan_video"
+    else:
+        raise ValueError(f"Unknown vae class name: {vae_cls_name}")
+
+    adapter_module = importlib.import_module(f".{adapter_name}", __package__)
+    parallelize_vae_fn = getattr(adapter_module, "parallelize_vae")
+    return parallelize_vae_fn(vae, *args, **kwargs)
