cb7132b6b6c4dd6ad9d694ceb1a95a16f304a58c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-19 20:26:33 +0000

    Add package uav4res-visualizer/0.1.3/uav4res_visualizer-0.1.3.tar.gz

---
+++ packages/uav4res-visualizer/uav4res_visualizer-0.1.3.tar.gz/uav4res_visualizer-0.1.3/setup.py
@@ -0,0 +1,46 @@
+import setuptools
+
+LONG_DESC = open("README.md").read()
+VERSION = "0.1.3"
+DOWNLOAD = "https://github.com/UAV4Res/uav4res-visualizer/archive/%s.tar.gz" % VERSION
+
+setuptools.setup(
+    name="uav4res-visualizer",
+    version=VERSION,
+    author="UAV4Res",
+    author_email="nhphuong.code@gmail.com",
+    description="uav4res-visualizer",
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESC,
+    keywords="uav4res_visualizer",
+    license="MIT",
+    url="https://github.com/UAV4Res/uav4res-visualizer",
+    download_url=DOWNLOAD,
+    classifiers=[
+        "Environment :: X11 Applications",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ],
+    packages=setuptools.find_packages(),
+    entry_points={
+        "console_scripts": ["uav4res_visualizer=uav4res_visualizer.__main__:main"]
+    },
+    python_requires=">=3.5",
+    install_requires=[
+        "pygame>=2.4.0",
+        "numpy",
+        "opencv-python",
+        "Pillow",
+    ],
+    package_data={
+        "uav4res_visualizer": [
+            "engine/*",
+            "assets/*",
+            "assets/images/*",
+        ]
+    },
+    include_package_data=True,
+    zip_safe=False,
+)
