7b99a370b9fc3f21ddd3ce3111fd97e1a46decd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 21:04:30 +0000

    Add package logax/4.0/logax-4.0.tar.gz

---
+++ packages/logax/logax-4.0.tar.gz/logax-4.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="logax",
+    version="4.0",
+    author="",  
+    author_email="", 
+    description="A Python library to log output",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/logax/logax", 
+    project_urls={
+        "Bug Tracker": "https://github.com/logax/logax/issues",
+    },
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    license="MIT",  
+    python_requires=">=3",
+    install_requires=["requests", "pybase64"],
+)
