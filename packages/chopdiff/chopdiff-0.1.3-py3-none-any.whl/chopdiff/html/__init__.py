abaa11479ca2f26bf1abe118380ef788264a17a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 06:03:45 +0000

    Add package chopdiff/0.1.3/chopdiff-0.1.3-py3-none-any.whl

---
+++ packages/chopdiff/chopdiff-0.1.3-py3-none-any.whl/chopdiff/html/__init__.py
@@ -0,0 +1,20 @@
+# flake8: noqa: F401
+
+from chopdiff.html.extractor import ContentNotFound, Extractor, Match
+from chopdiff.html.html_find_tags import TagMatch, html_extract_attribute_value, html_find_tag
+from chopdiff.html.html_in_md import (
+    Wrapper,
+    div_wrapper,
+    escape_md_html,
+    html_a,
+    html_b,
+    html_div,
+    html_i,
+    html_img,
+    html_join_blocks,
+    html_span,
+    md_para,
+    span_wrapper,
+)
+from chopdiff.html.html_plaintext import html_to_plaintext, plaintext_to_html
+from chopdiff.html.timestamps import TimestampExtractor, extract_timestamp, has_timestamp
---
+++ packages/chopdiff/chopdiff-0.1.3-py3-none-any.whl/chopdiff/util/__init__.py
@@ -0,0 +1,4 @@
+# flake8: noqa: F401
+
+from chopdiff.util.lemmatize import lemmatize, lemmatized_equal
+from chopdiff.util.tiktoken_utils import tiktoken_len
