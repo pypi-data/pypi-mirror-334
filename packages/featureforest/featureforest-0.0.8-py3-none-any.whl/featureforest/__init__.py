77649429317384b9c90bfdf14eb7c934fedde3e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 13:10:27 +0000

    Add package featureforest/0.0.8/featureforest-0.0.8-py3-none-any.whl

---
+++ packages/featureforest/featureforest-0.0.8-py3-none-any.whl/featureforest/__init__.py
@@ -0,0 +1,9 @@
+__version__ = "0.0.8"
+
+from ._feature_extractor_widget import FeatureExtractorWidget
+from ._segmentation_widget import SegmentationWidget
+
+__all__ = (
+    "FeatureExtractorWidget",
+    "SegmentationWidget",
+)
