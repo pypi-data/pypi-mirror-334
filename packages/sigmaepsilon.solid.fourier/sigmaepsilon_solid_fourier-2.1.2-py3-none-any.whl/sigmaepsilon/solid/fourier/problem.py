5c8596ba6c2cf317edd64f77c8b052c75ed80491
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 00:06:11 +0000

    Add package sigmaepsilon.solid.fourier/2.1.2/sigmaepsilon_solid_fourier-2.1.2-py3-none-any.whl

---
+++ packages/sigmaepsilon.solid.fourier/sigmaepsilon_solid_fourier-2.1.2-py3-none-any.whl/sigmaepsilon/solid/fourier/problem.py
@@ -0,0 +1,39 @@
+from abc import abstractmethod
+
+from .result import LoadCaseResultLinStat
+from .protocols import LoadGroupProtocol
+
+__all__ = ["NavierProblem"]
+
+
+class NavierProblem:
+    """
+    Base class for Navier problems. The sole reason of this class is to
+    avoid circular referencing.
+    """
+
+    result_class = LoadCaseResultLinStat
+
+    def __init__(self, *, loads: LoadGroupProtocol | None = None):
+        self._loads = loads
+
+    @property
+    def loads(self) -> LoadGroupProtocol | None:
+        """
+        Returns the loads.
+        """
+        return self._loads
+
+    @loads.setter
+    def loads(self, value: LoadGroupProtocol | None):
+        """
+        Sets the loads.
+        """
+        self._loads = value
+
+    def _postproc_linstat_load_case_result(self, data) -> LoadCaseResultLinStat:
+        res = self.result_class(data, name="values")
+        return res
+
+    @abstractmethod
+    def linear_static_analysis(self, *args, **kwargs): ...
