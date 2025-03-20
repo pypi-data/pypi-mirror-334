a93999256873601452edd0fc7aa79eb83171a30b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 09:04:44 +0000

    Add package justin-furuness/0.3.0/justin_furuness-0.3.0.tar.gz

---
+++ packages/justin-furuness/justin_furuness-0.3.0.tar.gz/justin_furuness-0.3.0/justin_furuness/__main__.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+from .speakers import Justin
+# from justin_furuness.speakers import Justin
+
+def main():
+    Justin().print_name()
+    # with open("names.txt") as f:
+    with (Path(__file__).parent / "names.txt").open() as f:
+        print(f.read())
+
+if __name__ == "__main__":
+    main()
