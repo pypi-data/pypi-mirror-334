e6e2e099f8129c74bbb700485d875803d8d884cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 23:05:45 +0000

    Add package fb2maker/0.1.1/fb2maker-0.1.1.tar.gz

---
+++ packages/fb2maker/fb2maker-0.1.1.tar.gz/fb2maker-0.1.1/fb2maker/__init__.py
@@ -0,0 +1,7 @@
+from .FictionBook2 import FictionBook2
+from .Author import Author
+from .TitleInfo import TitleInfo
+from .DocumentInfo import DocumentInfo
+from .FB2ImageDataclass import Image
+
+__all__ = ["FictionBook2", "Author", "TitleInfo", "DocumentInfo", "Image"]
