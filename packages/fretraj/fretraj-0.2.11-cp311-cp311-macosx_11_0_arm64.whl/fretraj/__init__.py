a5ad17801052395ffc2315a83916a65585f8061d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 17:05:29 +0000

    Add package fretraj/0.2.11/fretraj-0.2.11-cp311-cp311-macosx_11_0_arm64.whl

---
+++ packages/fretraj/fretraj-0.2.11-cp311-cp311-macosx_11_0_arm64.whl/fretraj/__init__.py
@@ -0,0 +1,60 @@
+import importlib.metadata as meta
+
+PACKAGE = "fretraj"
+
+try:
+    metadata = meta.metadata(PACKAGE)
+except meta.PackageNotFoundError:
+    print(f"{PACKAGE} is not installed yet.")
+
+
+def _get_urls():
+    __urls__ = {}
+    try:
+        for _url in metadata.get_all("Project-URL"):
+            __urls__.update(dict([_url.replace(" ", "").split(",")]))
+    except TypeError:
+        __urls__["Documentation"] = None
+        __urls__["Repository"] = None
+        __urls__["Issues"] = None
+    return __urls__
+
+
+__version__ = metadata["Version"]
+__author__ = metadata["Author-email"]
+__summary__ = metadata["Summary"]
+__keywords__ = metadata["Keywords"]
+__license__ = metadata["License"]
+__urls__ = _get_urls()
+
+
+try:
+    import LabelLib as ll
+except ModuleNotFoundError:
+    _LabelLib_found = False
+else:
+    _LabelLib_found = True
+
+try:
+    import nglview
+except ModuleNotFoundError:
+    _nglview_found = False
+    _jupyter_module_available = False
+else:
+    _nglview_found = True
+    _jupyter_module_available = True
+    from . import jupyter
+
+try:
+    from . import burst
+except (ImportError, ModuleNotFoundError):
+    _burst_module_available = False
+    print("The burst submodule could not be imported\n")
+else:
+    _burst_module_available = True
+
+from . import cloud
+from . import export
+from . import fret
+from . import grid
+from . import restraints
