4f124df6b71679c744111dea9f5b7f8816179bae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 23:05:13 +0000

    Add package xpip-upload/1.2.1/xpip_upload-1.2.1.tar.gz

---
+++ packages/xpip-upload/xpip_upload-1.2.1.tar.gz/xpip_upload-1.2.1/setup-upload.py
@@ -0,0 +1,36 @@
+# coding=utf-8
+
+import os
+
+from setuptools import find_packages
+from setuptools import setup
+
+from xpip_upload.attribute import __author__
+from xpip_upload.attribute import __author_email__
+from xpip_upload.attribute import __description__
+from xpip_upload.attribute import __project__
+from xpip_upload.attribute import __urlbugs__
+from xpip_upload.attribute import __urlcode__
+from xpip_upload.attribute import __urldocs__
+from xpip_upload.attribute import __urlhome__
+from xpip_upload.attribute import __version__
+
+long_description: str = os.path.join("docs", "xpip-upload.md")
+
+setup(
+    name=__project__,
+    version=__version__,
+    description=__description__,
+    long_description=open(long_description).read(),
+    long_description_content_type="text/markdown",
+    keywords=["twine", "wheel"],
+    url=__urlhome__,
+    author=__author__,
+    author_email=__author_email__,
+    project_urls={"Source Code": __urlcode__,
+                  "Bug Tracker": __urlbugs__,
+                  "Documentation": __urldocs__},
+    packages=find_packages(include=["xpip_upload*"]),
+    install_requires=["wheel", "twine", "keyring", "keyrings.alt"],
+    entry_points={"console_scripts": ["xpip-upload = xpip_upload.cmds:main"]},
+)
