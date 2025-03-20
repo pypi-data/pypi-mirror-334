7bd9f2d7d12202c71d5670331fd4bea42d37b9d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 01:31:10 +0000

    Add package dj-angles/0.17.0/dj_angles-0.17.0-py3-none-any.whl

---
+++ packages/dj-angles/dj_angles-0.17.0-py3-none-any.whl/dj_angles/templatetags/dj_angles.py
@@ -0,0 +1,19 @@
+from datetime import date, datetime, time
+from typing import Union
+
+from django import template
+
+from dj_angles.templatetags.call import do_call
+from dj_angles.templatetags.model import do_model
+
+register = template.Library()
+
+
+@register.filter(name="dateformat")
+def dateformat(value: Union[datetime, date, time], date_format: str):
+    return value.strftime(date_format)
+
+
+# Register custom template tags
+register.tag("call", do_call)
+register.tag("model", do_model)
