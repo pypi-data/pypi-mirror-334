79f8039b9e9d2c41bc7574cf2368895e9bfe5670
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 23:05:43 +0000

    Add package cogito/0.3.0a5/cogito-0.3.0a5-py3-none-any.whl

---
+++ packages/cogito/cogito-0.3.0a5-py3-none-any.whl/cogito/__init__.py
@@ -0,0 +1,9 @@
+from cogito.core.app import Application
+from cogito.core.models import BasePredictor
+from cogito.core.utils import model_download
+
+__all__ = [
+    "Application",
+    "BasePredictor",
+    "model_download",
+]
