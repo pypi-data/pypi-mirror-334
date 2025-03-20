f4126237837f9259f3b9308699d453df88793b1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 13:11:21 +0000

    Add package szn-pyfastrpc/1.1.37/szn_pyfastrpc-1.1.37.tar.gz

---
+++ packages/szn-pyfastrpc/szn_pyfastrpc-1.1.37.tar.gz/szn_pyfastrpc-1.1.37/setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='szn-pyfastrpc',
+    version='1.1.37',
+    description='A FastRPC protocol implementation in Python',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Roman Skvara',
+    author_email='skvara.roman@gmail.com',
+    url='https://github.com/opicevopice/szn-pyfastrpc',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT',
+    install_requires=[
+        'requests-html>=0.10.0'
+    ],
+    entry_points={
+        'console_scripts': [
+            # This entry point will call the open_readme() function defined in open_readme.py,
+            'open-readme=szn_pyfastrpc.open_readme:open_readme'
+        ]
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
