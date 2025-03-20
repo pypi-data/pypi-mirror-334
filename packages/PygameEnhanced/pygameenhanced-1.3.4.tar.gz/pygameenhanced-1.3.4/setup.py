16510aa96828fe8c3621061e224d8baf3cec0615
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 02:28:52 +0000

    Add package PygameEnhanced/1.3.4/pygameenhanced-1.3.4.tar.gz

---
+++ packages/PygameEnhanced/pygameenhanced-1.3.4.tar.gz/pygameenhanced-1.3.4/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="PygameEnhanced",
+    version="1.3.4",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={"PygameEnhanced": ["templates/*.html"]},
+    install_requires=["Flask"],
+)
