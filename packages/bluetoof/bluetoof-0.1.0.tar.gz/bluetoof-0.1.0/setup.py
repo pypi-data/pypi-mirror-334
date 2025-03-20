7a6e5fdd8413bf5b882ac7a07699b3474a35581d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 16:05:29 +0000

    Add package bluetoof/0.1.0/bluetoof-0.1.0.tar.gz

---
+++ packages/bluetoof/bluetoof-0.1.0.tar.gz/bluetoof-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="bluetoof",
+    version="0.1.0",
+    description="Librairie Python pour interagir avec des appareils Bluetooth",
+    author="mz4r",
+    author_email="mz4rr@proton.me",
+    packages=find_packages(),
+    install_requires=["pybluez"],
+)
---
+++ packages/bluetoof/bluetoof-0.1.0.tar.gz/bluetoof-0.1.0/tests/test_bluetoof.py
@@ -0,0 +1,5 @@
+import bluetoof
+
+def test_instance():
+    bt = bluetoof.Bluetoof()
+    assert bt is not None
