f56f7f225e3b68fef87c9f072a69dcd45a3ce15c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 21:04:52 +0000

    Add package AutoStatLib/0.2.6/autostatlib-0.2.6-py3-none-any.whl

---
+++ packages/AutoStatLib/autostatlib-0.2.6-py3-none-any.whl/AutoStatLib/__main__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+from AutoStatLib.AutoStatLib import StatisticalAnalysis
+from AutoStatLib._version import __version__
+
+if __name__ == '__main__':
+    print('This package works as an imported module only.\nUse "import autostatlib" statement')
