f4ffb0a4d9aa4153575a36d9c2e811f5ae4bee76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 06:03:54 +0000

    Add package plasann/1.0.5/plasann-1.0.5.tar.gz

---
+++ packages/plasann/plasann-1.0.5.tar.gz/plasann-1.0.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="plasann",
+    version="1.0.5",
+    author="Habibul Islam",
+    author_email="hislam2@ur.rochester.edu",
+    description="A plasmid annotation pipeline",
+    packages=find_packages(),
+    install_requires=[
+        "biopython",
+        "pandas",
+        "matplotlib",
+        "pycirclize",
+        "gdown",
+    ],
+    entry_points={
+        "console_scripts": [
+            "PlasAnn=plasann.cli:main",
+        ],
+    },
+)
