561315660a2cb7d7962e7f61b32ece7f2fe19ef8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 17:05:06 +0000

    Add package rapids-dependency-file-generator/1.18.1/rapids_dependency_file_generator-1.18.1.tar.gz

---
+++ packages/rapids-dependency-file-generator/rapids_dependency_file_generator-1.18.1.tar.gz/rapids_dependency_file_generator-1.18.1/src/rapids_dependency_file_generator/_constants.py
@@ -0,0 +1,14 @@
+cli_name = "rapids-dependency-file-generator"
+
+default_channels = [
+    "rapidsai",
+    "rapidsai-nightly",
+    "dask/label/dev",
+    "conda-forge",
+    "nvidia",
+]
+
+default_conda_dir = "conda/environments"
+default_requirements_dir = "python"
+default_pyproject_dir = "python"
+default_dependency_file_path = "dependencies.yaml"
