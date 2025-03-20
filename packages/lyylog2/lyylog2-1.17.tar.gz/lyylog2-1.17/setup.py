ff07de4e9c242a7a5d832edcc126d83ac89520af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 08:04:18 +0000

    Add package lyylog2/1.17/lyylog2-1.17.tar.gz

---
+++ packages/lyylog2/lyylog2-1.17.tar.gz/lyylog2-1.17/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='lyylog2',
+    version='1.17',
+    author='lyy',
+    author_email='',
+    description='lyylog2 for lyy',
+    #packages=find_packages(),
+    license="MIT",
+    install_requires=[
+
+
+    ],
+)
