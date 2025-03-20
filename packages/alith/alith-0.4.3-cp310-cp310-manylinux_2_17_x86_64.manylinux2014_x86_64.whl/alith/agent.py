daf507024bd10938ef201b13d2679b0ee05a2235
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 12:05:48 +0000

    Add package alith/0.4.3/alith-0.4.3-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/alith/alith-0.4.3-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/alith/agent.py
@@ -0,0 +1,43 @@
+from dataclasses import dataclass, field
+from typing import List, Union, Callable, Optional
+from .tool import Tool, create_delegate_tool
+from .store import Store
+from ._alith import DelegateAgent as _DelegateAgent
+from ._alith import DelegateTool as _DelegateTool
+
+
+@dataclass
+class Agent:
+    model: str
+    name: Optional[str] = field(default_factory=str)
+    preamble: Optional[str] = field(default_factory=str)
+    api_key: Optional[str] = field(default_factory=str)
+    base_url: Optional[str] = field(default_factory=str)
+    tools: List[Union[Tool, Callable]] = field(default_factory=list)
+    mcp_config_path: Optional[str] = field(default_factory=str)
+    store: Optional[Store] = None
+
+    def prompt(self, prompt: str) -> str:
+        tools = [
+            (
+                create_delegate_tool(tool)
+                if isinstance(tool, Callable)
+                else tool.to_delegate_tool() if isinstance(tool, Tool) else tool
+            )
+            for tool in self.tools or []
+        ]
+        agent = _DelegateAgent(
+            self.name or "",
+            self.model,
+            self.api_key,
+            self.base_url,
+            self.preamble,
+            tools,
+            self.mcp_config_path,
+        )
+        if self.store:
+            docs = self.store.search(prompt)
+            prompt = "{}\n\n<attachments>\n{}</attachments>\n".format(
+                prompt, "".join(docs)
+            )
+        return agent.prompt(prompt)
