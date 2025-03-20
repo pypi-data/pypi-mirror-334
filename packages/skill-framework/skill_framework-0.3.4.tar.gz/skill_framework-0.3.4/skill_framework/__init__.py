ae330b082c9480317db343a9761ef8c9e9d2238b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 16:06:14 +0000

    Add package skill-framework/0.3.4/skill_framework-0.3.4.tar.gz

---
+++ packages/skill-framework/skill_framework-0.3.4.tar.gz/skill_framework-0.3.4/skill_framework/__init__.py
@@ -0,0 +1,17 @@
+__all__ = [
+    'skill',
+    'SkillInput',
+    'SkillParameter',
+    'preview_skill',
+    'SkillOutput',
+    'ExitFromSkillException',
+    'ParameterDisplayDescription',
+    'SuggestedQuestion',
+    'SkillVisualization'
+]
+
+from skill_framework.skills import (skill, SkillInput, SkillParameter, SkillOutput, ExitFromSkillException,
+                                    ParameterDisplayDescription, SuggestedQuestion, SkillVisualization)
+from skill_framework.preview import preview_skill
+
+__version__ = '0.3.4'
---
+++ packages/skill-framework/skill_framework-0.3.4.tar.gz/skill_framework-0.3.4/test/test_input_init.py
@@ -0,0 +1,32 @@
+from skill_framework import skill, SkillParameter, SkillInput
+
+@skill(
+    name="some_skill",
+    parameters=[
+        SkillParameter(name='metrics', is_multi=True),
+        SkillParameter(name='dim')
+    ]
+)
+def dummy_skill():
+    pass
+
+
+def test_args():
+    skill_input: SkillInput = dummy_skill.create_input(arguments={'metrics': ['sales']})
+    assert skill_input.arguments.metrics[0] == 'sales'
+    assert skill_input.arguments.dim is None
+
+
+def test_empty_args():
+    skill_input = dummy_skill.create_input()
+    assert isinstance(skill_input.arguments.metrics, list)
+    assert len(skill_input.arguments.metrics) == 0
+    assert skill_input.arguments.dim is None
+
+
+def test_invalid_arg():
+    skill_input = dummy_skill.create_input(arguments={'metrics': ['sales'], 'bad_arg': 'some value'})
+    assert skill_input.arguments.metrics[0] == 'sales'
+    assert not hasattr(skill_input.arguments, 'bad_arg')
+
+
