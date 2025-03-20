b224525607e619434abd71379931336101b3fd4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 23:06:00 +0000

    Add package execo/2.8.2/execo-2.8.2.tar.gz

---
+++ packages/execo/execo-2.8.2.tar.gz/execo-2.8.2/doc/code_samples/g5k_iperf.py
@@ -0,0 +1,32 @@
+from execo import *
+from execo_g5k import *
+import itertools
+[(jobid, site)] = oarsub([
+  ( OarSubmission(resources = "/cluster=2/nodes=4"), "nancy")
+])
+if jobid:
+    try:
+        nodes = []
+        wait_oar_job_start(jobid, site)
+        nodes = get_oar_job_nodes(jobid, site)
+        # group nodes by cluster
+        sources, targets = [ list(n) for c, n in itertools.groupby(
+            sorted(nodes, key=get_host_cluster),
+            get_host_cluster) ]
+        servers = Remote("iperf -s",
+                         targets,
+                         connection_params = default_oarsh_oarcp_params)
+        clients = Remote("iperf -c {{[t.address for t in targets]}}",
+                         sources,
+                         connection_params = default_oarsh_oarcp_params)
+        with servers.start():
+            sleep(1)
+            clients.run()
+        servers.wait()
+        print(Report([ servers, clients ]).to_string())
+        for index, p in enumerate(clients.processes):
+            print("client %s -> server %s - stdout:" % (p.host.address,
+                                                        targets[index].address))
+            print(p.stdout)
+    finally:
+        oardel([(jobid, site)])
