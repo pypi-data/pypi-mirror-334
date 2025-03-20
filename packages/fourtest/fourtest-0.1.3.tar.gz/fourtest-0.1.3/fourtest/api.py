ad8a21e313b3139d83cbb9ebcc52f74ca5bb6d5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 11:04:45 +0000

    Add package fourtest/0.1.3/fourtest-0.1.3.tar.gz

---
+++ packages/fourtest/fourtest-0.1.3.tar.gz/fourtest-0.1.3/fourtest/api.py
@@ -0,0 +1,8 @@
+# fourtest/api.py
+
+def hello_world(name="World"):
+    return f"Hello, {name}!"
+
+def run():
+    print("Running the fourtest-api command!")
+    print("The command has been executed successfully!")
