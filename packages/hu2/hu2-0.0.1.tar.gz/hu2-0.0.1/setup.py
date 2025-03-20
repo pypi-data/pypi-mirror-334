ae770d6cfd5678ff139372987159a0a14f48c601
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 22:05:29 +0000

    Add package hu2/0.0.1/hu2-0.0.1.tar.gz

---
+++ packages/hu2/hu2-0.0.1.tar.gz/hu2-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+print(find_packages())
+setup(name='hu2',
+      version='0.0.1',
+      author='huzhe666',
+      author_email='huzhe666@live.com',
+      description='a small tool of python',
+      long_description='<p>no time write the document</p>',
+      packages=find_packages()
+      )
