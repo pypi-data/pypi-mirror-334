083d324e8cc9b4c0d66e775b10b078e1775ae758
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 00:05:32 +0000

    Add package poethepoet/0.33.1/poethepoet-0.33.1-py3-none-any.whl

---
+++ packages/poethepoet/poethepoet-0.33.1-py3-none-any.whl/poethepoet/context.py
@@ -0,0 +1,124 @@
+import re
+from collections.abc import Mapping
+from pathlib import Path
+from typing import TYPE_CHECKING, Any, Optional, Union
+
+if TYPE_CHECKING:
+    from .config import PoeConfig
+    from .env.manager import EnvVarsManager
+    from .executor import PoeExecutor
+    from .ui import PoeUi
+
+
+class RunContext:
+    config: "PoeConfig"
+    ui: "PoeUi"
+    env: "EnvVarsManager"
+    dry: bool
+    poe_active: Optional[str]
+    project_dir: Path
+    multistage: bool = False
+    exec_cache: dict[str, Any]
+    captured_stdout: dict[tuple[str, ...], str]
+
+    def __init__(
+        self,
+        config: "PoeConfig",
+        ui: "PoeUi",
+        env: Mapping[str, str],
+        dry: bool,
+        poe_active: Optional[str],
+        multistage: bool = False,
+        cwd: Optional[Union[Path, str]] = None,
+    ):
+        from .env.manager import EnvVarsManager
+
+        self.config = config
+        self.ui = ui
+        self.project_dir = Path(config.project_dir)
+        self.dry = dry
+        self.poe_active = poe_active
+        self.multistage = multistage
+        self.exec_cache = {}
+        self.captured_stdout = {}
+
+        # Init root EnvVarsManager
+        self.env = EnvVarsManager(self.config, self.ui, base_env=env, cwd=cwd)
+        for config_part in self.config.partitions():
+            self.env.apply_env_config(
+                envfile=config_part.get("envfile", None),
+                config_env=config_part.get("env", None),
+                config_dir=config_part.config_dir,
+                config_working_dir=config_part.cwd,
+            )
+
+    def _get_dep_values(
+        self, used_task_invocations: Mapping[str, tuple[str, ...]]
+    ) -> dict[str, str]:
+        """
+        Get env vars from upstream tasks declared via the uses option.
+        """
+        return {
+            var_name: self.get_task_output(invocation)
+            for var_name, invocation in used_task_invocations.items()
+        }
+
+    def save_task_output(self, invocation: tuple[str, ...], captured_stdout: bytes):
+        """
+        Store the stdout data from a task so that it can be reused by other tasks
+        """
+        try:
+            self.captured_stdout[invocation] = captured_stdout.decode()
+        except UnicodeDecodeError:
+            # Attempt to recover in case a specific encoding is configured
+            io_encoding = self.env.get("PYTHONIOENCODING")
+            if io_encoding:
+                self.captured_stdout[invocation] = captured_stdout.decode(io_encoding)
+            else:
+                raise
+
+    def get_task_output(self, invocation: tuple[str, ...]):
+        """
+        Get the stored stdout data from a task so that it can be reused by other tasks
+
+        New lines are replaced with whitespace similar to how unquoted command
+        interpolation works in bash.
+        """
+        return re.sub(r"\s+", " ", self.captured_stdout[invocation].strip("\r\n"))
+
+    def get_executor(
+        self,
+        invocation: tuple[str, ...],
+        env: "EnvVarsManager",
+        working_dir: Path,
+        *,
+        executor_config: Optional[Mapping[str, str]] = None,
+        capture_stdout: Union[str, bool] = False,
+        resolve_python: bool = False,
+        delegate_dry_run: bool = False,
+    ) -> "PoeExecutor":
+        """
+        Get an Executor object for use with this invocation.
+
+        if delegate_dry_run is set then the task will always be executed and be
+        entrusted to not have any side effects when the dry-run flag is set.
+        """
+
+        from .executor import PoeExecutor
+
+        if not executor_config:
+            if self.ui["executor"]:
+                executor_config = {"type": self.ui["executor"]}
+            else:
+                executor_config = self.config.executor
+
+        return PoeExecutor.get(
+            invocation=invocation,
+            context=self,
+            executor_config=executor_config,
+            env=env,
+            working_dir=working_dir,
+            capture_stdout=capture_stdout,
+            resolve_python=resolve_python,
+            dry=False if delegate_dry_run else self.dry,
+        )
---
+++ packages/poethepoet/poethepoet-0.33.1-py3-none-any.whl/poethepoet/executor/uv.py
@@ -0,0 +1,63 @@
+from collections.abc import Sequence
+from typing import TYPE_CHECKING, Optional
+
+from .base import PoeExecutor
+
+if TYPE_CHECKING:
+    from ..context import RunContext
+
+
+class UvExecutor(PoeExecutor):
+    """
+    A poe task executor implementation that executes inside a uv managed dev
+    environment
+    """
+
+    __key__ = "uv"
+    __options__: dict[str, type] = {}
+
+    @classmethod
+    def works_with_context(cls, context: "RunContext") -> bool:
+        if not context.config.is_uv_project:
+            return False
+        return bool(cls._uv_cmd_from_path())
+
+    def execute(
+        self, cmd: Sequence[str], input: Optional[bytes] = None, use_exec: bool = False
+    ) -> int:
+        """
+        Execute the given cmd as a subprocess inside the uv managed dev environment.
+
+        We simply use `uv run`, which handles the virtualenv and other setup for us.
+        """
+
+        uv_run_options = []
+        if self.context.ui.verbosity > 0:
+            uv_run_options.append("-v")
+        elif self.context.ui.verbosity < 0:
+            uv_run_options.append("-q")
+
+        if self.working_dir:
+            # Explicitly set the working directory and project directory for uv
+            uv_run_options.append(f"--directory={self.working_dir}")
+            uv_run_options.append(f"--project={self.context.config.project_dir}")
+
+        # Run this task with `uv run`
+        return self._execute_cmd(
+            (self._uv_cmd(), "run", *uv_run_options, *cmd),
+            input=input,
+            use_exec=use_exec,
+        )
+
+    @classmethod
+    def _uv_cmd(cls):
+        if from_path := cls._uv_cmd_from_path():
+            return from_path
+
+        return "uv"
+
+    @classmethod
+    def _uv_cmd_from_path(cls):
+        import shutil
+
+        return shutil.which("uv")
