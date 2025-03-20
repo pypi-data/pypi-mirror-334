21da7e437f2d148c617a22520fb09c24400efabc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 19:06:23 +0000

    Add package vptq-aqua/0.0.4/vptq_aqua-0.0.4-cp39-cp39-manylinux1_x86_64.whl

---
+++ packages/vptq-aqua/vptq_aqua-0.0.4-cp39-cp39-manylinux1_x86_64.whl/vptq/__init__.py
@@ -0,0 +1,12 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+# --------------------------------------------------------------------------
+
+import importlib.metadata
+
+from vptq.layers import AutoModelForCausalLM, VQuantLinear
+
+__version__ = importlib.metadata.version("vptq-aqua")
+
+__all__ = ["AutoModelForCausalLM", "VQuantLinear"]
---
+++ packages/vptq-aqua/vptq_aqua-0.0.4-cp39-cp39-manylinux1_x86_64.whl/vptq/layers/__init__.py
@@ -0,0 +1,8 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+# --------------------------------------------------------------------------
+
+from vptq.layers.model_base import AutoModelForCausalLM, VQuantLinear
+
+__all__ = ["AutoModelForCausalLM", "VQuantLinear"]
