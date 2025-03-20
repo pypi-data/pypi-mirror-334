20b2ff95505f6b42d2635cb307ebb29e8e1a924f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 14:05:13 +0000

    Add package pglast/6.16/pglast-6.16-cp311-cp311-win_amd64.whl

---
+++ packages/pglast/pglast-6.16-cp311-cp311-win_amd64.whl/pglast/enums/lockoptions.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# :Project:   pglast -- DO NOT EDIT: automatically extracted from lockoptions.h @ 16-5.2.0-0-gfce106a
+# :Author:    Lele Gaifax <lele@metapensiero.it>
+# :License:   GNU General Public License version 3 or later
+# :Copyright: © 2017-2024 Lele Gaifax
+#
+
+from enum import Enum, IntEnum, IntFlag, auto
+
+try:
+    from enum import StrEnum
+except ImportError:  # pragma: no cover
+    # Python < 3.10
+    class StrEnum(str, Enum):
+        pass
+
+
+class LockClauseStrength(IntEnum):
+    LCS_NONE = 0
+    LCS_FORKEYSHARE = auto()
+    LCS_FORSHARE = auto()
+    LCS_FORNOKEYUPDATE = auto()
+    LCS_FORUPDATE = auto()
+
+class LockTupleMode(IntEnum):
+    LockTupleKeyShare = 0
+    LockTupleShare = auto()
+    LockTupleNoKeyExclusive = auto()
+    LockTupleExclusive = auto()
+
+class LockWaitPolicy(IntEnum):
+    LockWaitBlock = 0
+    LockWaitSkip = auto()
+    LockWaitError = auto()
