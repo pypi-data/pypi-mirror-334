a7a8012bbfeba7ab21f3af65651a0a1628b2dfc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 09:04:02 +0000

    Add package sql-mongo-converter/1.0.1/sql-mongo-converter-1.0.1.tar.gz

---
+++ packages/sql-mongo-converter/sql-mongo-converter-1.0.1.tar.gz/sql-mongo-converter-1.0.1/setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup, find_packages
+
+# Get the directory where setup.py resides
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Read the long description from README.md
+with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="sql-mongo-converter",
+    version="1.0.1",
+    description="Convert SQL queries to MongoDB queries and vice versa.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Son Nguyen",
+    author_email="hoangson091104@gmail.com",
+    url="https://github.com/yourusername/sql-mongo-converter",
+    packages=find_packages(),
+    install_requires=[
+        "sqlparse",  # for basic SQL parsing
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
