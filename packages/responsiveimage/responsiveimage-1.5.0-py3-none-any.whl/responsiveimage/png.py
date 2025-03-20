88d1aa7f862f1d15a37fe8355c879ceaa78133cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 00:05:29 +0000

    Add package responsiveimage/1.5.0/responsiveimage-1.5.0-py3-none-any.whl

---
+++ packages/responsiveimage/responsiveimage-1.5.0-py3-none-any.whl/responsiveimage/png.py
@@ -0,0 +1,31 @@
+'''
+MIT License
+Copyright (c) 2023 Pascal Brand
+
+webp utility functions:
+- save
+'''
+
+from typing import Union
+from PIL import Image
+from . import argsResponsiveImage
+from . import exif as getexif
+
+def save(
+    image:Image.Image,
+    srcFullFilename:str,
+    dstFullFilename:str,
+    exif: Union[Image.Exif, None],
+    epoch:float,
+    args: argsResponsiveImage.argsResponsiveImage) -> None:
+  '''
+  save image as png format, as name dstFullFilename
+  - use exif is not None
+  - args is used to get webp parameters, as args.parameters['webp']
+  - srcFullFilename and epoch are used to keep modification dates
+  '''
+  if exif:
+    image.save(dstFullFilename, optimize=True, format='PNG', exif=exif)
+  else:
+    image.save(dstFullFilename, optimize=True, format='PNG')
+  getexif.updateFilestat(srcFullFilename, dstFullFilename, epoch)
