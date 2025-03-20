173e68235cfd465003a0b897593d8af59f801c2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 00:05:34 +0000

    Add package mgraph-db/1.2.12/mgraph_db-1.2.12-py3-none-any.whl

---
+++ packages/mgraph-db/mgraph_db-1.2.12-py3-none-any.whl/mgraph_db/providers/llms/utils/API__LLM__Groq.py
@@ -0,0 +1,8 @@
+from osbot_utils.helpers.llms.platforms.open_ai.API__LLM__Open_AI import API__LLM__Open_AI
+
+ENV_NAME_GROQ__API_KEY    = "GROQ__API_KEY"
+GROQ__LLM_MODEL__MIXTRAL = 'Mixtral-8x7b-32768'
+
+class API__LLM__Groq(API__LLM__Open_AI):
+    api_url     : str = "https://api.groq.com/openai/v1/chat/completions"
+    api_key_name: str = ENV_NAME_GROQ__API_KEY
