00600ff2a790294f5dc0cb3a329506b992b99e59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 12:04:50 +0000

    Add package fourtest/0.1.4/fourtest-0.1.4.tar.gz

---
+++ packages/fourtest/fourtest-0.1.4.tar.gz/fourtest-0.1.4/fourtest/__init__.py
@@ -0,0 +1,3 @@
+# fourtest/__iniy__.py 
+from .api import hello_world
+from .client import run_client
---
+++ packages/fourtest/fourtest-0.1.4.tar.gz/fourtest-0.1.4/fourtest/client.py
@@ -0,0 +1,11 @@
+# fourtest/client.py 
+
+import time
+
+def run_client():
+    print("Client is starting...")
+    time.sleep(2)  # 2초 대기
+    print("Client is running...")
+    time.sleep(2)
+    print("Client operation completed.")
+    
