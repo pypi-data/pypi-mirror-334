f9c6415d0ca5c60e9bcb307ac3317036056600f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 12:04:34 +0000

    Add package cryptix-proto/0.1/cryptix_proto-0.1.tar.gz

---
+++ packages/cryptix-proto/cryptix_proto-0.1.tar.gz/cryptix_proto-0.1/README.md
@@ -0,0 +1,4 @@
+# VexusProto
+
+VexusProto is an end-to-end encryption chat protocol implemented in Python.
+It uses modern cryptographic algorithms to ensure secure communication.
---
+++ packages/cryptix-proto/cryptix_proto-0.1.tar.gz/cryptix_proto-0.1/setup.py
@@ -0,0 +1,18 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="cryptix-proto",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        'cryptography',
+          'DoubleRatchet',  # Add any dependencies you have here
+    ],
+    description="End-to-end encryption chat protocol",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author="ALfisene Keita",
+    author_email="info@infopeklo.cz",
+    url="https://github.com/certikpolik1/fantomxryptx",
+)
