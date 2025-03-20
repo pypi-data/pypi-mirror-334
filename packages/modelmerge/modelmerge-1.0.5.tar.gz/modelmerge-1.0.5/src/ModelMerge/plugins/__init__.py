a62e3e270bb08be98be52edabd1d77bc59a6d50b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 14:04:55 +0000

    Add package modelmerge/1.0.5/modelmerge-1.0.5.tar.gz

---
+++ packages/modelmerge/modelmerge-1.0.5.tar.gz/modelmerge-1.0.5/src/ModelMerge/plugins/__init__.py
@@ -0,0 +1,21 @@
+from .websearch import *
+from .version import *
+from .today import *
+from .image import *
+from .run_python import *
+from .arXiv import *
+
+from .config import *
+
+__all__ = [
+    'PLUGINS',
+    'generate_image',
+    'get_url_content',
+    'get_version_info',
+    'run_python_script',
+    'function_call_list',
+    'get_search_results',
+    'get_date_time_weekday',
+    'get_tools_result_async',
+    'download_read_arxiv_pdf',
+]
