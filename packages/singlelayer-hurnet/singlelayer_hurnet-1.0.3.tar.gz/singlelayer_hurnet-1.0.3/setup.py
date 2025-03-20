d605e3f2af65e5f64c0ff2ac8c0af087a51a6504
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 06:04:03 +0000

    Add package singlelayer-hurnet/1.0.3/singlelayer_hurnet-1.0.3.tar.gz

---
+++ packages/singlelayer-hurnet/singlelayer_hurnet-1.0.3.tar.gz/singlelayer_hurnet-1.0.3/setup.py
@@ -0,0 +1,26 @@
+##################################################################################################################################################################
+# This is a backpropagation-free Artificial Neural Network algorithm developed by Sapiens Technology®.                                                           #
+# The algorithm is a simplified reduction without hidden layers to an example application of a HurNet-type Neural Network.                                       #
+# HurNet networks are a technological creation of Sapiens Technology® and their distribution or copying without our permission is strictly prohibited.           #
+# We do not permit copying, distribution or back-engineering of this or other versions of the HurNet network or any other code developed by Sapiens Technology®. #
+# We will prosecute anyone who fails to comply with the rules set out here or who discloses any details about this code.                                         #
+##################################################################################################################################################################
+from setuptools import setup, find_packages
+package_name = 'singlelayer-hurnet'
+version = '1.0.3'
+setup(
+    name=package_name,
+    version=version,
+    author='SAPIENS TECHNOLOGY',
+    packages=find_packages(),
+    install_requires=['numpy'],
+    url='https://github.com/',
+    license='Proprietary Software'
+)
+##################################################################################################################################################################
+# This is a backpropagation-free Artificial Neural Network algorithm developed by Sapiens Technology®.                                                           #
+# The algorithm is a simplified reduction without hidden layers to an example application of a HurNet-type Neural Network.                                       #
+# HurNet networks are a technological creation of Sapiens Technology® and their distribution or copying without our permission is strictly prohibited.           #
+# We do not permit copying, distribution or back-engineering of this or other versions of the HurNet network or any other code developed by Sapiens Technology®. #
+# We will prosecute anyone who fails to comply with the rules set out here or who discloses any details about this code.                                         #
+##################################################################################################################################################################
---
+++ packages/singlelayer-hurnet/singlelayer_hurnet-1.0.3.tar.gz/singlelayer_hurnet-1.0.3/singlelayer_hurnet/__init__.py
@@ -0,0 +1,15 @@
+##################################################################################################################################################################
+# This is a backpropagation-free Artificial Neural Network algorithm developed by Sapiens Technology®.                                                           #
+# The algorithm is a simplified reduction without hidden layers to an example application of a HurNet-type Neural Network.                                       #
+# HurNet networks are a technological creation of Sapiens Technology® and their distribution or copying without our permission is strictly prohibited.           #
+# We do not permit copying, distribution or back-engineering of this or other versions of the HurNet network or any other code developed by Sapiens Technology®. #
+# We will prosecute anyone who fails to comply with the rules set out here or who discloses any details about this code.                                         #
+##################################################################################################################################################################
+from .singlelayer_hurnet import *
+##################################################################################################################################################################
+# This is a backpropagation-free Artificial Neural Network algorithm developed by Sapiens Technology®.                                                           #
+# The algorithm is a simplified reduction without hidden layers to an example application of a HurNet-type Neural Network.                                       #
+# HurNet networks are a technological creation of Sapiens Technology® and their distribution or copying without our permission is strictly prohibited.           #
+# We do not permit copying, distribution or back-engineering of this or other versions of the HurNet network or any other code developed by Sapiens Technology®. #
+# We will prosecute anyone who fails to comply with the rules set out here or who discloses any details about this code.                                         #
+##################################################################################################################################################################
