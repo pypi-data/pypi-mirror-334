cc22be1280e6d3bfc83285716e50738483984d15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 11:05:07 +0000

    Add package ez/11.5.7/ez-11.5.7.tar.gz

---
+++ packages/ez/ez-11.5.7.tar.gz/ez-11.5.7/ez/__init__.py
@@ -0,0 +1,6 @@
+from . ez import *
+
+__all__=[
+'JDict', 'TimeStamp', 'Moment', 'GSheet', 'GCal',
+'requests', 'sqlite3', 'json', 'io', 'os', 'sys', 'platform', 'string', 'random', 're', 'datetime', 'tzlocal', 'pd', 'np', 'urlparse', 'BeautifulSoup', 
+]
