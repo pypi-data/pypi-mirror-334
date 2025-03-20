34ecbc6382b155b0b2f38d1cb03bd8e4ad94a916
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 01:30:52 +0000

    Add package cvxpy/1.6.4/cvxpy-1.6.4-cp310-cp310-win_amd64.whl

---
+++ packages/cvxpy/cvxpy-1.6.4-cp310-cp310-win_amd64.whl/cvxpy/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM CVXPY SETUP.PY
+short_version = '1.6.4'
+version = '1.6.4'
+full_version = '1.6.4'
+git_revision = '78b54aa'
+commit_count = '0'
+release = True
+if not release:
+    version = full_version
