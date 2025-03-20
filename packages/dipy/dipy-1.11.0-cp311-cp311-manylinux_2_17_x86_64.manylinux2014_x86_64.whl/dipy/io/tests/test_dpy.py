6a8688aaf842a7e3b59df5fbd0a60e40ce3e401d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 09:04:29 +0000

    Add package dipy/1.11.0/dipy-1.11.0-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/dipy/dipy-1.11.0-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/dipy/io/tests/test_dpy.py
@@ -0,0 +1,33 @@
+from os.path import join as pjoin
+from tempfile import TemporaryDirectory
+
+import numpy as np
+import numpy.testing as npt
+
+from dipy.io.dpy import Dpy, Streamlines
+
+
+def test_dpy():
+    with TemporaryDirectory() as tmpdir:
+        fname = pjoin(tmpdir, "test.bin")
+        dpw = Dpy(fname, mode="w")
+        A = np.ones((5, 3))
+        B = 2 * A.copy()
+        C = 3 * A.copy()
+        dpw.write_track(A)
+        dpw.write_track(B)
+        dpw.write_track(C)
+        dpw.write_tracks(Streamlines([C, B, A]))
+
+        all_tracks = np.ascontiguousarray(np.vstack([A, B, C, C, B, A]))
+        npt.assert_array_equal(all_tracks, dpw.tracks[:])
+        dpw.close()
+
+        dpr = Dpy(fname, mode="r")
+        npt.assert_equal(dpr.version() == "0.0.1", True)
+        T = dpr.read_tracksi([0, 1, 2, 0, 0, 2])
+        T2 = dpr.read_tracks()
+        npt.assert_equal(len(T2), 6)
+        dpr.close()
+        npt.assert_array_equal(A, T[0])
+        npt.assert_array_equal(C, T[5])
