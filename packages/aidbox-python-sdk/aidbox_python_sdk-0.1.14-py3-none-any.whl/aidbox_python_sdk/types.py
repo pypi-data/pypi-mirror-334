5a76591f98aac31daeba78e0f36f551bd87cf495
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 05:03:43 +0000

    Add package aidbox-python-sdk/0.1.14/aidbox_python_sdk-0.1.14-py3-none-any.whl

---
+++ packages/aidbox-python-sdk/aidbox_python_sdk-0.1.14-py3-none-any.whl/aidbox_python_sdk/types.py
@@ -0,0 +1,13 @@
+from typing import Any
+
+from aiohttp import web
+from typing_extensions import TypedDict
+
+SDKOperationRequest = TypedDict(
+    "SDKOperationRequest",
+    {"app": web.Application, "params": dict, "route-params": dict, "headers": dict, "resource": Any},
+)
+
+
+class SDKOperation(TypedDict):
+    pass
