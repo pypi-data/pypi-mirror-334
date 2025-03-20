34201b2855a5a12c23e2a4ddf1826f61436e814b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 05:03:55 +0000

    Add package runtimepy/5.10.0/runtimepy-5.10.0.tar.gz

---
+++ packages/runtimepy/runtimepy-5.10.0.tar.gz/runtimepy-5.10.0/runtimepy/__init__.py
@@ -0,0 +1,17 @@
+# =====================================
+# generator=datazen
+# version=3.1.4
+# hash=a9a53fbeb974140d3611fecadbce5da4
+# =====================================
+
+"""
+Useful defaults and other package metadata.
+"""
+
+DESCRIPTION = "A framework for implementing Python services."
+PKG_NAME = "runtimepy"
+VERSION = "5.10.0"
+
+# runtimepy-specific content.
+METRICS_NAME = "metrics"
+DEFAULT_EXT = "yaml"
