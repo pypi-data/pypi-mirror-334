3dfdcbfa96faec5dd515b3481deaa0324ab7b087
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 05:03:45 +0000

    Add package isoddeven/0.4.0/isoddeven-0.4.0.tar.gz

---
+++ packages/isoddeven/isoddeven-0.4.0.tar.gz/isoddeven-0.4.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='isoddeven',
+    version='0.4.0',
+    description='A Python package to check if a number is odd or even.',
+    author='Nilay Sarma',
+    packages=find_packages(),
+    install_requires=[],
+    license="MIT"
+)
