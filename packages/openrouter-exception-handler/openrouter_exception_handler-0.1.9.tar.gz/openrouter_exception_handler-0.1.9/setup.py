8b9254576da4399d098d514dcdacac59ddbf6975
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 17:05:32 +0000

    Add package openrouter-exception-handler/0.1.9/openrouter_exception_handler-0.1.9.tar.gz

---
+++ packages/openrouter-exception-handler/openrouter_exception_handler-0.1.9.tar.gz/openrouter_exception_handler-0.1.9/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='openrouter_exception_handler',
+    version='0.1.9',
+    packages=find_packages(),
+    install_requires=[
+        'requests'
+    ],
+    author='Dannybombastic',
+    description='handler for exceptions in python with openrouter.ai',
+    url='https://github.com/tu_usuario/openrouter_exception_handler',
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
