010f448bb7e39f05f1a3a6a4a9cc53ff2a966974
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 17:05:01 +0000

    Add package async-tiff/0.1.0b2/async_tiff-0.1.0b2-cp39-cp39-musllinux_1_2_x86_64.whl

---
+++ packages/async-tiff/async_tiff-0.1.0b2-cp39-cp39-musllinux_1_2_x86_64.whl/async_tiff/_async_tiff.pyi
@@ -0,0 +1,7 @@
+from ._decoder import Decoder as Decoder
+from ._decoder import DecoderRegistry as DecoderRegistry
+from ._geo import GeoKeyDirectory as GeoKeyDirectory
+from ._ifd import ImageFileDirectory as ImageFileDirectory
+from ._thread_pool import ThreadPool as ThreadPool
+from ._tiff import TIFF as TIFF
+from ._tile import Tile as Tile
