172e4a5b3cb7a06a6ab2e42b7be0989e3568fdad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 19:05:25 +0000

    Add package reduced-3dgs/1.3.4/reduced_3dgs-1.3.4-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/reduced-3dgs/reduced_3dgs-1.3.4-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/reduced_3dgs/quantization/__init__.py
@@ -0,0 +1,4 @@
+from .abc import AbstractQuantizer, QuantizeTrainerWrapper
+from .quantizer import VectorQuantizer
+from .exclude_zeros import ExcludeZeroQuantizer
+from .wrapper import VectorQuantizeTrainerWrapper, BaseVectorQuantizeTrainer
