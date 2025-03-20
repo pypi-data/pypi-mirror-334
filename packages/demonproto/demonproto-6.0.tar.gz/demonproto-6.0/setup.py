1437c8940ecf1320bef166252ce8f905760ff975
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 16:05:10 +0000

    Add package demonproto/6.0/demonproto-6.0.tar.gz

---
+++ packages/demonproto/demonproto-6.0.tar.gz/demonproto-6.0/setup.py
@@ -0,0 +1,18 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="demonproto",
+    version="6.0",
+    packages=find_packages(),
+    install_requires=[
+        'cryptography',
+        'DoubleRatchet',
+    ],
+    description="End-to-end encryption chat protocol",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author="ALfisene Keita",
+    author_email="info@infopeklo.cz",
+    url="https://github.com/certikpolik1/thecryptoxprotox",
+)
