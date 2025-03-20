9bc33f06a89b84fe21fd786e2728762e168fd882
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 08:04:16 +0000

    Add package aitop/0.6.0/aitop-0.6.0-py3-none-any.whl

---
+++ packages/aitop/aitop-0.6.0-py3-none-any.whl/aitop/version.py
@@ -0,0 +1,5 @@
+"""AITop version information."""
+
+__version__ = "0.6.0"  # Bumped to 0.6.0 with latest improvements
+__author__ = "Alexander Warth"
+__copyright__ = f"Copyright (c) 2025 {__author__}"
