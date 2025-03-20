a56c5e79bc93f55c2528b07fb37d8c2984b0db03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 22:05:26 +0000

    Add package jenkinsapi/0.3.14/jenkinsapi-0.3.14-py3-none-any.whl

---
+++ packages/jenkinsapi/jenkinsapi-0.3.14-py3-none-any.whl/jenkinsapi/command_line/jenkinsapi_version.py
@@ -0,0 +1,13 @@
+""" jenkinsapi.command_line.jenkinsapi_version
+"""
+
+import jenkinsapi
+import sys
+
+
+def main():
+    sys.stdout.write(jenkinsapi.__version__)
+
+
+if __name__ == "__main__":
+    main()
