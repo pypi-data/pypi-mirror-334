651ccccedbc608f71bb14084294437ae2c47545d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 12:05:13 +0000

    Add package odoo-health-report-Mar14/0.1.6/odoo_health_report_mar14-0.1.6.tar.gz

---
+++ packages/odoo-health-report-Mar14/odoo_health_report_mar14-0.1.6.tar.gz/odoo_health_report_mar14-0.1.6/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="odoo-health-report-Mar14",
+    version="0.1.6",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "Django>=3.2.0",
+        # Add other dependencies from your requirements.txt
+    ],
+    entry_points={
+        "console_scripts": [
+            "odoo-health-report=health_report.cli:main",
+        ],
+    },
+    python_requires=">=3.6",
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A Django application for monitoring Odoo server metrics",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/odoo-health-report",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+    ],
+)
