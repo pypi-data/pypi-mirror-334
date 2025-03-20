c3a06b553748c7b9d45337ce2a14076a1753dab1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 23:06:06 +0000

    Add package Canaille/0.0.65/canaille-0.0.65.tar.gz

---
+++ packages/Canaille/canaille-0.0.65.tar.gz/canaille-0.0.65/doc/tutorial/databases.rst
@@ -0,0 +1,179 @@
+Databases
+#########
+
+Canaille can read and save data in different databases.
+This page presents the different database backends and their specificities:
+
+Memory
+======
+
+Canaille comes with a lightweight inmemory backend by default.
+It is used when no other backend has been configured.
+
+This backend is only for test purpose and should not be used in production environments.
+
+SQL
+===
+
+Canaille can use any database supported by `SQLAlchemy <https://www.sqlalchemy.org/>`_, such as
+sqlite, postgresql or mariadb.
+
+Configuration
+-------------
+
+It is used when the ``CANAILLE_SQL`` configuration parameter is defined. For instance:
+
+.. code-block:: toml
+    :caption: config.toml
+
+    [CANAILLE_SQL]
+    SQL_DATABASE_URI = "postgresql://user:password@localhost/database"
+
+You can find more details on the SQL configuration in the :class:`dedicated section <canaille.backends.sql.configuration.SQLSettings>`.
+
+Migrations
+----------
+
+By default, migrations are applied when you run the web application.
+You can disable this behavior with the :attr:`~canaille.backends.sql.configuration.SQLSettings.AUTO_MIGRATE` setting.
+Migrations are not automatically applied with the use of the CLI though.
+
+Migrations are done with :doc:`flask-alembic <flask-alembic:use>`, that provides a dedicated CLI to manually tune migrations.
+You can check the :doc:`flask-alembic documentation <flask-alembic:index>` and the ``canaille db`` command line if you are in trouble.
+
+LDAP
+====
+
+Canaille can use OpenLDAP as its main database.
+It is used when the ``CANAILLE_LDAP`` configuration parameter is defined. For instance:
+
+.. code-block:: toml
+    :caption: config.toml
+
+    [CANAILLE_LDAP]
+    URI = "ldap://ldap"
+    ROOT_DN = "dc=example,dc=org"
+    BIND_DN = "cn=admin,dc=example,dc=org"
+    BIND_PW = "very-secret-password"
+
+    USER_BASE = "ou=users,dc=example,dc=org"
+    USER_CLASS = "inetOrgPerson"
+
+    GROUP_BASE = "ou=groups,dc=example,dc=org"
+
+If you want to use TOTP/HOTP authentication, you will need to add the ``oathHOTPToken`` class to the user:
+
+.. code-block:: toml
+
+   USER_CLASS = ["inetOrgPerson", "oathHOTPToken"]
+
+You can find more details on the LDAP configuration in the :class:`dedicated section <canaille.backends.ldap.configuration.LDAPSettings>`.
+
+.. note ::
+   Currently, only the ``inetOrgPerson``, ``oathHOTPToken`` and ``groupOfNames`` schemas have been tested.
+   If you want to use different schemas or LDAP servers, adaptations may be needed.
+   Patches are welcome.
+
+OpenLDAP overlays integration
+-----------------------------
+
+Canaille can integrate with several OpenLDAP overlays:
+
+memberof / refint
+~~~~~~~~~~~~~~~~~
+
+`memberof <https://www.openldap.org/doc/admin26/overlays.html#Reverse%20Group%20Membership%20Maintenance>`_
+and `refint <https://www.openldap.org/doc/admin26/overlays.html#Referential%20Integrity>`_
+overlays are needed for the Canaille group membership to work correctly.
+
+Here is a configuration example compatible with canaille:
+
+.. literalinclude :: ../..//demo/ldif/memberof-config.ldif
+   :language: ldif
+   :caption: memberof-config.ldif
+
+.. literalinclude :: ../..//demo/ldif/refint-config.ldif
+   :language: ldif
+   :caption: refint-config.ldif
+
+You can adapt and load those configuration files with:
+
+.. code-block:: bash
+
+    # Adapt those commands according to your setup
+    sudo ldapadd -Q -H ldapi:/// -Y EXTERNAL -f memberof-config.ldif
+    sudo ldapadd -Q -H ldapi:/// -Y EXTERNAL -f refint-config.ldif
+
+ppolicy
+~~~~~~~
+
+If the `ppolicy <https://www.ietf.org/archive/id/draft-behera-ldap-password-policy-11.html>`_ overlay is configured and the ``pwdEndTime`` attribute is available (since OpenLDAP 2.6), then account locking support will be enabled in canaille. To allow users to manage account expiration, they need to have a *write* permission on the :attr:`~canaille.core.models.User.lock_date` attribute.
+
+Here is a configuration example compatible with canaille:
+
+.. literalinclude :: ../../demo/ldif/ppolicy-config.ldif
+   :language: ldif
+   :caption: ppolicy-config.ldif
+
+.. literalinclude :: ../../demo/ldif/ppolicy.ldif
+   :language: ldif
+   :caption: ppolicy.ldif
+
+You can adapt and load those configuration files with:
+
+.. code-block:: bash
+
+    # Adapt those commands according to your setup
+    sudo ldapadd -Q -H ldapi:/// -Y EXTERNAL -f ppolicy-config.ldif
+    sudo ldapadd -Q -H ldapi:/// -Y EXTERNAL -f ppolicy.ldif
+
+otp
+~~~~~~~
+
+If the `otp <https://www.openldap.org/software/man.cgi?query=slapo-otp>`_ overlay is configured, you will be able to add one-time password authentication in canaille.
+
+Here is a configuration example compatible with canaille:
+
+.. literalinclude :: ../../demo/ldif/otp-config.ldif
+   :language: ldif
+   :caption: otp-config.ldif
+
+You can adapt and load this configuration file with:
+
+.. code-block:: bash
+
+    # Adapt this command according to your setup
+    sudo ldapadd -Q -H ldapi:/// -Y EXTERNAL -f otp-config.ldif
+
+You will also need to add the ``oathHOTPToken`` class to the user:
+
+.. code-block:: toml
+    :caption: config.toml
+
+    [CANAILLE_LDAP]
+    ...
+    USER_CLASS = ["inetOrgPerson", "oathHOTPToken"]
+
+Dump and restore
+================
+
+Backups
+-------
+
+The :ref:`cli_dump` and :ref:`cli_restore` commands can be used to dump all the Canaille objects, or load them in the current database.
+Those commands can be used for backuping Canaille data.
+
+.. _database_migration:
+
+Migration
+---------
+
+The dump format is the same whatever database backend is used, and thus it can be used to migrate from a database backend to another.
+To achieve a migration, you need to have two configuration files prepared for the source database and the destination database.
+For instance, if you want to migrate from a LDAP database to a SQL database, you can execute something like this:
+
+.. code-block:: console
+    :caption: Migrating data from a LDAP directory to a SQL database
+
+    env CONFIG=sql-config.toml canaille install
+    env CONFIG=ldap-config.toml canaille dump | env CONFIG=sql-config.toml canaille restore
---
+++ packages/Canaille/canaille-0.0.65.tar.gz/canaille-0.0.65/tests/oidc/test_dynamic_client_registration_management.py
@@ -0,0 +1,188 @@
+import json
+import warnings
+from datetime import datetime
+
+from joserfc.jwk import KeySet
+
+from canaille.app import models
+
+
+def test_get(testclient, backend, client, user, client_jwks):
+    public_key, _ = client_jwks
+    key_set = KeySet([public_key]).as_dict()
+    assert not testclient.app.config["CANAILLE_OIDC"].get(
+        "DYNAMIC_CLIENT_REGISTRATION_OPEN"
+    )
+    testclient.app.config["CANAILLE_OIDC"]["DYNAMIC_CLIENT_REGISTRATION_TOKENS"] = [
+        "static-token"
+    ]
+
+    headers = {"Authorization": "Bearer static-token"}
+    res = testclient.get(
+        f"/oauth/register/{client.client_id}", headers=headers, status=200
+    )
+    assert res.json == {
+        "client_id": client.client_id,
+        "client_secret": client.client_secret,
+        "client_id_issued_at": int(datetime.timestamp(client.client_id_issued_at)),
+        "client_secret_expires_at": 0,
+        "redirect_uris": [
+            "https://client.test/redirect1",
+            "https://client.test/redirect2",
+        ],
+        "registration_access_token": "static-token",
+        "registration_client_uri": f"http://canaille.test/oauth/register/{client.client_id}",
+        "token_endpoint_auth_method": "client_secret_basic",
+        "grant_types": [
+            "password",
+            "authorization_code",
+            "implicit",
+            "hybrid",
+            "refresh_token",
+            "client_credentials",
+            "urn:ietf:params:oauth:grant-type:jwt-bearer",
+        ],
+        "response_types": ["code", "token", "id_token"],
+        "client_name": "Some client",
+        "client_uri": "https://client.test",
+        "logo_uri": "https://client.test/logo.webp",
+        "scope": "openid email profile groups address phone",
+        "contacts": ["contact@mydomain.test"],
+        "tos_uri": "https://client.test/tos",
+        "policy_uri": "https://client.test/policy",
+        "jwks": json.dumps(key_set),
+        "jwks_uri": None,
+        "software_id": None,
+        "software_version": None,
+    }
+
+
+def test_update(testclient, backend, client, user, client_jwks):
+    public_key, _ = client_jwks
+    key_set = KeySet([public_key]).as_dict()
+    assert not testclient.app.config["CANAILLE_OIDC"].get(
+        "DYNAMIC_CLIENT_REGISTRATION_OPEN"
+    )
+    testclient.app.config["CANAILLE_OIDC"]["DYNAMIC_CLIENT_REGISTRATION_TOKENS"] = [
+        "static-token"
+    ]
+
+    assert client.redirect_uris != ["https://newname.example.test/callback"]
+    assert client.token_endpoint_auth_method != "none"
+    assert client.grant_types != ["refresh_token"]
+    assert client.response_types != ["code", "token"]
+    assert client.client_name != "new name"
+    assert client.client_uri != "https://newname.example.test"
+    assert client.logo_uri != "https://newname.example.test/logo.webp"
+    assert client.scope != ["openid", "profile", "email"]
+    assert client.contacts != ["newcontact@example.test"]
+    assert client.tos_uri != "https://newname.example.test/tos"
+    assert client.policy_uri != "https://newname.example.test/policy"
+    assert client.jwks_uri != "https://newname.example.test/my_public_keys.jwks"
+    assert client.software_id != "new_software_id"
+    assert client.software_version != "3.14"
+
+    payload = {
+        "client_id": client.client_id,
+        "redirect_uris": ["https://newname.example.test/callback"],
+        "token_endpoint_auth_method": "none",
+        "grant_types": ["refresh_token"],
+        "response_types": ["code", "token"],
+        "client_name": "new name",
+        "client_uri": "https://newname.example.test",
+        "logo_uri": "https://newname.example.test/logo.webp",
+        "scope": "openid profile email",
+        "contacts": ["newcontact@example.test"],
+        "tos_uri": "https://newname.example.test/tos",
+        "policy_uri": "https://newname.example.test/policy",
+        "jwks_uri": "https://newname.example.test/my_public_keys.jwks",
+        "software_id": "new_software_id",
+        "software_version": "3.14",
+    }
+
+    headers = {"Authorization": "Bearer static-token"}
+    res = testclient.put_json(
+        f"/oauth/register/{client.client_id}", payload, headers=headers, status=200
+    )
+    client = backend.get(models.Client, client_id=res.json["client_id"])
+
+    assert res.json == {
+        "client_id": client.client_id,
+        "client_secret": client.client_secret,
+        "client_id_issued_at": int(datetime.timestamp(client.client_id_issued_at)),
+        "client_secret_expires_at": 0,
+        "redirect_uris": ["https://newname.example.test/callback"],
+        "registration_access_token": "static-token",
+        "registration_client_uri": f"http://canaille.test/oauth/register/{client.client_id}",
+        "token_endpoint_auth_method": "none",
+        "grant_types": ["refresh_token"],
+        "response_types": ["code", "token"],
+        "client_name": "new name",
+        "client_uri": "https://newname.example.test",
+        "logo_uri": "https://newname.example.test/logo.webp",
+        "scope": "openid profile email",
+        "contacts": ["newcontact@example.test"],
+        "tos_uri": "https://newname.example.test/tos",
+        "policy_uri": "https://newname.example.test/policy",
+        "jwks": json.dumps(key_set),
+        "jwks_uri": "https://newname.example.test/my_public_keys.jwks",
+        "software_id": "new_software_id",
+        "software_version": "3.14",
+    }
+
+    assert client.redirect_uris == ["https://newname.example.test/callback"]
+    assert client.token_endpoint_auth_method == "none"
+    assert client.grant_types == ["refresh_token"]
+    assert client.response_types == ["code", "token"]
+    assert client.client_name == "new name"
+    assert client.client_uri == "https://newname.example.test"
+    assert client.logo_uri == "https://newname.example.test/logo.webp"
+    assert client.scope == ["openid", "profile", "email"]
+    assert client.contacts == ["newcontact@example.test"]
+    assert client.tos_uri == "https://newname.example.test/tos"
+    assert client.policy_uri == "https://newname.example.test/policy"
+    assert client.jwks_uri == "https://newname.example.test/my_public_keys.jwks"
+    assert client.software_id == "new_software_id"
+    assert client.software_version == "3.14"
+
+
+def test_delete(testclient, backend, user):
+    assert not testclient.app.config["CANAILLE_OIDC"].get(
+        "DYNAMIC_CLIENT_REGISTRATION_OPEN"
+    )
+    testclient.app.config["CANAILLE_OIDC"]["DYNAMIC_CLIENT_REGISTRATION_TOKENS"] = [
+        "static-token"
+    ]
+
+    client = models.Client(client_id="foobar", client_name="Some client")
+    backend.save(client)
+
+    headers = {"Authorization": "Bearer static-token"}
+    with warnings.catch_warnings(record=True):
+        testclient.delete(
+            f"/oauth/register/{client.client_id}", headers=headers, status=204
+        )
+    assert not backend.get(models.Client, client_id=client.client_id)
+
+
+def test_invalid_client(testclient, backend, user):
+    assert not testclient.app.config["CANAILLE_OIDC"].get(
+        "DYNAMIC_CLIENT_REGISTRATION_OPEN"
+    )
+    testclient.app.config["CANAILLE_OIDC"]["DYNAMIC_CLIENT_REGISTRATION_TOKENS"] = [
+        "static-token"
+    ]
+
+    payload = {
+        "client_id": "invalid-client-id",
+        "redirect_uris": ["https://newname.example.test/callback"],
+    }
+
+    headers = {"Authorization": "Bearer static-token"}
+    res = testclient.put_json(
+        "/oauth/register/invalid-client-id", payload, headers=headers, status=401
+    )
+    assert res.json == {
+        "error": "invalid_client",
+        "error_description": "The client does not exist on this server.",
+    }
