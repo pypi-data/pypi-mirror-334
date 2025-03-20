9df920d769c131cdc477260c50ef61e7ad7d0b53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 01:31:04 +0000

    Add package zuspec-sv/0.1.0.13878536674rc0/zuspec_sv-0.1.0.13878536674rc0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/zuspec-sv/zuspec_sv-0.1.0.13878536674rc0-cp38-cp38-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/zsp_sv/share/include/zsp/sv/zsp_sv/action_constraint_c.svh
@@ -0,0 +1,35 @@
+/*
+ * action_constraint_c.svh
+ *
+ * Copyright 2023 Matthew Ballance and Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may 
+ * not use this file except in compliance with the License.  
+ * You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ *
+ * Created on:
+ *     Author:
+ */
+class action_constraint_base_c;
+    rand bit        valid;
+endclass
+
+class action_constraint_c #(type Ta=action_c) extends action_constraint_base_c;
+    rand Ta         action;
+
+    function new(Ta action);
+        this.action = action;
+    endfunction
+
+endclass
+
+// Define class between target <action> and parent <this>
+
