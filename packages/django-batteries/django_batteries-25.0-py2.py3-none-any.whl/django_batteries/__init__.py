258fff2a2760700480130d91712c02269766d45c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 23:05:49 +0000

    Add package django-batteries/25.0/django_batteries-25.0-py2.py3-none-any.whl

---
+++ packages/django-batteries/django_batteries-25.0-py2.py3-none-any.whl/django_batteries/__init__.py
@@ -0,0 +1,13 @@
+"""
+================
+Django Batteries
+================
+
+A proof-of-concept for how we might package more with Django, outside the main
+``django/django`` repo.
+
+This is currently an initial holding release merely to setup the packaging.
+
+"""
+
+__version__ = "25.0"
