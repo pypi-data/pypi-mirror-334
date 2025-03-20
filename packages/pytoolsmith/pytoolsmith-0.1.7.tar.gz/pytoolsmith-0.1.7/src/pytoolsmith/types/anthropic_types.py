cdee73199cbb2e689665f5d344c1ca41efcad7a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 17:05:16 +0000

    Add package pytoolsmith/0.1.7/pytoolsmith-0.1.7.tar.gz

---
+++ packages/pytoolsmith/pytoolsmith-0.1.7.tar.gz/pytoolsmith-0.1.7/src/pytoolsmith/types/anthropic_types.py
@@ -0,0 +1,24 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from dataclasses import dataclass
+from typing import Literal
+
+
+@dataclass
+class AnthropicCacheControlParam:
+    type: Literal["ephemeral"]
+
+
+@dataclass
+class AnthropicInputSchema:
+    type: Literal["object"]
+    properties: dict
+    """The input properties"""
+
+
+@dataclass
+class AnthropicToolParam:
+    input_schema: AnthropicInputSchema
+    name: str
+    description: str
+    cache_control: AnthropicCacheControlParam | None = None
