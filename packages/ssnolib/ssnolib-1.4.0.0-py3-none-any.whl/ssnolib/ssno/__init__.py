f32cf713fbec143ecf29c33f85d1ade19cd48cb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 07:03:58 +0000

    Add package ssnolib/1.4.0.0/ssnolib-1.4.0.0-py3-none-any.whl

---
+++ packages/ssnolib/ssnolib-1.4.0.0-py3-none-any.whl/ssnolib/ssno/__init__.py
@@ -0,0 +1,15 @@
+from .standard_name import StandardName, ScalarStandardName, VectorStandardName
+from .standard_name_table import StandardNameTable, Qualification, VectorQualification, Transformation, Character, \
+    parse_table, AgentRole, StandardNameModification
+
+__all__ = ('StandardNameTable',
+           'Qualification',
+           'VectorQualification',
+           'ScalarStandardName',
+           'VectorStandardName',
+           'Transformation',
+           'StandardName',
+           'Character',
+           'AgentRole',
+           'StandardNameModification'
+           )
