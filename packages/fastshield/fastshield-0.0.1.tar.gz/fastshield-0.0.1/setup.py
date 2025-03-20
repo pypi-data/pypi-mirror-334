58297fb61851ca4f2fb285a0c037a6fa4f87628f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 07:04:04 +0000

    Add package fastshield/0.0.1/fastshield-0.0.1.tar.gz

---
+++ packages/fastshield/fastshield-0.0.1.tar.gz/fastshield-0.0.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+  with open('README.md', 'r') as f:
+    return f.read()
+
+
+setup(
+  name='fastshield',
+  version='0.0.1',
+  author='ilpdakz',
+  author_email='khasanknyazev81@gmail.com',
+  description='library to protect your fastapi applications',
+  long_description=readme(),
+  long_description_content_type='text/markdown',
+  url='https://github.com/knyazevi81/fastshield',
+  packages=find_packages(),
+  install_requires=['requests>=2.25.1'],
+  classifiers=[
+    'Programming Language :: Python :: 3.11',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent'
+  ],
+  keywords='files speedfiles ',
+  project_urls={
+    'GitHub': 'https://github.com/knyazevi81'
+  },
+  python_requires='>=3.6'
+)
