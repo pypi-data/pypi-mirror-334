74f4cb368a97ab3ac6843b0201f65afa364a455a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 22:04:53 +0000

    Add package neurequa/0.1.1.5/neurequa-0.1.1.5-py2.py3-none-any.whl

---
+++ packages/neurequa/neurequa-0.1.1.5-py2.py3-none-any.whl/NEUREQUA/__init__.py
@@ -0,0 +1,10 @@
+"""
+NeuReQua
+
+Python library to monitor your signal quality
+"""
+
+__version__ = "0.1.0"
+__author__ = 'Vincent Dornier'
+__credits__ = 'Centre National de la Recherche Scientifique (CNRS)'
+
