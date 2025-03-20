e1feff4e235837df5e952ce9dadb96be45668eda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 01:30:59 +0000

    Add package jessilver-django-seed/1.0.0/jessilver_django_seed-1.0.0.tar.gz

---
+++ packages/jessilver-django-seed/jessilver_django_seed-1.0.0.tar.gz/jessilver_django_seed-1.0.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jessilver_django_seed',
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT License',
+    description='A library to facilitate the creation of fake data (seeds) in Django projects.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/jessilver/django_seed',
+    author='Jesse Silva',
+    author_email='jesse1eliseu@gmail.com',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Framework :: Django',
+    ],
+    install_requires=[
+        'Django>=5.1',
+    ],
+)
