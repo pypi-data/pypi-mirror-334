17628de558c3f010836fbb708718c34413177aa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 12:05:57 +0000

    Add package torchrunx/0.3.1/torchrunx-0.3.1.tar.gz

---
+++ packages/torchrunx/torchrunx-0.3.1.tar.gz/torchrunx-0.3.1/src/torchrunx/__main__.py
@@ -0,0 +1,25 @@
+"""CLI entrypoint used for starting agents on different nodes."""
+
+from argparse import ArgumentParser
+
+from .agent import main
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--launcher-hostname", type=str)
+    parser.add_argument("--launcher-port", type=int)
+    parser.add_argument("--logger-port", type=int)
+    parser.add_argument("--world-size", type=int)
+    parser.add_argument("--rank", type=int)
+    parser.add_argument("--hostname", type=str)
+    args = parser.parse_args()
+
+    main(
+        launcher_hostname=args.launcher_hostname,
+        launcher_port=args.launcher_port,
+        world_size=args.world_size,
+        rank=args.rank,
+        logger_hostname=args.launcher_hostname,
+        logger_port=args.logger_port,
+        hostname=args.hostname,
+    )
