86f47b660cb49185db628af562a2370d9640fca4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 23:05:34 +0000

    Add package sionna-rt/1.0.0/sionna_rt-1.0.0-py3-none-any.whl

---
+++ packages/sionna-rt/sionna_rt-1.0.0-py3-none-any.whl/sionna/__init__.py
@@ -0,0 +1,9 @@
+#
+# SPDX-FileCopyrightText: Copyright (c) 2021-2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+"""Sionna Library with Sionna RT"""
+
+__version__ = "1.0.0"
+
+from . import rt
