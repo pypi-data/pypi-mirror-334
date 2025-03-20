2eb61f34fc5e8e2d2e3fca60c8937bc6c495b54b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 17:05:11 +0000

    Add package codegen/0.51.4/codegen-0.51.4-cp313-cp313-manylinux_2_17_aarch64.manylinux2014_aarch64.manylinux_2_34_aarch64.whl

---
+++ packages/codegen/codegen-0.51.4-cp313-cp313-manylinux_2_17_aarch64.manylinux2014_aarch64.manylinux_2_34_aarch64.whl/codegen/git/configs/constants.py
@@ -0,0 +1,5 @@
+"""Git related constants"""
+
+CODEGEN_BOT_NAME = "codegen-bot"
+CODEGEN_BOT_EMAIL = "team+codegenbot@codegen.sh"
+CODEOWNERS_FILEPATHS = [".github/CODEOWNERS", "CODEOWNERS", "docs/CODEOWNERS"]
