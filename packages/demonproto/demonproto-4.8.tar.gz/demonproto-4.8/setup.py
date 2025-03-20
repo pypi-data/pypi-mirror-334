bafde619097f4a825f802df1c44b18142b0843be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 21:04:42 +0000

    Add package demonproto/4.8/demonproto-4.8.tar.gz

---
+++ packages/demonproto/demonproto-4.8.tar.gz/demonproto-4.8/setup.py
@@ -0,0 +1,17 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="demonproto",
+    version="4.8",
+    packages=find_packages(),
+    install_requires=[
+        'cryptography',
+    ],
+    description="End-to-end encryption chat protocol",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author="ALfisene Keita",
+    author_email="info@infopeklo.cz",
+    url="https://github.com/certikpolik1/thecryptoxprotox",
+)
