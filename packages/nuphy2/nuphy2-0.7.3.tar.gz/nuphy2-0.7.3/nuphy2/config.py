fe1718cd2fbab2effe4e2cfbd80123fd7795ab9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 23:05:46 +0000

    Add package nuphy2/0.7.3/nuphy2-0.7.3.tar.gz

---
+++ packages/nuphy2/nuphy2-0.7.3.tar.gz/nuphy2-0.7.3/nuphy2/config.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import sys # print stderr
+
+from fire import Fire
+
+from fire import Fire
+from nuphy2.version import __version__
+
+DEBUG = False
+
+def main():
+    print(f"i... config")
+
+if __name__ == "__main__":
+    Fire(main)
