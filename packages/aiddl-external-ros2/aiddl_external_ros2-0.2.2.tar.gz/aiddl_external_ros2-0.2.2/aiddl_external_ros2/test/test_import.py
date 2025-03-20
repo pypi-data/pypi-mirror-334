15a430566e5560c00c8e3a980db74046680ec3c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 22:05:08 +0000

    Add package aiddl-external-ros2/0.2.2/aiddl_external_ros2-0.2.2.tar.gz

---
+++ packages/aiddl-external-ros2/aiddl_external_ros2-0.2.2.tar.gz/aiddl_external_ros2-0.2.2/aiddl_external_ros2/test/test_import.py
@@ -0,0 +1,11 @@
+
+
+class TestImport:
+    def test_import_point_converter(self):
+        from aiddl_external_ros2.msg.geometry import PointConverter
+        assert True
+
+    def test_import_float_converter(self):
+        import aiddl_external_ros2.msg.std
+        assert True
+
