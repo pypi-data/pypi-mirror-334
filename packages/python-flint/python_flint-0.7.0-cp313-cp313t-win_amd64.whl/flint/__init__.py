c33e73ead065aa1a2f76b74382d3001bda49819a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 09:05:14 +0000

    Add package python-flint/0.7.0/python_flint-0.7.0-cp313-cp313t-win_amd64.whl

---
+++ packages/python-flint/python_flint-0.7.0-cp313-cp313t-win_amd64.whl/flint/__init__.py
@@ -0,0 +1,62 @@
+"""""" # start delvewheel patch
+def _delvewheel_patch_1_10_0():
+    import os
+    if os.path.isdir(libs_dir := os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'python_flint.libs'))):
+        os.add_dll_directory(libs_dir)
+
+
+_delvewheel_patch_1_10_0()
+del _delvewheel_patch_1_10_0
+# end delvewheel patch
+
+from .pyflint import *
+
+from .types.fmpz import *
+from .types.fmpz_poly import *
+from .types.fmpz_mat import *
+from .types.fmpz_series import *
+from .types.fmpz_vec import fmpz_vec
+
+from .types.fmpq import *
+from .types.fmpq_poly import *
+from .types.fmpq_mat import *
+from .types.fmpq_series import *
+from .types.fmpq_vec import fmpq_vec
+
+from .types.nmod import *
+from .types.nmod_poly import *
+from .types.nmod_mpoly import nmod_mpoly_ctx, nmod_mpoly, nmod_mpoly_vec
+from .types.nmod_mat import *
+from .types.nmod_series import *
+
+from .types.fmpz_mpoly import fmpz_mpoly_ctx, fmpz_mpoly, fmpz_mpoly_vec
+from .types.fmpz_mod import *
+from .types.fmpz_mod_poly import *
+from .types.fmpz_mod_mpoly import fmpz_mod_mpoly_ctx, fmpz_mod_mpoly, fmpz_mod_mpoly_vec
+from .types.fmpz_mod_mat import fmpz_mod_mat
+
+from .types.fmpq_mpoly import fmpq_mpoly_ctx, fmpq_mpoly, fmpq_mpoly_vec
+
+from .types.fq_default import *
+from .types.fq_default_poly import *
+
+from .types.arf import *
+from .types.arb import *
+from .types.arb_poly import *
+from .types.arb_mat import *
+from .types.arb_series import *
+from .types.acb import *
+from .types.acb_poly import *
+from .types.acb_mat import *
+from .types.acb_series import *
+
+from .types.dirichlet import *
+from .functions.showgood import good, showgood
+
+from .flint_base.flint_base import (
+    FLINT_VERSION as __FLINT_VERSION__,
+    FLINT_RELEASE as __FLINT_RELEASE__,
+    Ordering,
+)
+
+__version__ = '0.7.0'
