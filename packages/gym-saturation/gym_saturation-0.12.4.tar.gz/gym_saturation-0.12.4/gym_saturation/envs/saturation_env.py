65e286e921254b7eea0be72710d0545b952a1edb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 08:04:43 +0000

    Add package gym-saturation/0.12.4/gym_saturation-0.12.4.tar.gz

---
+++ packages/gym-saturation/gym_saturation-0.12.4.tar.gz/gym_saturation-0.12.4/gym_saturation/envs/saturation_env.py
@@ -0,0 +1,211 @@
+# Copyright 2021-2025 Boris Shminke
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# noqa: D205, D400
+"""
+Saturation Environment
+=======================
+"""
+import random
+from abc import abstractmethod
+from typing import Any, Optional
+
+import numpy as np
+from gymnasium import Env, spaces
+from gymnasium.spaces.text import alphanumeric
+
+from gym_saturation.constants import MOCK_TPTP_PROBLEM
+from gym_saturation.proof_state import ProofState
+from gym_saturation.utils import pretty_print
+
+MAX_CLAUSES = 1000
+ALPHANUMERIC_WITH_UNDERSCORE = "".join(alphanumeric) + "_"
+SHORT_TEXT_SPACE = spaces.Text(256, charset=ALPHANUMERIC_WITH_UNDERSCORE)
+LONG_TEXT_SPACE = spaces.Text(
+    4000,
+    charset=ALPHANUMERIC_WITH_UNDERSCORE + "(), |~=!$",
+)
+
+
+class SaturationEnv(Env[tuple[dict[str, Any], ...], np.int64]):
+    """
+    Saturation algorithm in a reinforcement learning friendly way.
+
+    It's an abstract class, so here we have only trivial smoke tests.
+    One should override ``_do_deductions`` method in children classes.
+
+    Refer to :ref:`saturation_env` for more documentation.
+
+    >>> class DummyProver(SaturationEnv):
+    ...     def _do_deductions(action):
+    ...         pass
+    >>> env = DummyProver(render_mode="rgb_array")
+    Traceback (most recent call last):
+     ...
+    ValueError: Expected a render mode among ['ansi', 'human'] but got rgb_a...
+
+    >>> env = DummyProver()
+    >>> env.render_mode = "rgb_array"
+    >>> env.render()
+    Traceback (most recent call last):
+     ...
+    NotImplementedError
+    """
+
+    metadata = {"render_modes": ["ansi", "human"], "render_fps": 1}
+    reward_range = (0, 1)
+    action_space: spaces.Space
+    observation_space: spaces.Sequence
+
+    def __init__(
+        self,
+        max_clauses: int = MAX_CLAUSES,
+        render_mode: str = "human",
+    ):
+        """
+        Initialise spaces et al.
+
+        :param max_clauses: maximal number of clauses to store in proof state
+        :param render_mode: a mode of running ``render`` method
+        """
+        super().__init__()
+        self.state = ProofState(
+            clauses={},
+            step_number=-1,
+            max_clauses=max_clauses,
+        )
+        self.observation_space = spaces.Sequence(
+            spaces.Dict(
+                {
+                    "label": SHORT_TEXT_SPACE,
+                    "role": SHORT_TEXT_SPACE,
+                    "literals": LONG_TEXT_SPACE,
+                    "inference_rule": SHORT_TEXT_SPACE,
+                    "inference_parents": spaces.Sequence(SHORT_TEXT_SPACE),
+                    "birth_step": spaces.Discrete(max_clauses),
+                }
+            )
+        )
+        self._task = MOCK_TPTP_PROBLEM
+        self.render_mode = self._check_render_mode(render_mode)
+
+    def _check_render_mode(self, render_mode: str) -> str:
+        if render_mode in self.metadata["render_modes"]:
+            return render_mode
+        raise ValueError(
+            f"Expected a render mode among {self.metadata['render_modes']} "
+            f"but got {render_mode}"
+        )
+
+    def reset(
+        self,
+        *,
+        seed: Optional[int] = None,
+        options: Optional[dict[str, Any]] = None,
+    ) -> tuple[tuple[dict[str, Any], ...], dict[str, Any]]:
+        """
+        Reset the environment.
+
+        :param seed: seed for compatibility
+        :param options: options for compatibility
+        :returns: observations and info
+        """
+        super().reset(seed=seed)
+        random.seed(seed)
+        self.state = ProofState(
+            clauses={},
+            step_number=0,
+            max_clauses=self.state.max_clauses,
+        )
+        return tuple(self.state.clauses.values()), {}
+
+    @abstractmethod
+    def _do_deductions(self, action: Any) -> None:
+        raise NotImplementedError  # pragma: no cover
+
+    def step(
+        self, action: Any
+    ) -> tuple[tuple[dict[str, Any], ...], float, bool, bool, dict[str, Any]]:
+        # noqa: D301
+        """
+        Run one time-step of the environment's dynamics.
+
+        When end of episode is reached, you are responsible for calling
+        ``reset()`` to reset this environment's state.
+        Accepts an action and returns a tuple
+        (observation, reward, terminated, truncated, info)
+
+        :param action: an action provided by the agent
+        :returns: a tuple of four values:\n
+            * observation: agent's observation of the current environment
+            * reward: amount of reward returned after previous action
+            * terminated: Whether the proof was found
+            * truncated: Whether the maximal number of clauses in the proof
+              state were reached
+            * info: contains auxiliary diagnostic information (helpful for
+              debugging, and sometimes learning)
+        """
+        if not (self.state.terminated or self.state.truncated):
+            self.state.step_number += 1
+            self._do_deductions(action)
+        if self.state.truncated:
+            self.on_truncated()
+        return (
+            tuple(self.state.clauses.values()),
+            1.0 if self.state.terminated else 0.0,
+            self.state.terminated,
+            self.state.truncated,
+            {},
+        )
+
+    # pylint: disable=inconsistent-return-statements
+    def render(self) -> None:
+        """
+        Return or print the proof state.
+
+        :returns: proof state (TPTP formatted) or nothing
+            (depending on ``render_mode``)
+        """
+        tptp_string = "\n".join(
+            map(
+                pretty_print,
+                self.state.clauses.values(),
+            )
+        )
+        if self.render_mode == "ansi":
+            return tptp_string  # type: ignore
+        if self.render_mode == "human":
+            print(tptp_string)
+        else:
+            super().render()
+
+    def set_task(self, task: str) -> None:
+        """
+        Set the specified task to the current environment.
+
+        :param task: a TPTP problem filename
+        """
+        self._task = task
+
+    def get_task(self) -> str:
+        """
+        Get the task that the agent is performing in the current environment.
+
+        :returns: a TPTP problem filename
+        :raises ValueError: is task is not set
+        """
+        return self._task
+
+    def on_truncated(self) -> None:
+        """Prover-specific episode truncation."""
---
+++ packages/gym-saturation/gym_saturation-0.12.4.tar.gz/gym_saturation-0.12.4/gym_saturation/envs/vampire_env.py
@@ -0,0 +1,179 @@
+# Copyright 2021-2025 Boris Shminke
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# noqa: D205, D400
+"""
+Saturation Environment with Vampire back-end
+============================================
+"""
+import os
+from typing import Any, Optional
+
+import numpy as np
+from gymnasium.spaces import Discrete
+
+from gym_saturation.constants import FALSEHOOD_SYMBOL
+from gym_saturation.envs.saturation_env import MAX_CLAUSES, SaturationEnv
+from gym_saturation.vampire_wrapper import VampireWrapper
+
+
+class VampireEnv(SaturationEnv):
+    """
+    An RL environment wrapper around Vampire prover.
+
+    Refer to :ref:`saturation_env` for more documentation.
+
+    We can run a full Gymnasium environment check:
+
+    >>> from gymnasium.utils.env_checker import check_env
+    >>> import gymnasium as gym
+    >>> env = gym.make("Vampire-v0").unwrapped
+    >>> check_env(env)
+    cnf(1, ...).
+    ...
+    cnf(5, ...).
+
+    repeating actions change nothing
+
+    >>> env = gym.make("Vampire-v0", max_clauses=5)
+    >>> _ = env.reset()
+    >>> one = env.step(0)
+    >>> two = env.step(0)
+    >>> one == two
+    True
+
+    episode is truncated if we have more than ``max_clauses`` clauses
+
+    >>> _, _, _, truncated, _ = env.step(1)
+    >>> _, _, _, truncated, _ = env.step(2)
+    >>> truncated
+    True
+
+    sometimes Vampire can solve a problem during pre-processing
+
+    >>> from gym_saturation.constants import MOCK_TPTP_PROBLEM
+    >>> trivial_problem = os.path.join(os.path.dirname(MOCK_TPTP_PROBLEM),
+    ...     "TST002-1.p")
+    >>> env.unwrapped.set_task(trivial_problem)
+    >>> _, _ = env.reset()
+    >>> env.unwrapped.state.terminated
+    True
+    >>> _, _, terminated, _, _ = env.step(0)
+    >>> terminated
+    True
+
+    a test of an unexpected reply from Vampire
+
+    >>> from gym_saturation.constants import MOCK_TPTP_FOLDER
+    >>> vampire_binary = os.path.join(MOCK_TPTP_FOLDER, "..", "vampire-mock")
+    >>> vampire_env = VampireEnv(prover_binary_path=vampire_binary)
+    >>> vampire_env.reset()
+    Traceback (most recent call last):
+     ...
+    ValueError: ('Unexpected response type: ', 'who could expect that?')
+    """
+
+    def __init__(
+        self,
+        max_clauses: int = MAX_CLAUSES,
+        render_mode: str = "human",
+        prover_binary_path: str = "vampire",
+    ):
+        """
+        Initialise a :ref:`VampireWrapper <vampire-wrapper>`.
+
+        :param max_clauses: maximal number of clauses in proof state
+        :param render_mode: a mode of running ``render`` method
+        :param prover_binary_path: a path to Vampire binary;
+            by default we expect it to be in the $PATH
+        """
+        super().__init__(max_clauses, render_mode)
+        self._vampire = VampireWrapper(prover_binary_path)
+        self.action_space = Discrete(self.state.max_clauses)
+
+    def _parse_vampire_response(
+        self, vampire_response: tuple[tuple[str, str, str], ...]
+    ) -> None:
+        for response_type, clause_label, clause_text in vampire_response:
+            if response_type == "passive" or FALSEHOOD_SYMBOL in clause_text:
+                self.state.clauses[clause_label] = self._parse_vampire_clause(
+                    clause_label, clause_text
+                )
+            elif response_type not in {
+                "active",
+                "forward reduce",
+                "backward reduce",
+                "new propositional",
+                "new",
+                "final",
+                "input",
+                "fn def discovered",
+            }:
+                raise ValueError("Unexpected response type: ", response_type)
+
+    def reset(
+        self,
+        *,
+        seed: Optional[int] = None,
+        options: Optional[dict[str, Any]] = None,
+    ) -> tuple[tuple[dict[str, Any], ...], dict[str, Any]]:
+        """
+        Reset the environment.
+
+        :param seed: seed for compatibility
+        :param options: options for compatibility
+        :returns: observations and info
+        """
+        super().reset(seed=seed)
+        tptp_folder = os.path.join(
+            os.path.dirname(self.get_task()), "..", ".."
+        )
+        vampire_response = self._vampire.start(self.get_task(), tptp_folder)
+        self._parse_vampire_response(vampire_response)
+        return tuple(self.state.clauses.values()), {}
+
+    def _do_deductions(self, action: np.int64) -> None:
+        if action < len(self.state.clauses):
+            self._parse_vampire_response(
+                self._vampire.pick_a_clause(
+                    list(self.state.clauses.keys())[action]
+                )
+            )
+
+    def _parse_vampire_clause(
+        self, clause_label: str, clause_text: str
+    ) -> dict[str, Any]:
+        formula, inference_info = clause_text.split("[")
+        pre_inference = inference_info.split("]")[0].split(" ")
+        if len(pre_inference) > 1:
+            inference_parents = tuple(pre_inference[-1].split(","))
+            inference_rule = "_".join(pre_inference[:-1])
+        else:
+            inference_parents, inference_rule = (), pre_inference[0]
+        return {
+            "literals": formula.strip(),
+            "label": clause_label,
+            "role": "lemma",
+            "inference_rule": inference_rule,
+            "inference_parents": inference_parents,
+            "birth_step": self.state.step_number,
+        }
+
+    def on_truncated(self) -> None:
+        """Terminate Vampire process."""
+        self._vampire.terminate()
+
+    def close(self) -> None:
+        """Terminate Vampire process."""
+        self._vampire.terminate()
