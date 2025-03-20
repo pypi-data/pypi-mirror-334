b659f61ca0e97cb16cdcaef4bafffd9f79de1ad1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 07:03:50 +0000

    Add package diverge/4.0.4/diverge-4.0.4-cp311-cp311-win_amd64.whl

---
+++ packages/diverge/diverge-4.0.4-cp311-cp311-win_amd64.whl/diverge/__init__.py
@@ -0,0 +1,12 @@
+from .binding import (
+    Gu99, Gu2001, Type2, Fdr, Effective, Asym, TypeOneAnalysis, Rvs
+)
+from .utils import (
+    CalPipe
+)
+from .super_cluster import SuperCluster
+from .treeplot import draw_phylogenetic_tree
+
+__version__ = '4.0.1'
+
+
