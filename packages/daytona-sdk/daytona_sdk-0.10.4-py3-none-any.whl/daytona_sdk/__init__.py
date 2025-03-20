1047a6d844136ec524972a64224ab4e63974ac36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 18:05:30 +0000

    Add package daytona-sdk/0.10.4/daytona_sdk-0.10.4-py3-none-any.whl

---
+++ packages/daytona-sdk/daytona_sdk-0.10.4-py3-none-any.whl/daytona_sdk/__init__.py
@@ -0,0 +1,48 @@
+from .daytona import (
+    Daytona,
+    DaytonaConfig,
+    CodeLanguage,
+    SessionExecuteRequest,
+    SessionExecuteResponse,
+    DaytonaError,
+    CreateSandboxParams,
+    SandboxResources,
+)
+from .lsp_server import LspLanguageId
+from .common.code_run_params import CodeRunParams
+from .sandbox import (
+    Sandbox,
+    SandboxTargetRegion,
+)
+from daytona_api_client import WorkspaceState as SandboxState
+
+
+# Create deprecated aliases with proper warnings
+from ._utils.deprecation import deprecated_alias
+CreateWorkspaceParams = deprecated_alias('CreateWorkspaceParams', 'CreateSandboxParams')(CreateSandboxParams)
+Workspace = deprecated_alias('Workspace', 'Sandbox')(Sandbox)
+WorkspaceTargetRegion = deprecated_alias('WorkspaceTargetRegion', 'SandboxTargetRegion')(SandboxTargetRegion)
+WorkspaceResources = deprecated_alias('WorkspaceResources', 'SandboxResources')(SandboxResources)
+WorkspaceState = deprecated_alias('WorkspaceState', 'SandboxState')(SandboxState)
+
+__all__ = [
+    "Daytona",
+    "DaytonaConfig",
+    "CodeLanguage",
+    "SessionExecuteRequest",
+    "SessionExecuteResponse",
+    "DaytonaError",
+    "LspLanguageId",
+    "WorkspaceTargetRegion",
+    "CodeRunParams",
+    "CreateSandboxParams",
+    "Sandbox",
+    "SandboxTargetRegion",
+    "SandboxResources",
+    "SandboxState",
+    "CreateWorkspaceParams",
+    "Workspace",
+    "WorkspaceTargetRegion",
+    "WorkspaceResources",
+    "WorkspaceState",
+]
