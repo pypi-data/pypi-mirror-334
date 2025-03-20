49493679231f74bc77fa7e9a390d227047c8f25c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 00:05:58 +0000

    Add package owui-pipelines/0.1.3/owui_pipelines-0.1.3.tar.gz

---
+++ packages/owui-pipelines/owui_pipelines-0.1.3.tar.gz/owui_pipelines-0.1.3/owui_pipelines/__init__.py
@@ -0,0 +1,10 @@
+"""
+owui-pipelines - Package for OWUI pipelines
+"""
+
+__version__ = "0.1.3"
+
+# Expose the api subpackage
+from owui_pipelines import api
+
+__all__ = ["api"]
---
+++ packages/owui-pipelines/owui_pipelines-0.1.3.tar.gz/owui_pipelines-0.1.3/owui_pipelines/api/__init__.py
@@ -0,0 +1,5 @@
+"""API components for the owui-pipelines package."""
+
+from owui_pipelines.api.azure_agent import Pipeline, get_text_from_thread_message
+
+__all__ = ["Pipeline", "get_text_from_thread_message"]
