053fe576ba61f6159ad71f2f865ecc6bfa34408f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 05:03:59 +0000

    Add package forestatrisk/1.3.2/forestatrisk-1.3.2-cp311-cp311-macosx_11_0_arm64.whl

---
+++ packages/forestatrisk/forestatrisk-1.3.2-cp311-cp311-macosx_11_0_arm64.whl/forestatrisk/data/download/__init__.py
@@ -0,0 +1,6 @@
+"""Download geospatial data."""
+
+from .download_gadm import download_gadm
+from .download_osm import download_osm
+from .download_srtm import download_srtm
+from .download_wdpa import download_wdpa
