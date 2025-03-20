8fd9bb37ee718514a30a5da196a6f236efc01f66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 13:11:17 +0000

    Add package linux-parsers/0.2.0/linux_parsers-0.2.0-py3-none-any.whl

---
+++ packages/linux-parsers/linux_parsers-0.2.0-py3-none-any.whl/linux_parsers/parsers/filesystem/ls.py
@@ -0,0 +1,15 @@
+import re
+
+
+def parse_ls(command_output: str) -> list[dict[str, any]]:
+    """Parse `ls -la` command output."""
+    pattern = re.compile(
+        r"(?P<Permissions>\S+)\s+"
+        r"(?P<HardLinks>\d+)\s"
+        r"(?P<Owner>\S+)\s+"
+        r"(?P<Group>\S+)\s+"
+        r"(?P<Size>\d+)\s+"
+        r"(?P<LastModified>\w{3}\s+\d+\s+\d+:\d+)\s+"
+        r"(?P<File>.+)"
+    )
+    return [i.groupdict() for i in pattern.finditer(command_output)]
