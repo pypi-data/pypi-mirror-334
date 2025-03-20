c9ace6acbed2afda011a287b6ae496b4917410df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 17:05:04 +0000

    Add package gaia-browser/0.1.0/gaia_browser-0.1.0.tar.gz

---
+++ packages/gaia-browser/gaia_browser-0.1.0.tar.gz/gaia_browser-0.1.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="gaia-browser",
+    version="0.1.0",
+    author="Ma Weiyi",
+    author_email="fwyr@proton.me",
+    license="MIT",
+    description="a terminal library genesis browser",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/fwyr/gaia",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        "rich",
+        "beautifulsoup4",
+        "pyfiglet"
+    ],
+    entry_points={
+        "console_scripts": [
+            "gaia=gaia.cli:main",  # Adjust this to point to your main function
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
