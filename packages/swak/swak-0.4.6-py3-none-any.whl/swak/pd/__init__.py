2cba8ffe60ed1bd743523dfc8d2308c96cf28020
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 23:05:33 +0000

    Add package swak/0.4.6/swak-0.4.6-py3-none-any.whl

---
+++ packages/swak/swak-0.4.6-py3-none-any.whl/swak/pd/__init__.py
@@ -0,0 +1,55 @@
+"""Pandas utilities and partials of dataframe method calls.
+
+Parameters that are known at program start are used to initialize the classes
+so that, at runtime, dataframes can flow through a preconfigured processing
+pipe of callable objects.
+
+"""
+
+from .read import ParquetReader
+from .write import ParquetWriter
+from .groupby import (
+    FrameGroupByAgg,
+    RollingGroupBy,
+    RollingGroupByAgg,
+    RollingGroupByApply
+)
+from .frame import (
+    AsType,
+    ColumnSelector,
+    ColumnsSelector,
+    ColumnMapper,
+    RowsSelector,
+    FrameGroupBy,
+    Join,
+    Assign,
+    Drop,
+    DropNA,
+    SortValues,
+    SetIndex,
+    ResetIndex,
+    Rename
+)
+
+__all__ = [
+    'ParquetReader',
+    'ParquetWriter',
+    'AsType',
+    'ColumnSelector',
+    'ColumnsSelector',
+    'ColumnMapper',
+    'RowsSelector',
+    'Join',
+    'Assign',
+    'Drop',
+    'DropNA',
+    'SortValues',
+    'SetIndex',
+    'ResetIndex',
+    'Rename',
+    'FrameGroupBy',
+    'FrameGroupByAgg',
+    'RollingGroupBy',
+    'RollingGroupByAgg',
+    'RollingGroupByApply'
+]
