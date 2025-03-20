0d33b541a9b1ac9116b56e9a0246e0824bccade5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 08:04:23 +0000

    Add package ghcs/0.1.0/ghcs-0.1.0.tar.gz

---
+++ packages/ghcs/ghcs-0.1.0.tar.gz/ghcs-0.1.0/ghcs/downloader.py
@@ -0,0 +1,21 @@
+import requests
+import os
+
+def download_file(url, path, token=None, download_dir="codes"):
+    try:
+        headers = {}
+        if token:
+            headers["Authorization"] = f"token {token}"
+
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+            save_path = os.path.join(download_dir, path)
+            os.makedirs(os.path.dirname(save_path), exist_ok=True)
+            with open(save_path, "w", encoding="utf-8") as f:
+                f.write(response.text)
+            print(f"Downloaded: {save_path}")
+        else:
+            print(f"Failed to download {url}")
+    except Exception as e:
+        print(f"Error: {e}")
+        print(f"Failed to download {url}")
---
+++ packages/ghcs/ghcs-0.1.0.tar.gz/ghcs-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ghcs",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=["requests", "python-dotenv"],
+    entry_points={"console_scripts": ["ghcs=ghcs.cli:main"]},
+    author="Md. Sazzad Hissain Khan",
+    author_email='hissain.khan@gmail.com',
+    description="GitHub Code Search CLI with file downloading capability.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/hissain/ghcs',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
