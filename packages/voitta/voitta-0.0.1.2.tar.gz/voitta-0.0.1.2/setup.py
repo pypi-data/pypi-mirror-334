c3e5b83012840f5b22c6b7d7b9b107b14d35b93d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 08:04:41 +0000

    Add package voitta/0.0.1.2/voitta-0.0.1.2.tar.gz

---
+++ packages/voitta/voitta-0.0.1.2.tar.gz/voitta-0.0.1.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='voitta',
+    version='0.0.1.2',
+    packages=find_packages(),
+    install_requires=[],
+    author='Voitta',
+    author_email='support@voitta.ai',
+    description='LLM tool calls routing and automation',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://voitta.ai',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
