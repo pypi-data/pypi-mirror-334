6b7585833b2013158527531578bd9432b20d3a2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 18:05:38 +0000

    Add package ep-stats/2.5.7/ep_stats-2.5.7.tar.gz

---
+++ packages/ep-stats/ep_stats-2.5.7.tar.gz/ep_stats-2.5.7/src/epstats/main.py
@@ -0,0 +1,62 @@
+import logging
+from concurrent.futures import ThreadPoolExecutor
+
+from prometheus_client import CollectorRegistry, make_asgi_app, multiprocess
+from pydantic_settings import BaseSettings
+
+from .server import ApiSettings, get_api, serve
+from .toolkit.testing import TestDaoFactory, TestData
+
+
+class Settings(BaseSettings):
+    api: ApiSettings = ApiSettings()
+
+    evaluation_pool_size: int = 5
+
+
+settings = Settings()
+
+
+def get_dao_factory():
+    return TestDaoFactory(TestData())
+
+
+def get_dao():
+    try:
+        dao = get_dao_factory().get_dao()
+        yield dao
+    finally:
+        dao.close()
+
+
+def get_executor_pool():
+    try:
+        evaluation_pool_size = settings.evaluation_pool_size
+        evaluation_pool = ThreadPoolExecutor(max_workers=evaluation_pool_size)
+        yield evaluation_pool
+    finally:
+        pass
+
+
+def make_metrics_app():
+    if settings.api.web_workers == 1:
+        return make_asgi_app()
+    registry = CollectorRegistry()
+    multiprocess.MultiProcessCollector(registry)
+    return make_asgi_app(registry=registry)
+
+
+api = get_api(settings.api, get_dao, get_executor_pool)
+metrics_app = make_metrics_app()
+api.mount("/metrics", metrics_app)
+
+
+def main():
+    from .config import config
+
+    logging.config.dictConfig(config["logging"])
+    serve("epstats:api", settings.api, config["logging"])
+
+
+if __name__ == "__main__":
+    main()
