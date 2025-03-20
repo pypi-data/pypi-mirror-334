8bcbbd74dd5b2852689ed4743b278a0691dc5a3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 09:04:07 +0000

    Add package ST4/0.3/st4-0.3.tar.gz

---
+++ packages/ST4/st4-0.3.tar.gz/st4-0.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ST4",
+    version="0.3",
+    author="HUSSAIN",
+    author_email="reco9678785@gmail.com",
+    description="lib in Python for instagram",
+    packages=find_packages(),
+    install_requires=[
+        "user_agent",
+        "requests",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
