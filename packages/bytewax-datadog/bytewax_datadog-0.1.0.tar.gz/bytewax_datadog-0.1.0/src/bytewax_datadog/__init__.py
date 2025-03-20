00e1e9267d938b43227d777c1d8fcd30f52b9a55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 08:04:42 +0000

    Add package bytewax-datadog/0.1.0/bytewax_datadog-0.1.0.tar.gz

---
+++ packages/bytewax-datadog/bytewax_datadog-0.1.0.tar.gz/bytewax_datadog-0.1.0/src/bytewax_datadog/__init__.py
@@ -0,0 +1,15 @@
+from .inputs import (
+    LogEntry,
+    LogSource,
+)
+from .outputs import (
+    CreateLogEntry,
+    LogSink,
+)
+
+__all__ = (
+    "LogEntry",
+    "LogSource",
+    "CreateLogEntry",
+    "LogSink",
+)
