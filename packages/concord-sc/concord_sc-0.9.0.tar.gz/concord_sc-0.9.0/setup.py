fc035a77d97e3b259700ef2e0b08e5ef614fd389
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 09:03:52 +0000

    Add package concord-sc/0.9.0/concord_sc-0.9.0.tar.gz

---
+++ packages/concord-sc/concord_sc-0.9.0.tar.gz/concord_sc-0.9.0/setup.py
@@ -0,0 +1,46 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='concord-sc',  # Unique PyPI package name
+    version='0.9.0',
+    description='CONCORD: Contrastive Learning for Cross-domain Reconciliation and Discovery',
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(include=['Concord', 'Concord.*']),  # Lowercase to match import conventions
+    author='Qin Zhu',
+    author_email='qin.zhu@ucsf.edu',
+    url='https://github.com/Gartner-Lab/Concord',
+    license="MIT",
+    install_requires=[
+        "anndata>=0.8",
+        "numpy>=1.23,<2.0",
+        "h5py>=3.1",
+        "tqdm",
+        "umap-learn>=0.5.1",
+        "matplotlib>=3.6",
+        "pandas>=1.5",
+        "plotly>=5.0.0",
+        "scanpy>=1.1",
+        "scikit-learn>=0.24",
+        "scipy>=1.8",
+        "seaborn>=0.13",
+        "scikit-misc",
+        "nbformat",
+        "build"
+    ],
+    extras_require={
+        "optional": [
+            "gseapy>=1.1.0",
+            "Pillow>=10.0.0",
+            "plottable>=0.1",
+            "requests>=2.0",
+            "rpy2>=3.5"
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
