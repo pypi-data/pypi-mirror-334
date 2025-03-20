2b062684f10b17e030e993c72c5924a5c37b47c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 06:04:31 +0000

    Add package github-cli-mamba/0.1.0/github_cli_mamba-0.1.0-py3-none-any.whl

---
+++ packages/github-cli-mamba/github_cli_mamba-0.1.0-py3-none-any.whl/github_cli_mamba/cli/user.py
@@ -0,0 +1,24 @@
+import typer
+from github_cli_mamba.github import GithubAPI
+from github_cli_mamba.options import OutputOption
+from github_cli_mamba.utils import print_beautify
+
+user_app = typer.Typer()
+
+
+@user_app.command(name="profile", help="list user profile")
+def profile(
+    username: str = typer.Option(..., "--username", "-u", help="github username")
+):
+    github_api = GithubAPI()
+    repos = github_api.get_all_user_repositories(username)
+    followers = github_api.list_followers_of_user(username)
+    followings = github_api.list_people_user_follows(username)
+
+    profile = {
+        "username": username,
+        "followers": len(followers) if followers else 0,
+        "followings": len(followings) if followings else 0,
+        "repos": len(repos) if repos else 0,
+    }
+    print_beautify([profile], OutputOption.table)
---
+++ packages/github-cli-mamba/github_cli_mamba-0.1.0-py3-none-any.whl/github_cli_mamba/main.py
@@ -0,0 +1,20 @@
+import os
+
+import typer
+from dotenv import load_dotenv
+
+from github_cli_mamba.cli.repo import repo_app
+from github_cli_mamba.cli.user import user_app
+
+if os.path.exists(".env"):
+    load_dotenv()
+
+
+app = typer.Typer()
+
+
+app.add_typer(repo_app, name="repo", help="github repository commands")
+app.add_typer(user_app, name="user", help="github user commands")
+
+if __name__ == "__main__":
+    app() 
