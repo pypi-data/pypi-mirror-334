1ddabf130e86424c9d021e492331b51e9629ae38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 16:05:23 +0000

    Add package ByteLightProject/2.6/bytelightproject-2.6.tar.gz

---
+++ packages/ByteLightProject/bytelightproject-2.6.tar.gz/bytelightproject-2.6/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ByteLightProject',
+    version='2.6',
+    packages=find_packages(),
+    install_requires=[
+        'pyautogui',
+        'requests',
+        'pyperclip'
+    ],
+    extras_require={
+        "email_support": ["smtplib"],
+    },
+    author='ByteLightDev',
+    author_email='bytelightdevofficial@gmail.com',
+    description='ByteLightProject Python Module',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/ByteLightDev1/ByteLight',
+    classifiers=[  
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+    ],
+)
