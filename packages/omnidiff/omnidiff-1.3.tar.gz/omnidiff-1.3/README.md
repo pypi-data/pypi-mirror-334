890b203492985542b916467f7300602bd5acf619
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 08:04:21 +0000

    Add package omnidiff/1.3/omnidiff-1.3.tar.gz

---
+++ packages/omnidiff/omnidiff-1.3.tar.gz/omnidiff-1.3/README.md
@@ -0,0 +1,1 @@
+# DiffKit
---
+++ packages/omnidiff/omnidiff-1.3.tar.gz/omnidiff-1.3/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+from setuptools import find_packages
+with open("README.md", "r") as f:
+  long_description = f.read()
+
+setup(name='omnidiff',
+      version='1.3',
+      description='A diff toolkit for python',
+      author='game1024',
+      author_email='pengbin.xyz@gmail.com',
+      url='https://github.com/game1024/omnidiff',
+      install_requires=["jmespath"],
+      license='MIT',
+      packages=find_packages(),
+      platforms=["all"],
+      classifiers=[
+            'Intended Audience :: Developers',
+            'Operating System :: OS Independent',
+            'Natural Language :: Chinese (Simplified)',
+            'Programming Language :: Python',
+            'Programming Language :: Python :: 3',
+      ],
+      python_requires='>=3',
+      )
