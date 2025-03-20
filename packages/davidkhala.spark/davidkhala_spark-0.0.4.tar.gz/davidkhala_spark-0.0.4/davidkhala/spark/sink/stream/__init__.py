001286ccd8255c1e5edadcaffc9eb937ea7bc478
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 12:05:16 +0000

    Add package davidkhala.spark/0.0.4/davidkhala_spark-0.0.4.tar.gz

---
+++ packages/davidkhala.spark/davidkhala_spark-0.0.4.tar.gz/davidkhala_spark-0.0.4/davidkhala/spark/sink/stream/__init__.py
@@ -0,0 +1,30 @@
+from abc import abstractmethod, ABC
+from typing import Callable, Protocol
+
+from pyspark.sql.connect.dataframe import DataFrame
+from pyspark.sql.connect.streaming import StreamingQuery
+from pyspark.sql.types import Row
+
+
+class ForeachWriter(ABC):
+    """
+    object class used in DataStreamWriter.foreach(...)
+    """
+
+    def open(self, partition_id: int, epoch_id: int) -> bool: pass
+
+    @abstractmethod
+    def process(self, row: Row) -> None: ...
+
+    def close(self, error: Exception | None) -> None: pass
+
+
+class ForeachBatchWriter(Protocol):
+    @property
+    def on_batch(self) -> Callable[[DataFrame, int], None]: ...
+
+
+def startAny(df: DataFrame, writer: ForeachBatchWriter) -> StreamingQuery:
+    assert df.isStreaming
+
+    return df.writeStream.foreachBatch(writer.on_batch).start()
