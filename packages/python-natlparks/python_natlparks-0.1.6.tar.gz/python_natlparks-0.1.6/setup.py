4d0363039641b85113fb22d6a208c9dfa6a2027b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 09:05:01 +0000

    Add package python-natlparks/0.1.6/python_natlparks-0.1.6.tar.gz

---
+++ packages/python-natlparks/python_natlparks-0.1.6.tar.gz/python_natlparks-0.1.6/setup.py
@@ -0,0 +1,37 @@
+"""Setup for the natlparks package."""
+
+import setuptools
+
+
+with open("README.md") as f:
+    README = f.read()
+
+setuptools.setup(
+    author="Ira Horecka",
+    author_email="ira89@icloud.com",
+    name="python-natlparks",
+    license="MIT",
+    description="Simple API wrapper for US National Park Services",
+    version="v0.1.6",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/irahorecka/python-natlparks",
+    packages=["natlparks"],
+    python_requires=">=3.8",
+    install_requires=["requests"],
+    classifiers=[
+        # Trove classifiers
+        # (https://pypi.python.org/pypi?%3Aaction=list_classifiers)
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Intended Audience :: Developers",
+    ],
+)
