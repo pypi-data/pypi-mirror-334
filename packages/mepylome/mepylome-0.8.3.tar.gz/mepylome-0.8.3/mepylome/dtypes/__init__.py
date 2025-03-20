aa5913860d30bf5f55688cddf4d44103305320e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 06:04:09 +0000

    Add package mepylome/0.8.3/mepylome-0.8.3.tar.gz

---
+++ packages/mepylome/mepylome-0.8.3.tar.gz/mepylome-0.8.3/mepylome/dtypes/__init__.py
@@ -0,0 +1,63 @@
+"""Module providing access to various data types and utilities."""
+
+from .arrays import ArrayType
+from .beads import (
+    NEUTRAL_BETA,
+    MethylData,
+    RawData,
+    ReferenceMethylData,
+    _overlap_indices,
+    idat_basepaths,
+    is_valid_idat_basepath,
+)
+from .cache import get_id_tuple, input_args_id, memoize
+from .chromosome import Chromosome
+from .cnv import CNV, ZIP_ENDING, Annotation, _get_cgsegment
+from .genetic_data import (
+    CHROMOSOME_DATA,
+    GAPS,
+    GENES,
+    IMPORTANT_GENES,
+)
+from .idat import IdatParser
+from .manifests import Manifest
+from .plots import (
+    CNVPlot,
+    cnv_plot_from_data,
+    get_cn_summary,
+    read_cnv_data_from_disk,
+)
+from .probes import Channel, InfiniumDesignType, ProbeType
+
+__all__ = [
+    "Annotation",
+    "ArrayType",
+    "CHROMOSOME_DATA",
+    "CNV",
+    "CNVPlot",
+    "NEUTRAL_BETA",
+    "input_args_id",
+    "Channel",
+    "Chromosome",
+    "GAPS",
+    "GENES",
+    "IMPORTANT_GENES",
+    "IdatParser",
+    "InfiniumDesignType",
+    "Manifest",
+    "MethylData",
+    "ProbeType",
+    "RawData",
+    "ReferenceMethylData",
+    "ZIP_ENDING",
+    "cache",
+    "cnv_plot_from_data",
+    "get_cn_summary",
+    "get_id_tuple",
+    "idat_basepaths",
+    "is_valid_idat_basepath",
+    "memoize",
+    "_overlap_indices",
+    "_get_cgsegment",
+    "read_cnv_data_from_disk",
+]
