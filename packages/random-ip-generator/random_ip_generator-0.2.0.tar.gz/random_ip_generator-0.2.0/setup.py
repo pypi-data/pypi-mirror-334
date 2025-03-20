1d56ac2d0602c8f2c147b8725cfbeabd86e07f39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 07:04:08 +0000

    Add package random-ip-generator/0.2.0/random_ip_generator-0.2.0.tar.gz

---
+++ packages/random-ip-generator/random_ip_generator-0.2.0.tar.gz/random_ip_generator-0.2.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="random-ip-generator",
+    version="0.2.0",
+    author="edde746",
+    description="A package to generate random IP addresses for a given country code",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/edde746/random-ip-generator",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "ipaddress",
+    ],
+    entry_points={
+        "console_scripts": [
+            "random-ip-generator=random_ip_generator.main:main",
+        ],
+    },
+)
