0137c6856fee75b4f2172cddd90da0341b6ce756
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 23:05:49 +0000

    Add package eudplib/0.80.2/eudplib-0.80.2-cp310-abi3-win_amd64.whl

---
+++ packages/eudplib/eudplib-0.80.2-cp310-abi3-win_amd64.whl/eudplib/utils/__init__.py
@@ -0,0 +1,76 @@
+# Copyright 2014 by trgk.
+# All rights reserved.
+# This file is part of EUD python library (eudplib),
+# and is released under "MIT License Agreement". Please see the LICENSE
+# file that should have been included as part of this package.
+
+from .binio import b2i1, b2i2, b2i4, bits, i2b1, i2b2, i2b4
+from .blockstru import (
+    EUDCreateBlock,
+    EUDGetBlockList,
+    EUDGetLastBlock,
+    EUDGetLastBlockOfName,
+    EUDPeekBlock,
+    EUDPopBlock,
+)
+from .eperror import (
+    EPError,
+    EPWarning,
+    TriggerScopeError,
+    ep_assert,
+    ep_eprint,
+    ep_warn,
+)
+from .etc import (
+    EPD,
+    Assignable2List,
+    FlattenIter,
+    FlattenList,
+    List2Assignable,
+    SCMD2Text,
+    _rand_lst,
+    cachedfunc,
+    classproperty,
+    find_data_file,
+)
+from .exprproxy import ExprProxy, isUnproxyInstance, unProxy
+from .ubconv import b2u, b2utf8, u2b, u2utf8
+
+__all__ = [
+    "b2i1",
+    "b2i2",
+    "b2i4",
+    "bits",
+    "i2b1",
+    "i2b2",
+    "i2b4",
+    "EUDCreateBlock",
+    "EUDGetBlockList",
+    "EUDGetLastBlock",
+    "EUDGetLastBlockOfName",
+    "EUDPeekBlock",
+    "EUDPopBlock",
+    "EPError",
+    "EPWarning",
+    "TriggerScopeError",
+    "ep_assert",
+    "ep_eprint",
+    "ep_warn",
+    "EPD",
+    "Assignable2List",
+    "FlattenIter",
+    "FlattenList",
+    "List2Assignable",
+    "SCMD2Text",
+    "_rand_lst",
+    "cachedfunc",
+    "classproperty",
+    "find_data_file",
+    "ExprProxy",
+    "isUnproxyInstance",
+    "unProxy",
+    "b2u",
+    "b2utf8",
+    "u2b",
+    "u2utf8",
+]
