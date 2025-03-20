03f595189c5d71776d5da82a000483c34acaa77d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 09:04:20 +0000

    Add package dungeondice/1.1.2/dungeondice-1.1.2-py3-none-any.whl

---
+++ packages/dungeondice/dungeondice-1.1.2-py3-none-any.whl/dungeondice/lib/discord_templates.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+from dungeondice.lib import dice
+
+
+def dicerolls(author: str, rollgroups: list[dice.Rollgroup], comment: str):
+    comment = "__{}__".format(comment) if comment else comment
+    out = ''
+
+    for rg in rollgroups:
+        out += '''\
+---------------------------------
+> {}
+**Total: {}**
+_Details: {}_
+---------------------------------
+'''.format(rg.rollstring, rg.total, rg.rollsets)
+
+    return '''\
+**{}** rolled {}
+{}
+'''.format(author, comment, out)
---
+++ packages/dungeondice/dungeondice-1.1.2-py3-none-any.whl/dungeondice/main.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+import discord
+from discord.ext import commands
+
+import os
+import sys
+from dotenv import load_dotenv
+
+from dungeondice.lib import dice
+from dungeondice.lib import discord_templates
+
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+diceparser = dice.Parser()
+
+
+@bot.event
+async def on_ready():
+    if bot.user:
+        print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+        print('------')
+    else:
+        print('Something is seriously wrong with this bot.')
+
+
+@bot.command(name='roll', aliases=['r'])
+async def roll(
+    ctx,
+    dicestring: str,
+    *, message: str = ''
+):
+    """Uses the diceparser to roll dice."""
+    try:
+        requester = ctx.author
+
+        result = discord_templates.dicerolls(
+            requester.display_name, diceparser.parse(dicestring), message
+        )
+    except ValueError:
+        result = "Invalid dicestring."
+    except Exception as exc:
+        print(exc)
+        result = "Error while rolling."
+
+    await ctx.send(result)
+
+
+def start_bot():
+    if TOKEN:
+        bot.run(TOKEN)
+    else:
+        sys.exit("Missing a discord token. Supply it as environment variable.")
