8c304bf905ceffc0dcdb49c3bb506fe5ad3709ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 12:06:22 +0000

    Add package agentrix/0.0.1/agentrix-0.0.1.tar.gz

---
+++ packages/agentrix/agentrix-0.0.1.tar.gz/agentrix-0.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Agentrix",
+    version="0.0.1",
+    author="Sanjeev",
+    author_email="sanjeevsaritha03@gmail.com",
+    description="A framework for creating AI agents and agent managers",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/sanjeev-0407/Agentic.git",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "openai",
+    ],
+)
