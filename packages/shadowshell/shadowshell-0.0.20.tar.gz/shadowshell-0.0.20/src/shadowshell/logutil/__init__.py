97023c6c40d1e79523e0259a1fc4ac8991781bae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 22:04:43 +0000

    Add package shadowshell/0.0.20/shadowshell-0.0.20.tar.gz

---
+++ packages/shadowshell/shadowshell-0.0.20.tar.gz/shadowshell-0.0.20/src/shadowshell/logutil/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+@author: shadow shell
+"""
+
+from .console_logger import ConsoleLogger
+from .logger import Logger
+from .logger_factory import LoggerFactory
+
+__all__ = ['ConsoleLogger', 'Logger', 'LoggerFactory']
---
+++ packages/shadowshell/shadowshell-0.0.20.tar.gz/shadowshell-0.0.20/tests/tests.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+tests
+
+@author: shadow shell
+"""
+
+from shadowshell import *
+
+ShadowShell().hello()
+
+cnnserver()
+
