4d0114e33b2d3da30d3f0e441a7ef8b0258c3a26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 12:05:03 +0000

    Add package mutils-devkit/0.1.0/mutils_devkit-0.1.0.tar.gz

---
+++ packages/mutils-devkit/mutils_devkit-0.1.0.tar.gz/mutils_devkit-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mutils_devkit",  # Your package name
+    version="0.1.0",  # Version number
+    packages=find_packages(),
+    install_requires=[],  # List dependencies here
+    author="pythonpackages0.0.1",
+    author_email="packagespython@gmail.com",
+    description="",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.5",
+)
