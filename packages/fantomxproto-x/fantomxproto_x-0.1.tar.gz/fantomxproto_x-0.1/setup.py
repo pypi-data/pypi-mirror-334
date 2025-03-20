a5ea5aa4dedcd046c4cde54b191dca46c8c4d5ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 12:05:42 +0000

    Add package fantomxproto-x/0.1/fantomxproto_x-0.1.tar.gz

---
+++ packages/fantomxproto-x/fantomxproto_x-0.1.tar.gz/fantomxproto_x-0.1/setup.py
@@ -0,0 +1,17 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="fantomxproto-x",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        'cryptography',  # Add any dependencies you have here
+    ],
+    description="End-to-end encryption chat protocol",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author="ALfisene Keita",
+    author_email="info@infopeklo.cz",
+    url="https://github.com/certikpolik1/fantomxryptx",
+)
