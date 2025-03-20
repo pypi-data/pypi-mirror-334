96d309af914e4729ce8ddea4f06be7c701939671
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 11:05:15 +0000

    Add package PyMatterSim/0.0.9/PyMatterSim-0.0.9.tar.gz

---
+++ packages/PyMatterSim/PyMatterSim-0.0.9.tar.gz/PyMatterSim-0.0.9/setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="PyMatterSim",  # Replace with your project name
+    version="0.0.9",
+    author="Yuan-Chao Hu",
+    author_email="ychu0213@gmail.com",
+    description="A python data analysis library for computer simulations",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yuanchaohu/pymattersim",
+    packages=find_packages(),  # Automatically discover packages
+    install_requires=[i.strip("\n") for i in open("requirements.txt").readlines()],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6,<3.12",
+)
