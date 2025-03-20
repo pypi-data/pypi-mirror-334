e55d257fd37ea2b4ed5bcac0f33bcc3a88dc4659
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 14:05:06 +0000

    Add package uipath_connectors.oracle_netsuite/1.4.7.dev6/uipath_connectors_oracle_netsuite-1.4.7.dev6.tar.gz

---
+++ packages/uipath_connectors.oracle_netsuite/uipath_connectors_oracle_netsuite-1.4.7.dev6.tar.gz/uipath_connectors_oracle_netsuite-1.4.7.dev6/uipath_connectors/oracle_netsuite/models/create_vendor_request_address_1.py
@@ -0,0 +1,62 @@
+from pydantic import BaseModel, ConfigDict, Field
+from typing import Any, Dict, Optional, Type
+
+from ..models.create_vendor_request_address_1_address_1_country import (
+    CreateVendorRequestAddress1Address1Country,
+)
+
+
+class CreateVendorRequestAddress1(BaseModel):
+    """
+    Attributes:
+        city (Optional[str]): The Address 1 city Example: Hyderabad.
+        country (Optional[CreateVendorRequestAddress1Address1Country]): The Address 1 country Example: _unitedStates.
+        state (Optional[str]): The Address 1 state Example: TG.
+        default_billing (Optional[bool]): The Address 1 default billing Example: false.
+        zip_ (Optional[str]): The Address 1 zip Example: 505473.
+        addr1 (Optional[str]): The Address 1 addr 1 Example: jntu.
+        default_shipping (Optional[bool]): The Address 1 default shipping Example: true.
+        addr2 (Optional[str]): The Address 1 addr 2 Example: SR.
+    """
+
+    model_config = ConfigDict(extra="allow", populate_by_name=True)
+
+    city: Optional[str] = Field(alias="city", default=None)
+    country: Optional[CreateVendorRequestAddress1Address1Country] = Field(
+        alias="country", default=None
+    )
+    state: Optional[str] = Field(alias="state", default=None)
+    default_billing: Optional[bool] = Field(alias="defaultBilling", default=None)
+    zip_: Optional[str] = Field(alias="zip", default=None)
+    addr1: Optional[str] = Field(alias="addr1", default=None)
+    default_shipping: Optional[bool] = Field(alias="defaultShipping", default=None)
+    addr2: Optional[str] = Field(alias="addr2", default=None)
+
+    def to_dict(self) -> Dict[str, Any]:
+        return self.model_dump(exclude_none=True, by_alias=True)
+
+    @classmethod
+    def from_dict(cls: Type["CreateVendorRequestAddress1"], src_dict: Dict[str, Any]):
+        return cls.model_validate(src_dict)
+
+    @property
+    def additional_keys(self) -> list[str]:
+        base_fields = self.model_fields.keys()
+        return [k for k in self.__dict__ if k not in base_fields]
+
+    def __getitem__(self, key: str) -> Any:
+        if key in self.__dict__:
+            return self.__dict__[key]
+        raise KeyError(key)
+
+    def __setitem__(self, key: str, value: Any) -> None:
+        self.__dict__[key] = value
+
+    def __delitem__(self, key: str) -> None:
+        if key in self.__dict__:
+            del self.__dict__[key]
+        else:
+            raise KeyError(key)
+
+    def __contains__(self, key: str) -> bool:
+        return key in self.__dict__
