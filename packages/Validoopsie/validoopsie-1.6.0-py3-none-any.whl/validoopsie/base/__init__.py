4e9c6a15581f354997d59becddb9f363ca3693ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 16:05:48 +0000

    Add package Validoopsie/1.6.0/validoopsie-1.6.0-py3-none-any.whl

---
+++ packages/Validoopsie/validoopsie-1.6.0-py3-none-any.whl/validoopsie/base/__init__.py
@@ -0,0 +1,15 @@
+from .base_validation import BaseValidation
+from .results_typedict import (
+    ResultsTypedDict,
+    ResultValidationTypedDict,
+    SummaryTypedDict,
+    ValidationTypedDict,
+)
+
+__all__ = [
+    "BaseValidation",
+    "ResultValidationTypedDict",
+    "ResultsTypedDict",
+    "SummaryTypedDict",
+    "ValidationTypedDict",
+]
