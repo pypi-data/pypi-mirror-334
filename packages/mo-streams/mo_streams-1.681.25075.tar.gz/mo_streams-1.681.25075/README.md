1e37b31ed978df9ca8d3738bcb30c5e29117c9f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 09:04:37 +0000

    Add package mo-streams/1.681.25075/mo_streams-1.681.25075.tar.gz

---
+++ packages/mo-streams/mo_streams-1.681.25075.tar.gz/mo_streams-1.681.25075/README.md
@@ -0,0 +1,66 @@
+# More Streams!!
+
+[![PyPI Latest Release](https://img.shields.io/pypi/v/mo-streams.svg)](https://pypi.org/project/mo-streams/)
+ [![Build Status](https://github.com/klahnakoski/mo-streams/actions/workflows/build.yml/badge.svg?branch=master)](https://github.com/klahnakoski/mo-streams/actions/workflows/build.yml)
+ [![Coverage Status](https://coveralls.io/repos/github/klahnakoski/mo-streams/badge.svg?branch=dev)](https://coveralls.io/github/klahnakoski/mo-streams?branch=dev)
+[![Downloads](https://static.pepy.tech/badge/mo-streams/month)](https://pepy.tech/project/mo-streams)
+
+
+Python code is more elegant with method chaining!
+
+
+## Overview
+
+There are two families of "streams" in this library, both are lazy:
+
+1. `ByteStream` - a traditional stream of bytes intended to pipe bytes through various byte transformers, like compression, encoding and encyrption.  
+2. `ObjectStream`: An iterator/generator with a number of useful methods.
+
+### Example
+
+In this case I am iterating through all files in a tar and parsing them:
+
+    results = (
+        File("tests/so_queries/so_queries.tar.zst")
+        .content()
+        .content()
+        .exists()
+        .utf8()
+        .to_str()
+        .map(parse)
+        .to_list()
+    )
+    
+ Each of the steps constructs a generator, and no work is done until the last step
+ 
+ 
+ * `File().content()` - will unzst and untar the file content to an `ObjectStream` of file-like objects.  It is short form for `stream(File().read_bytes()).from_zst().from_tar()`
+ * The second `.content()` is applied to each of the file-like objects, returning `ByteStream` of the content for each
+ * `.exists()` - some of the files (aka directories) in the tar file do not have content, we only include content that exists.
+ * `.utf8` - convert to a `StringStream`
+ * `.to_str` - convert to a Python `str`, we trust the content is not too large
+ * `.map(parse)` - run the parser on each string
+ * `.to_list()` - a "terminator", which executes the chain and returns a Python `list` with the results
+ 
+## Project Status
+
+Alive and in use, but 
+
+* basic functions missing
+* inefficient - written using generators
+* generators not properly closed
+
+
+## Optional Reading
+
+The method chaining style has two distinct benefits
+
+* functions are in the order they are applied 
+* intermediate values need no temporary variables
+
+The detriments are the same that we find in any declarative language: Incorrect code can be difficult to debug because you can not step through it to isolate the problem.  For this reason, the majority of the code in this library is dedicated to validating the links in the function chain before they are run.
+
+### Lessons
+
+The function chaining style, called "streams" in Java or "linq" in C#, leans heavly on the strict typed nature of those langauges.  This is missing in Python, but type annotations help support this style of programming.
+
---
+++ packages/mo-streams/mo_streams-1.681.25075.tar.gz/mo_streams-1.681.25075/mo_streams/__init__.py
@@ -0,0 +1,153 @@
+# encoding: utf-8
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at https://www.mozilla.org/en-US/MPL/2.0/.
+#
+# Contact: Kyle Lahnakoski (kyle@lahnakoski.com)
+#
+
+from mo_dots import Data, is_many
+from mo_dots.utils import is_finite
+from mo_files import File
+from mo_future import first
+from mo_imports import export
+
+from mo_json import JxType, JX_TEXT
+from mo_streams._utils import Stream, Reader
+from mo_streams.byte_stream import ByteStream
+from mo_streams.empty_stream import EmptyStream
+from mo_streams.files import content, File_usingStream
+from mo_streams.function_factory import it
+from mo_streams.object_stream import ObjectStream, ERROR, WARNING, NONE
+from mo_streams.string_stream import StringStream
+from mo_streams.type_utils import Typer, CallableTyper, StreamTyper, LazyTyper
+
+
+def stream(value):
+    if isinstance(value, (dict, Data)):
+        kv = first(value.items())
+        if not kv:
+            return EmptyStream()
+        _, example = kv
+        return ObjectStream(((v, {"key": k}) for k, v in value.items()), Typer(example=example), JxType(key=JX_TEXT),)
+    elif isinstance(value, bytes):
+        return ByteStream(Reader(iter([value])))
+    elif isinstance(value, str):
+        return StringStream(iter([value]))
+    elif value == None:
+        return EmptyStream
+    elif isinstance(value, Stream):
+        return value
+    elif isinstance(value, type(range(1))):
+        return ObjectStream(((v, {}) for v in value), Typer(example=value.stop), JxType())
+    elif is_finite(value):
+        example = first(value)
+
+        def read_from_list():
+            for v in value:
+                yield v, {}
+
+        return ObjectStream(read_from_list(), Typer(example=example), JxType())
+    elif is_many(value):
+        example = first(value)
+
+        def read():
+            yield example, {}
+            for v in value:
+                yield v, {}
+
+        return ObjectStream(read(), Typer(example=example), JxType())
+    else:
+        return ObjectStream(iter([(value, {})]), Typer(example=value), JxType())
+
+
+def from_s3(bucket, key):
+    import boto3
+    return S3Object(boto3.resource('s3').Object(bucket, key))
+
+
+class S3Object:
+    def __init__(self, obj):
+        self.obj = obj
+
+    def content(self) -> ByteStream:
+        return ByteStream(self.obj.get()['Body'])
+
+
+STR_CALL = CallableTyper(return_type=str)
+BOOL_CALL = CallableTyper(return_type=bool)
+INT_CALL = CallableTyper(return_type=int)
+BYTES_CALL = CallableTyper(return_type=bytes)
+STR = Typer(python_type=str)
+
+
+ANNOTATIONS = {
+    # THE str METHODS DO NOT APPEAR TO BE IN THE str CLASS ANNOTATIONS
+    (str, "encode"): BYTES_CALL,
+    (str, "startswith"): BOOL_CALL,
+    (str, "__repr__"): STR_CALL,
+    (str, "__str__"): STR_CALL,
+    (str, "__lt__"): BOOL_CALL,
+    (str, "__le__"): BOOL_CALL,
+    (str, "__eq__"): BOOL_CALL,
+    (str, "__ne__"): BOOL_CALL,
+    (str, "__gt__"): BOOL_CALL,
+    (str, "__ge__"): BOOL_CALL,
+    (str, "__len__"): INT_CALL,
+    (str, "__add__"): STR_CALL,
+    (str, "__contains__"): BOOL_CALL,
+    (str, "encode"): BYTES_CALL,
+    (str, "replace"): STR_CALL,
+    (str, "split"): CallableTyper(return_type=StreamTyper(member_type=STR, _schema=JX_TEXT)),
+    (str, "rsplit"): CallableTyper(return_type=StreamTyper(member_type=STR, _schema=JX_TEXT)),
+    (str, "join"): STR_CALL,
+    (str, "capitalize"): STR_CALL,
+    (str, "casefold"): STR_CALL,
+    (str, "title"): STR_CALL,
+    (str, "center"): STR_CALL,
+    (str, "count"): INT_CALL,
+    (str, "expandtabs"): STR_CALL,
+    (str, "find"): INT_CALL,
+    (str, "partition"): BOOL_CALL,
+    (str, "index"): INT_CALL,
+    (str, "ljust"): STR_CALL,
+    (str, "lower"): STR_CALL,
+    (str, "lstrip"): STR_CALL,
+    (str, "rfind"): INT_CALL,
+    (str, "rindex"): INT_CALL,
+    (str, "rjust"): BOOL_CALL,
+    (str, "rstrip"): BOOL_CALL,
+    (str, "rpartition"): BOOL_CALL,
+    (str, "splitlines"): BOOL_CALL,
+    (str, "strip"): STR_CALL,
+    (str, "swapcase"): STR_CALL,
+    (str, "translate"): STR_CALL,
+    (str, "upper"): STR_CALL,
+    (str, "startswith"): BOOL_CALL,
+    (str, "endswith"): BOOL_CALL,
+    (str, "removeprefix"): STR_CALL,
+    (str, "removesuffix"): STR_CALL,
+    (str, "isascii"): BOOL_CALL,
+    (str, "islower"): BOOL_CALL,
+    (str, "isupper"): BOOL_CALL,
+    (str, "istitle"): BOOL_CALL,
+    (str, "isspace"): BOOL_CALL,
+    (str, "isdecimal"): BOOL_CALL,
+    (str, "isdigit"): BOOL_CALL,
+    (str, "isnumeric"): BOOL_CALL,
+    (str, "isalpha"): BOOL_CALL,
+    (str, "isalnum"): BOOL_CALL,
+    (str, "isidentifier"): BOOL_CALL,
+    (str, "isprintable"): BOOL_CALL,
+    (File_usingStream, "content"): CallableTyper(return_type=ByteStream),
+    (File, "content"): CallableTyper(return_type=ByteStream),
+    (ByteStream, "utf8"): CallableTyper(return_type=StringStream),
+    (StringStream, "lines"): CallableTyper(return_type=StreamTyper(member_type=STR, _schema=JxType())),
+    (ByteStream, "lines"): CallableTyper(return_type=StreamTyper(member_type=STR, _schema=JxType())),
+    (ObjectStream, "map"): CallableTyper(return_type=StreamTyper(member_type=LazyTyper(), _schema=JxType())),
+}
+
+export("mo_streams.object_stream", stream)
+export("mo_streams.type_utils", ANNOTATIONS)
+export("mo_streams.function_factory", ANNOTATIONS)
