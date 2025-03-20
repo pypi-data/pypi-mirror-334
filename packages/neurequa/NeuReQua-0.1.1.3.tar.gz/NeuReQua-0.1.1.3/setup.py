ac8b76989cc02ffdbce8a4807cf7e7a90fa3e5f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 15:05:17 +0000

    Add package neurequa/0.1.1.3/NeuReQua-0.1.1.3.tar.gz

---
+++ packages/neurequa/NeuReQua-0.1.1.3.tar.gz/NeuReQua-0.1.1.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='NeuReQua',
+    version='0.1.1.3',    
+    description='Python package to monitor micro-recording quality in humans',
+    url='https://github.com/dornierv/NEUREQUA',
+    author='Vincent Dornier',
+    author_email='vincent.dornier@cnrs.fr',
+    license='MIT',
+    packages=['NEUREQUA'],
+    install_requires=['neo',
+                      'numpy',  
+                      'scipy',
+                      'matplotlib',
+                      'seaborn',
+                      'mne',
+                      'pandas',                   
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Programming Language :: Python :: 3.12',
+    ],
+)
