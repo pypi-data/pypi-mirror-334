916876598137fde5024a315ee304ff5c72803520
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 11:04:32 +0000

    Add package BigGo-MCP-Server/0.1.27/biggo_mcp_server-0.1.27-py3-none-any.whl

---
+++ packages/BigGo-MCP-Server/biggo_mcp_server-0.1.27-py3-none-any.whl/biggo_mcp_server/types/api_ret/price_history.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+
+
+from pydantic import BaseModel, RootModel
+
+
+class Days(BaseModel):
+    days: int
+    max_price: float
+    min_price: float
+    up_times: int
+    down_times: int
+    last_continue_times: int
+    last_increase_status: bool
+
+
+class Statistics(BaseModel):
+    days730: Days | None = None
+    days365: Days | None = None
+    days180: Days | None = None
+    days90: Days | None = None
+    days30: Days | None = None
+    days7: Days | None = None
+
+
+class PriceHistoryAttributes(BaseModel):
+    symbol: str
+    currency: str
+    # nindex: str
+    # oid: str
+    current_price: float
+    # datetime_format: str
+    # price_history: List[PriceHistoryItem]
+    # purl: str | None = None
+    url: str | None = None
+    title: str
+    nindex_name: str
+    # icon: str
+    statistics: Statistics
+    state: str
+
+
+class PriceHistoryAPIRet(RootModel[dict[str, PriceHistoryAttributes]]):
+    root: dict[str, PriceHistoryAttributes]
