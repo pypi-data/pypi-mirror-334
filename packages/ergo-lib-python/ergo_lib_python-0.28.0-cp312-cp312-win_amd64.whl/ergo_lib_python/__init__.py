dd689d82a8e54d7ee53b156088dd62ac05eafd6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 20:06:59 +0000

    Add package ergo-lib-python/0.28.0/ergo_lib_python-0.28.0-cp312-cp312-win_amd64.whl

---
+++ packages/ergo-lib-python/ergo_lib_python-0.28.0-cp312-cp312-win_amd64.whl/ergo_lib_python/__init__.py
@@ -0,0 +1,7 @@
+from .ergo_lib_python import *
+
+__doc__ = ergo_lib_python.__doc__
+if hasattr(ergo_lib_python, "__all__"):
+    __all__ = ergo_lib_python.__all__
+if hasattr(ergo_lib_python, "__version__"):
+    __version__ = ergo_lib_python.__version__
