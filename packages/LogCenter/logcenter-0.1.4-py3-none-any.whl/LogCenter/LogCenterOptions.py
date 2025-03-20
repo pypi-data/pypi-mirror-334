9b2f7306e66e277920ed1fe0437fe0a11b1d3fc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 09:03:56 +0000

    Add package LogCenter/0.1.4/logcenter-0.1.4-py3-none-any.whl

---
+++ packages/LogCenter/logcenter-0.1.4-py3-none-any.whl/LogCenter/LogCenterOptions.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+import uuid
+
+@dataclass
+class LogCenterOptions:
+
+    url: str
+    table: str
+    token: str
+    trace_id: str = None
+
+    def __init__(self, url: str, table: str, token: str, consoleLog: bool = True, consoleLogEntireObject: bool = False):
+        """Configura es para conex o com o LogCenter."""
+        self.url = url
+        self.table = table
+        self.token = token
+        self.consoleLog = consoleLog
+        self.consoleLogEntireObject = consoleLogEntireObject
