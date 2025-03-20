939916bce2b32dbf21c5005515dc01744cd7b732
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 14:05:26 +0000

    Add package dbt-maxcompute/1.9.0a9/dbt_maxcompute-1.9.0a9.tar.gz

---
+++ packages/dbt-maxcompute/dbt_maxcompute-1.9.0a9.tar.gz/dbt_maxcompute-1.9.0a9/dbt/adapters/maxcompute/context.py
@@ -0,0 +1,8 @@
+GLOBAL_SQL_HINTS = {
+    "odps.sql.allow.fullscan": "true",
+    "odps.sql.select.output.format": "csv",
+    "odps.sql.submit.mode": "script",
+    "odps.sql.allow.cartesian": "true",
+    "odps.sql.timezone": "GMT",
+    "odps.sql.allow.schema.evolution": "true",
+}
