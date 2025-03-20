4511410b56ae8cad4c851dced4ca2c4ac2b6a434
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 01:31:16 +0000

    Add package leaderbot/0.3.2/leaderbot-0.3.2-py3-none-any.whl

---
+++ packages/leaderbot/leaderbot-0.3.2-py3-none-any.whl/leaderbot/__init__.py
@@ -0,0 +1,22 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# SPDX-FileType: SOURCE
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the license found in the LICENSE.txt file in the root
+# directory of this source tree.
+
+
+from .data import load, convert, split
+from .models import BradleyTerry, BradleyTerryScaled, \
+    BradleyTerryScaledR, BradleyTerryScaledRIJ, RaoKupper, RaoKupperScaled, \
+    RaoKupperScaledR, RaoKupperScaledRIJ, Davidson, DavidsonScaled, \
+    DavidsonScaledR, DavidsonScaledRIJ
+from .evaluate import model_selection, goodness_of_fit, generalization, \
+    compare_ranks
+
+__all__ = ['load', 'convert', 'split', 'BradleyTerry', 'BradleyTerryScaled',
+           'BradleyTerryScaledR', 'BradleyTerryScaledRIJ', 'RaoKupper',
+           'RaoKupperScaled', 'RaoKupperScaledR', 'RaoKupperScaledRIJ',
+           'Davidson', 'DavidsonScaled', 'DavidsonScaledR',
+           'DavidsonScaledRIJ', 'model_selection', 'goodness_of_fit',
+           'generalization', 'compare_ranks']
