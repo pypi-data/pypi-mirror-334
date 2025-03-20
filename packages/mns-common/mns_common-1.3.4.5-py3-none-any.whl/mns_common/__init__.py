743413ace0a81a6dec467472b04117a2ad090e89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 21:04:49 +0000

    Add package mns-common/1.3.4.5/mns_common-1.3.4.5-py3-none-any.whl

---
+++ packages/mns-common/mns_common-1.3.4.5-py3-none-any.whl/mns_common/__init__.py
@@ -0,0 +1,13 @@
+import sys
+import os
+
+file_path = os.path.abspath(__file__)
+end = file_path.index('mns') + 14
+project_path = file_path[0:end]
+sys.path.append(project_path)
+
+import warnings
+import pandas as pd
+
+# 禁用 SettingWithCopyWarning
+warnings.filterwarnings('ignore', category=pd.errors.SettingWithCopyWarning)
