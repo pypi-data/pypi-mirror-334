b26b2c24cb47526b87de018d7839d1d43901e7dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 12:06:16 +0000

    Add package kcpp/0.1/kcpp-0.1.tar.gz

---
+++ packages/kcpp/kcpp-0.1.tar.gz/kcpp-0.1/setup.py
@@ -0,0 +1,24 @@
+import os.path
+import re
+import setuptools
+
+
+def find_version(filename):
+    with open(filename) as f:
+        text = f.read()
+    match = re.search(r"^_version_str = '(.*)'$", text, re.MULTILINE)
+    if not match:
+        raise RuntimeError('cannot find version')
+    return match.group(1)
+
+
+tld = os.path.abspath(os.path.dirname(__file__))
+version = find_version(os.path.join(tld, 'kcpp', '__init__.py'))
+
+
+setuptools.setup(
+    version=version,
+    python_requires='>=3.8',
+    packages=['kcpp'],
+    download_url=('https://github.com/kyuupichan/kcpp/archive/{version}.tar.gz'),
+)
