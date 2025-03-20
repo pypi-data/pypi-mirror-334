316dba4b0df5bb0fe94a180f630d93250bfe8e9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 12:06:29 +0000

    Add package servicex-local/1.0.0b4/servicex_local-1.0.0b4-py3-none-any.whl

---
+++ packages/servicex-local/servicex_local-1.0.0b4-py3-none-any.whl/servicex_local/__init__.py
@@ -0,0 +1,4 @@
+from .science_images import WSL2ScienceImage, DockerScienceImage  # noqa: F401
+from .codegen import LocalXAODCodegen, DockerCodegen  # noqa: F401
+from .adaptor import SXLocalAdaptor  # noqa: F401
+from .deliver import deliver  # noqa: F401
