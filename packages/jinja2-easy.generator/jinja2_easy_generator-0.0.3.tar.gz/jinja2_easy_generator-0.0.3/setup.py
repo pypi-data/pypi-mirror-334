7e12d0a52948b7080a46b4c5ddaa3214769f9cc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 20:06:29 +0000

    Add package jinja2-easy.generator/0.0.3/jinja2_easy_generator-0.0.3.tar.gz

---
+++ packages/jinja2-easy.generator/jinja2_easy_generator-0.0.3.tar.gz/jinja2_easy_generator-0.0.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name="jinja2-easy.generator",
+    version="0.0.3",
+    description="Jinja2 easy generator",
+    summary="Jinja2 easy generator",
+    license="GPLv3",
+    url="https://github.com/huakim/python-jinja2_easy",
+    packages=["jinja2_easy"],
+    install_requires=["platformdirs", "jinja2"],
+)
