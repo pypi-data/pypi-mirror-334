252eaece3bfd5167c049ab529c738521d8dfb580
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 06:03:54 +0000

    Add package aitop/0.5.6/aitop-0.5.6.tar.gz

---
+++ packages/aitop/aitop-0.5.6.tar.gz/aitop-0.5.6/aitop/version.py
@@ -0,0 +1,5 @@
+"""AITop version information."""
+
+__version__ = "0.5.6"  # Updated version with latest improvements
+__author__ = "Alexander Warth"
+__copyright__ = f"Copyright (c) 2025 {__author__}"
