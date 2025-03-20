7d7cf3e96827744d081b89dbf1bc2e0e827c4fea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 18:05:30 +0000

    Add package fonarmeister-latex-helper/0.1.0/fonarmeister_latex_helper-0.1.0.tar.gz

---
+++ packages/fonarmeister-latex-helper/fonarmeister_latex_helper-0.1.0.tar.gz/fonarmeister_latex_helper-0.1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fonarmeister_latex_helper",
+    version="0.1.0",
+    author="Ilya",
+    packages=find_packages(),
+    python_requires=">=3.10",
+)
