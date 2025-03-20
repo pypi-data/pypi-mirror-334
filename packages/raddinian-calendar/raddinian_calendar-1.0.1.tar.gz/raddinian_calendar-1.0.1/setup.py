6b0a0218f95722721adde4295db12859997879e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 09:04:21 +0000

    Add package raddinian-calendar/1.0.1/raddinian_calendar-1.0.1.tar.gz

---
+++ packages/raddinian-calendar/raddinian_calendar-1.0.1.tar.gz/raddinian_calendar-1.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="raddinian-calendar",
+    version="1.0.1",
+    description="A module to convert between Raddinian and Gregorian calendars.",
+    author="Violet Radd",
+    author_email="metatronzero199@gmail.com",
+    url="https://github.com/violetradd/raddinian-calendar",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
