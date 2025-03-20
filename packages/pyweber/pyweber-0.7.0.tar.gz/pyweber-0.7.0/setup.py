5a0292750177149ed2614ae7a76f2cd8652dffe1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 09:05:13 +0000

    Add package pyweber/0.7.0/pyweber-0.7.0.tar.gz

---
+++ packages/pyweber/pyweber-0.7.0.tar.gz/pyweber-0.7.0/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyweber',
+    version='0.7.0',
+    packages=find_packages(include=['pyweber', 'pyweber.*']),
+    install_requires=[
+        'websockets',
+        'watchdog',
+        'lxml'
+    ],
+    entry_points={
+        'console_scripts': [
+            'pyweber=pyweber.cli.commands:app',
+        ],
+    },
+    package_data={
+        "pyweber": ["static/**/*"]
+    },
+    author='DevPythonMZ',
+    author_email='zoidycine@egmail.com',
+    description='A lightweight Python framework for building and managing web applications.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://pyweber.readthedocs.io/en/latest',
+    project_urls={
+        'Documentation': 'https://pyweber.readthedocs.io/en/latest',
+        'Source Code': 'https://github.com/webtechmoz/pyweber',
+        'Issue Tracker': 'https://github.com/webtechmoz/pyweber/issues',
+        'PyPI': 'https://pypi.org/project/pyweber/',
+        'Youtube': 'https://youtube.com/@devpythonMZ'
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10',
+)
