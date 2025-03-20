88f12036149ed386ca49096fba685467e39744d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 17:05:27 +0000

    Add package arcosparse/0.2.0/arcosparse-0.2.0-py3-none-any.whl

---
+++ packages/arcosparse/arcosparse-0.2.0-py3-none-any.whl/arcosparse/__init__.py
@@ -0,0 +1,13 @@
+from arcosparse.models import UserConfiguration
+from arcosparse.subsetter import (
+    get_platforms_names,
+    subset_and_return_dataframe,
+    subset_and_save,
+)
+
+__all__ = [
+    "get_platforms_names",
+    "subset_and_return_dataframe",
+    "subset_and_save",
+    "UserConfiguration",
+]
