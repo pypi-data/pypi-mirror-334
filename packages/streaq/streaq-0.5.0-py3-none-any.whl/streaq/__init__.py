a7d30ced0df9974b25e6a21eb041b385fb801574
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 17:05:43 +0000

    Add package streaq/0.5.0/streaq-0.5.0-py3-none-any.whl

---
+++ packages/streaq/streaq-0.5.0-py3-none-any.whl/streaq/__init__.py
@@ -0,0 +1,22 @@
+import logging
+
+VERSION = "0.5.0"
+__version__ = VERSION
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# ruff: noqa: E402
+
+from .task import StreaqRetry, TaskStatus
+from .types import WrappedContext
+from .utils import StreaqError
+from .worker import Worker
+
+__all__ = [
+    "StreaqError",
+    "StreaqRetry",
+    "TaskStatus",
+    "Worker",
+    "WrappedContext",
+]
