1eac30573e1dcc14f80cc4796252855b589c96de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 17:05:31 +0000

    Add package hse-advanced-python-hw2-maksimkaSh/0.0.1/hse_advanced_python_hw2_maksimkash-0.0.1.tar.gz

---
+++ packages/hse-advanced-python-hw2-maksimkaSh/hse_advanced_python_hw2_maksimkash-0.0.1.tar.gz/hse_advanced_python_hw2_maksimkash-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+    name='hse_advanced_python_hw2_maksimkaSh',
+    version='0.0.1',
+    author='Maksim Shtein',
+    author_email='shteinmaks@gmail.com',
+    description='Latex library',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
