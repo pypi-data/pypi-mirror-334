44c3474e8baa8d554d89d13e74abe104e58e8366
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 11:05:13 +0000

    Add package fastled/1.2.46/fastled-1.2.46.tar.gz

---
+++ packages/fastled/fastled-1.2.46.tar.gz/fastled-1.2.46/compiler/init_runtime.py
@@ -0,0 +1,75 @@
+import glob
+import os
+import warnings
+from concurrent.futures import ThreadPoolExecutor
+from pathlib import Path
+
+HERE = Path(__file__).parent
+
+
+_COMPILER_DIR = Path("/js/compiler")
+_FASTLED_COMPILER_DIR = Path("/js/fastled/src/platforms/wasm/compiler")
+
+
+def copy_task(src: str | Path) -> None:
+    src = Path(src)
+    if "entrypoint.sh" in str(src):
+        return
+    link_dst = Path("/js") / src.name
+
+    # Handle shell scripts
+    if src.suffix == ".sh":
+        os.system(f"dos2unix {src} && chmod +x {src}")
+
+    # if link exists, remove it
+    if link_dst.exists():
+        print(f"Removing existing link {link_dst}")
+        try:
+            os.remove(link_dst)
+        except Exception as e:
+            warnings.warn(f"Failed to remove {link_dst}: {e}")
+
+    if not link_dst.exists():
+        print(f"Linking {src} to {link_dst}")
+        try:
+            os.symlink(str(src), str(link_dst))
+        except FileExistsError:
+            print(f"Target {link_dst} already exists")
+    else:
+        print(f"Target {link_dst} already exists")
+
+
+def make_links() -> None:
+    # Define file patterns to include
+    patterns = [
+        "*.h",
+        "*.hpp",
+        "*.cpp",
+        "*.py",
+        "*.css",
+        "*.sh",
+        "*.ino",
+        "*.ini",
+        "*.txt",
+    ]
+
+    # Get all matching files in compiler directory
+    files = []
+    for pattern in patterns:
+        files.extend(glob.glob(str(_COMPILER_DIR / pattern)))
+
+    for pattern in patterns:
+        files.extend(glob.glob(str(_FASTLED_COMPILER_DIR / pattern)))
+
+    # Process files in parallel using ThreadPoolExecutor
+    with ThreadPoolExecutor(max_workers=16) as executor:
+        executor.map(copy_task, files)
+
+
+def init_runtime() -> None:
+    os.chdir(str(HERE))
+    make_links()
+
+
+if __name__ == "__main__":
+    init_runtime()
