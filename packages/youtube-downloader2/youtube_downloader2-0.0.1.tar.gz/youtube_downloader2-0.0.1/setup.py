7b949e9aa1e058047d8f1301c30f2f50d2324451
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 13:10:34 +0000

    Add package youtube-downloader2/0.0.1/youtube_downloader2-0.0.1.tar.gz

---
+++ packages/youtube-downloader2/youtube_downloader2-0.0.1.tar.gz/youtube_downloader2-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="youtube_downloader2",
+    version="0.0.1",
+    description="A package to download youtube videos and audio from youtube",
+    author="Sino Farmonov",
+    url="https://github.com/sinofarmonovzfkrvjl/youtube-downloader",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
+    install_requires=["requests"]
+)
