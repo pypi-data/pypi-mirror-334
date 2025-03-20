33a5c3da8c2561ab3edfd6ec8e3be55b31c519e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 09:04:14 +0000

    Add package luziip/0.2/luziip-0.2-py3-none-any.whl

---
+++ packages/luziip/luziip-0.2-py3-none-any.whl/luziip/luziip.py
@@ -0,0 +1,12 @@
+# luziip.py
+import requests
+
+def fetch_ip_info(ip):
+    url = f'https://ip-api.com/json/{ip}'
+    response = requests.get(url)
+    data = response.json()
+
+    if data['status'] == 'fail':
+        return f"Error: {data['message']}"
+
+    return data
