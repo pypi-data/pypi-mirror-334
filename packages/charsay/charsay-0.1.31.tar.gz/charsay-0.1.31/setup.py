49f93054135dae562650a221ad9fdea31ceb697b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 14:05:31 +0000

    Add package charsay/0.1.31/charsay-0.1.31.tar.gz

---
+++ packages/charsay/charsay-0.1.31.tar.gz/charsay-0.1.31/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='charsay',
+    version='0.1.31',
+    packages=find_packages(),
+    author='Kishan S',
+    author_email='senthilkumarkishan@gmail.com',
+    description='A simple Python package that generates ASCII art of characters saying a given string.',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url='https://github.com/kish-0/charsay',
+    license='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
