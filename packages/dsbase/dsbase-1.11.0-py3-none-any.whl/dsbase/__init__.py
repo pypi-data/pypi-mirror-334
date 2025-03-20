58ca58a69744ff6e3f002f764d10db44d30181a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 09:04:06 +0000

    Add package dsbase/1.11.0/dsbase-1.11.0-py3-none-any.whl

---
+++ packages/dsbase/dsbase-1.11.0-py3-none-any.whl/dsbase/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+from dsbase.common import Singleton
+from dsbase.log import LocalLogger, TimeAwareLogger
+from dsbase.text import Text
+from dsbase.time import TZ, TimeParser
+from dsbase.tools import configure_traceback
