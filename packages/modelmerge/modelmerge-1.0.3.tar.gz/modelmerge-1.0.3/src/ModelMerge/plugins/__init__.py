2306319bf1ebb4a83073d6776059fa2c6fdf727c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 06:04:25 +0000

    Add package modelmerge/1.0.3/modelmerge-1.0.3.tar.gz

---
+++ packages/modelmerge/modelmerge-1.0.3.tar.gz/modelmerge-1.0.3/src/ModelMerge/plugins/__init__.py
@@ -0,0 +1,24 @@
+from .websearch import *
+from .tarvel import *
+from .flight import *
+from .version import *
+from .today import *
+from .image import *
+from .run_python import *
+from .arXiv import *
+
+from .config import *
+
+__all__ = [
+    'get_city_tarvel_info',
+    'get_Round_trip_flight_price',
+    'get_date_time_weekday',
+    'get_version_info',
+    'get_search_results',
+    'Web_crawler',
+    'generate_image',
+    'run_python_script',
+    'get_tools_result_async',
+    'download_read_arxiv_pdf',
+    'PLUGINS',
+]
