6b0e4f690459a86da74902f145b4bd70565c6194
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 18:05:42 +0000

    Add package mitol-django-google-sheets-refunds/2025.3.17/mitol_django_google_sheets_refunds-2025.3.17-py3-none-any.whl

---
+++ packages/mitol-django-google-sheets-refunds/mitol_django_google_sheets_refunds-2025.3.17-py3-none-any.whl/mitol/google_sheets_refunds/apps.py
@@ -0,0 +1,18 @@
+"""Google Sheets app AppConfigs"""
+
+import os
+
+from mitol.common.apps import BaseApp
+
+
+class GoogleSheetsRefundsApp(BaseApp):
+    """Default configuration for the Google Sheets Refunds app"""
+
+    name = "mitol.google_sheets_refunds"
+    label = "google_sheets_refunds"
+    verbose_name = "Google Sheets Refunds"
+
+    required_settings = []
+
+    # necessary because this is a namespaced app
+    path = os.path.dirname(os.path.abspath(__file__))  # noqa: PTH100, PTH120
