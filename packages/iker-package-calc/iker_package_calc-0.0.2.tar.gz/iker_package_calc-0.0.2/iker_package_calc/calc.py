ba4b3ffdc398dd366116e41f8449dac8caa20158
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 23:05:34 +0000

    Add package iker-package-calc/0.0.2/iker_package_calc-0.0.2.tar.gz

---
+++ packages/iker-package-calc/iker_package_calc-0.0.2.tar.gz/iker_package_calc-0.0.2/iker_package_calc/calc.py
@@ -0,0 +1,9 @@
+from iker_package_calc.const import PI
+
+
+def sum(num1: float, num2: float) -> float:
+    return num1 + num2
+
+
+def area_circle(radio: float) -> float:
+    return radio * radio * PI
