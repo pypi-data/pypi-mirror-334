740e37864e284824b768ba6a76fa43b4eb6bc30c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 17:05:20 +0000

    Add package refrapy/0.2.4.1/refrapy-0.2.4.1-py3-none-any.whl

---
+++ packages/refrapy/refrapy-0.2.4.1-py3-none-any.whl/refrapy/__init__.py
@@ -0,0 +1,6 @@
+from importlib.metadata import version
+
+from refrapy.refrainv import Refrainv  # noqa: F401
+from refrapy.refrapick import Refrapick  # noqa: F401
+
+__version__ = version("refrapy")
