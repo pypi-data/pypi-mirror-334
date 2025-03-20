a14b5bcb503ff7300155932772ba40c0821c5d98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 13:10:17 +0000

    Add package forbidden/13.2/forbidden-13.2.tar.gz

---
+++ packages/forbidden/forbidden-13.2.tar.gz/forbidden-13.2/src/stresser/utils/config.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+APP_VERSION = "v13.2"
+
+USER_AGENT = "Stresser/13.2"
+
+def banner():
+	"""
+	Display the banner.
+	"""
+	print("########################################################################")
+	print("#                                                                      #")
+	print("#                            Stresser v13.2                            #")
+	print("#                                by Ivan Sincek                        #")
+	print("#                                                                      #")
+	print("# Bypass 4xx HTTP response status codes with stress testing.           #")
+	print("# GitHub repository at github.com/ivan-sincek/forbidden.               #")
+	print("#                                                                      #")
+	print("########################################################################")
