e35b6681a655eedfeb46859647ffd958b818ad64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 02:28:45 +0000

    Add package litecli/1.15.0/litecli-1.15.0-py3-none-any.whl

---
+++ packages/litecli/litecli-1.15.0-py3-none-any.whl/litecli/packages/special/__init__.py
@@ -0,0 +1,15 @@
+# ruff: noqa
+
+__all__ = []
+
+
+def export(defn):
+    """Decorator to explicitly mark functions that are exposed in a lib."""
+    globals()[defn.__name__] = defn
+    __all__.append(defn.__name__)
+    return defn
+
+
+from . import dbcommands
+from . import iocommands
+from . import llm
