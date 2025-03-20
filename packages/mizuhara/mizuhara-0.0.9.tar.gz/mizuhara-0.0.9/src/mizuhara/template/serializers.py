6929e2302dd5536c58c7957f62a45f6b6ae035f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 11:05:06 +0000

    Add package mizuhara/0.0.9/mizuhara-0.0.9.tar.gz

---
+++ packages/mizuhara/mizuhara-0.0.9.tar.gz/mizuhara-0.0.9/src/mizuhara/template/serializers.py
@@ -0,0 +1,8 @@
+# please import handlers classes in core.handlers
+# and inherit one of them to your custom handler serializer classes.
+from core.handlers.handlers import *
+from core.routes import CLIENT_INFO
+from project.apis import *
+
+
+# please write down your code below.
