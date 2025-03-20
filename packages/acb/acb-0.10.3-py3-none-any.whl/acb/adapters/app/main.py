977d7a68ee02a936d5b9366be78d7a2ee2256dd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 12:05:51 +0000

    Add package acb/0.10.3/acb-0.10.3-py3-none-any.whl

---
+++ packages/acb/acb-0.10.3-py3-none-any.whl/acb/adapters/app/main.py
@@ -0,0 +1,48 @@
+from time import perf_counter
+
+from acb.adapters import import_adapter
+from acb.config import Config
+from acb.depends import depends
+
+from ._base import AppBase, AppBaseSettings
+
+main_start = perf_counter()
+
+Logger = import_adapter()
+
+
+class AppSettings(AppBaseSettings): ...
+
+
+class App(AppBase):
+    async def init(self) -> None:
+        self.logger.info("Application starting...")
+
+        # put app startup code here
+
+        async def post_startup() -> None:
+            if not self.config.deployed:
+                from aioconsole import aprint
+                from pyfiglet import Figlet
+
+                fig = Figlet(font="slant", width=90, justify="center")
+                await aprint(f"\n\n{fig.renderText(self.config.app.name.upper())}\n")
+            if not self.config.debug.production and self.config.deployed:
+                self.logger.info("Entering production mode...")
+
+        await post_startup()
+        main_start_time = perf_counter() - main_start
+        self.logger.info(f"App started in {main_start_time} s")
+
+    @depends.inject
+    async def main(
+        self,
+        config: Config = depends(),
+        logger: Logger = depends(),
+    ) -> None:
+        ...
+
+        # put app main code here
+
+
+depends.set(App)
---
+++ packages/acb/acb-0.10.3-py3-none-any.whl/acb/depends.py
@@ -0,0 +1,28 @@
+import typing as t
+
+from bevy import dependency, get_repository
+from bevy import inject as inject_dependency
+
+
+class Depends:
+    @staticmethod
+    def inject(func: t.Callable[..., t.Any]) -> t.Callable[..., t.Any]:
+        return inject_dependency(func)
+
+    @staticmethod
+    def set(class_: t.Any) -> t.Any:
+        return get_repository().set(class_, class_())
+
+    @staticmethod
+    def get(class_: t.Any) -> t.Any:
+        if isinstance(class_, str):
+            from acb.adapters import import_adapter
+
+            class_ = import_adapter(class_)
+        return t.cast(class_, get_repository().get(class_))
+
+    def __call__(self, *args: t.Any, **kwargs: t.Any) -> t.Any:
+        return dependency()
+
+
+depends = Depends()
