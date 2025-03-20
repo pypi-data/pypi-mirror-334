fe1f89fc19506f5ca94997fe3ffbecc2c1c3135d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 16:05:12 +0000

    Add package backpack-exchange-sdk/1.0.31/backpack_exchange_sdk-1.0.31-py3-none-any.whl

---
+++ packages/backpack-exchange-sdk/backpack_exchange_sdk-1.0.31-py3-none-any.whl/enums/RequestEnums.py
@@ -0,0 +1,57 @@
+from enum import Enum
+
+
+class TimeInForce(Enum):
+    GTC = "GTC"
+    IOC = "IOC"
+    FOK = "FOK"
+
+
+class Side(Enum):
+    BID = "Bid"
+    ASK = "Ask"
+
+
+class SelfTradePrevention(Enum):
+    REJECT_TAKER = "RejectTaker"
+    REJECT_MAKER = "RejectMaker"
+    REJECT_BOTH = "RejectBoth"
+    ALLOW = "Allow"
+
+
+class OrderType(Enum):
+    MARKET = "Market"
+    LIMIT = "Limit"
+
+
+class BorrowLendEventType(Enum):
+    BORROW = "Borrow"
+    BORROW_REPAY = "BorrowRepay"
+    LEND = "Lend"
+    LEND_REDEEM = "LendRedeem"
+
+
+class InterestPaymentSource(Enum):
+    UNREALIZED_PNL = "UnrealizedPnl"
+    BORROW_LEND = "BorrowLend"
+
+
+class BorrowLendSide(Enum):
+    BORROW = "Borrow"
+    LEND = "Lend"
+
+
+class BorrowLendPositionState(Enum):
+    OPEN = "Open"
+    CLOSED = "Closed"
+
+
+class SettlementSourceFilter(Enum):
+    BACKSTOP_LIQUIDATION = "BackstopLiquidation"
+    CULLED_BORROW_INTEREST = "CulledBorrowInterest"
+    CULLED_REALIZE_PNL = "CulledRealizePnl"
+    CULLED_REALIZE_PNL_BOOK_UTILIZATION = "CulledRealizePnlBookUtilization"
+    FUNDING_PAYMENT = "FundingPayment"
+    REALIZE_PNL = "RealizePnl"
+    TRADING_FEES = "TradingFees"
+    TRADING_FEES_SYSTEM = "TradingFeesSystem"
