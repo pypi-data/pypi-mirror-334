41f95abedbf2b96da5fb381711522fb0541b1fca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 15:05:23 +0000

    Add package ep-stats/2.5.6/ep_stats-2.5.6.tar.gz

---
+++ packages/ep-stats/ep_stats-2.5.6.tar.gz/ep_stats-2.5.6/tests/epstats/toolkit/test_parser.py
@@ -0,0 +1,382 @@
+import numpy as np
+import pandas as pd
+import pytest
+from numpy.testing import assert_almost_equal
+from pyparsing import ParseException
+
+from src.epstats.toolkit.parser import MultOp, Parser
+
+
+def test_evaluate_agg():
+    variants = ["a", "b", "c", "d"]
+    goals = ["click", "exposure", "conversion", "refund"]
+    ln = len(variants) * len(goals)
+
+    goals = pd.DataFrame(
+        {
+            "exp_id": "test",
+            "exp_variant_id": np.repeat(variants, len(goals)),
+            "unit_type": "test_unit_type",
+            "agg_type": "unit",
+            "goal": goals * len(variants),
+            "count": 1000 + np.random.randint(-100, 100, ln),
+            "sum_sqr_count": 1000 + np.random.randint(-100, 100, ln),
+            "sum_value": 10 + np.random.normal(0, 3, ln),
+            "sum_sqr_value": 100 + np.random.normal(0, 10, ln),
+        }
+    )
+
+    parser = Parser(
+        "count(test_unit_type.unit.click)",
+        "count(test_unit_type.unit.exposure)",
+    )
+    assert_count_value(
+        parser.evaluate_agg(goals),
+        goals[goals.goal == "exposure"]["count"].values,
+        goals[goals.goal == "click"]["count"].values,
+        goals[goals.goal == "click"]["sum_sqr_count"].values,
+    )
+
+    parser = Parser(
+        "value(test_unit_type.unit.conversion) - value(test_unit_type.unit.refund)",
+        "count(test_unit_type.unit.exposure)",
+    )
+
+    conversion_sqr_value = goals[goals.goal == "conversion"]["sum_sqr_value"].to_numpy()
+    refund_sqr_value = goals[goals.goal == "refund"]["sum_sqr_value"].to_numpy()
+    conversion_value = goals[goals.goal == "conversion"]["sum_value"].to_numpy()
+    refund_value = goals[goals.goal == "refund"]["sum_value"].to_numpy()
+    assert_count_value(
+        parser.evaluate_agg(goals),
+        goals[goals.goal == "exposure"]["count"],
+        conversion_value - refund_value,
+        conversion_sqr_value - refund_sqr_value,
+    )
+
+    parser = Parser(
+        "value(test_unit_type.unit.conversion) ~ value(test_unit_type.unit.refund)",
+        "count(test_unit_type.unit.exposure)",
+    )
+    assert_count_value(
+        parser.evaluate_agg(goals),
+        goals[goals.goal == "exposure"]["count"],
+        conversion_value - refund_value,
+        conversion_sqr_value + refund_sqr_value,
+    )
+
+    parser = Parser(
+        "value(test_unit_type.unit.conversion)",
+        "count(test_unit_type.unit.exposure) / 1000",
+    )
+    assert_count_value(
+        parser.evaluate_agg(goals),
+        goals[goals.goal == "exposure"]["count"].to_numpy() / 1000,
+        conversion_value,
+        conversion_sqr_value,
+    )
+
+    parser = Parser(
+        """
+        value(test_unit_type.unit.conversion)
+        - value(test_unit_type.unit.refund)
+        - value(test_unit_type.unit.refund)
+        """,
+        "count(test_unit_type.unit.exposure)",
+    )
+    assert_count_value(
+        parser.evaluate_agg(goals),
+        goals[goals.goal == "exposure"]["count"],
+        conversion_value - refund_value - refund_value,
+        conversion_sqr_value - refund_sqr_value - refund_sqr_value,
+    )
+
+
+def test_evaluate_agg_dimensional():
+    goals = pd.DataFrame(
+        {
+            "exp_id": "testt",
+            "exp_variant_id": ["a", "a", "a", "b", "b", "b"],
+            "unit_type": "test_unit_type",
+            "agg_type": "unit",
+            "goal": ["exposure", "click", "click"] * 2,
+            "count": 1000 + np.random.randint(-100, 100, 6),
+            "sum_sqr_count": 1000 + np.random.randint(-100, 100, 6),
+            "sum_value": 10 + np.random.normal(0, 3, 6),
+            "sum_sqr_value": 100 + np.random.normal(0, 10, 6),
+            "product": ["", "", "p_1"] * 2,
+            "country": ["", "", "US"] * 2,
+        }
+    )
+
+    parser = Parser(
+        "count(test_unit_type.unit.click(country=US, product=p_1))",
+        "count(test_unit_type.unit.exposure)",
+    )
+
+    click_mask = (goals["goal"] == "click") & (goals["country"] == "US") & (goals["product"] == "p_1")
+
+    assert_count_value(
+        parser.evaluate_agg(goals),
+        goals[goals["goal"] == "exposure"]["count"].values,
+        goals[click_mask]["count"].values,
+        goals[click_mask]["sum_sqr_count"].values,
+    )
+
+
+def test_fail_if_unknown_function():
+    with pytest.raises(ParseException):
+        parser = Parser(
+            "foo(test_unit_type.unit.click) * count(test_unit_type.unit.bar)",
+            "count(test_unit_type.global.exposure)",
+        )
+        parser.evaluate_agg(pd.DataFrame({"foo": np.ones(10)}))
+
+
+def test_fail_if_unknown_unit_type():
+    with pytest.raises(KeyError):
+        parser = Parser(
+            "count(unknown_unit_type.unit.click) * count(test_unit_type.unit.bar)",
+            "count(test_unit_type.global.exposure)",
+        )
+        parser.evaluate_agg(pd.DataFrame({"foo": np.ones(10)}))
+
+
+def test_fail_if_incorrect_syntax():
+    with pytest.raises(ParseException):
+        parser = Parser(
+            " / count(test_unit_type.global.exposure)",
+            "count(test_unit_type.unit.bar)",
+        )
+        parser.evaluate_agg(pd.DataFrame({"foo": np.ones(10)}))
+
+
+@pytest.mark.parametrize(
+    "parser, expected",
+    [
+        (
+            Parser(
+                "value(test_unit_type.unit.conversion) - value(test_unit_type.unit.refund)",
+                "count(test_unit_type.global.exposure)",
+            ),
+            {
+                "test_unit_type.unit.conversion",
+                "test_unit_type.unit.refund",
+                "test_unit_type.global.exposure",
+            },
+        ),
+        (
+            Parser(
+                "value(test_unit_type.global.conversion)",
+                "count(test_unit_type.global.exposure)",
+            ),
+            {
+                "test_unit_type.global.conversion",
+                "test_unit_type.global.exposure",
+            },
+        ),
+        (
+            Parser(
+                "value(test_unit_type.unit.conversion) ~ value(test_unit_type.unit.refund)",
+                "count(test_unit_type.global.exposure)",
+            ),
+            {
+                "test_unit_type.unit.conversion",
+                "test_unit_type.unit.refund",
+                "test_unit_type.global.exposure",
+            },
+        ),
+        (
+            Parser(
+                "value(test_unit_type.unit.conversion(a=b, y=x, test=test))",
+                "count(test_unit_type.global.exposure)",
+            ),
+            {
+                "test_unit_type.unit.conversion[a=b, y=x, test=test]",
+                "test_unit_type.global.exposure",
+            },
+        ),
+        (
+            Parser(
+                "value(test_unit_type.unit.conversion(a=b))",
+                "count(test_unit_type.global.exposure)",
+            ),
+            {
+                "test_unit_type.unit.conversion[a=b]",
+                "test_unit_type.global.exposure",
+            },
+        ),
+        (
+            Parser(
+                "value(test_unit_type.unit.conversion(product=p_1))",
+                "value(test_unit_type.unit.conversion(product=p_1))",
+            ),
+            {"test_unit_type.unit.conversion[product=p_1]"},
+        ),
+        (
+            Parser(
+                "value(test_unit_type.unit.conversion(product=p_1))",
+                "value(test_unit_type.unit.conversion)",
+            ),
+            {"test_unit_type.unit.conversion[product=p_1]", "test_unit_type.unit.conversion"},
+        ),
+        (
+            Parser(
+                "value(test_unit_type.unit.conversion(product=p_1)) + "
+                "value(test_unit_type.unit.conversion(product=p_1))",
+                "value(test_unit_type.unit.conversion)",
+            ),
+            {"test_unit_type.unit.conversion[product=p_1]", "test_unit_type.unit.conversion"},
+        ),
+    ],
+)
+def test_get_goals(parser, expected):
+    assert parser.get_goals_str() == expected
+
+
+def test_fail_if_duplicate_dimensions():
+    with pytest.raises(ParseException):
+        Parser(
+            "value(test_unit_type.unit.conversion(a=b, a=c))",
+            "count(test_unit_type.global.exposure)",
+        )
+
+
+@pytest.mark.parametrize(
+    "parser, expected_goals",
+    [
+        (
+            Parser(
+                "count(test_unit_type.global.conversion(product=p_1)) + count(test_unit_type.global.conversion)",
+                "count(test_unit_type.unit.conversion(product=p_1_2))",
+            ),
+            {
+                "test_unit_type.global.conversion[product=p_1]": {"product": "p_1"},
+                "test_unit_type.global.conversion": {"product": ""},
+                "test_unit_type.unit.conversion[product=p_1_2]": {"product": "p_1_2"},
+            },
+        ),
+        (
+            Parser(
+                "count(test_unit_type.global.conversion(x=1, y=2))",
+                "count(test_unit_type.unit.conversion)",
+            ),
+            {
+                "test_unit_type.global.conversion[x=1, y=2]": {"x": "1", "y": "2"},
+                "test_unit_type.unit.conversion": {"x": "", "y": ""},
+            },
+        ),
+        (
+            Parser(
+                "count(test_unit_type.global.conversion(x=1, y=2))",
+                "count(test_unit_type.global.conversion(x=1))",
+            ),
+            {
+                "test_unit_type.global.conversion[x=1, y=2]": {"x": "1", "y": "2"},
+                "test_unit_type.global.conversion[x=1]": {"x": "1", "y": ""},
+            },
+        ),
+        (
+            Parser(
+                "count(test_unit_type.global.conversion(x=A/ A, y=A B /C))",
+                "count(test_unit_type.unit.conversion)",
+            ),
+            {
+                "test_unit_type.global.conversion[x=A/ A, y=A B /C]": {"x": "A/ A", "y": "A B /C"},
+                "test_unit_type.unit.conversion": {"x": "", "y": ""},
+            },
+        ),
+        (
+            Parser(
+                "count(test_unit_type.global.conversion(x=A/ A|BB, y=X|Y|Z))",
+                "count(test_unit_type.unit.conversion)",
+            ),
+            {
+                "test_unit_type.global.conversion[x=A/ A|BB, y=X|Y|Z]": {"x": "A/ A|BB", "y": "X|Y|Z"},
+                "test_unit_type.unit.conversion": {"x": "", "y": ""},
+            },
+        ),
+        (
+            Parser(
+                "count(test_unit_type.global.conversion(x=^test|test, y=^test))",
+                "count(test_unit_type.unit.conversion)",
+            ),
+            {
+                "test_unit_type.global.conversion[x=^test|test, y=^test]": {"x": "^test|test", "y": "^test"},
+                "test_unit_type.unit.conversion": {"x": "", "y": ""},
+            },
+        ),
+        (
+            Parser(
+                "count(test.global.conversion(x=test, y>=123))",
+                "count(test.unit.conversion(a<=4, b>42))",
+            ),
+            {
+                "test.global.conversion[x=test, y>=123]": {"x": "test", "y": ">=123", "a": "", "b": ""},
+                "test.unit.conversion[a<=4, b>42]": {"a": "<=4", "b": ">42", "x": "", "y": ""},
+            },
+        ),
+        (
+            Parser(
+                "count(test.global.conversion(x=test, y!=123))",
+                "count(test.global.conversion)",
+            ),
+            {
+                "test.global.conversion[x=test, y!=123]": {"x": "test", "y": "!=123"},
+                "test.global.conversion": {"x": "", "y": ""},
+            },
+        ),
+        (
+            Parser(
+                "count(test.global.conversion(x_1=test, x_234=234))",
+                "count(test.global.conversion)",
+            ),
+            {
+                "test.global.conversion[x_1=test, x_234=234]": {"x_1": "test", "x_234": "234"},
+                "test.global.conversion": {"x_1": "", "x_234": ""},
+            },
+        ),
+    ],
+)
+def test_get_goals_dimensional(parser, expected_goals):
+    goals = parser.get_goals()
+
+    for g in goals:
+        assert expected_goals[str(g)] == g.dimension_to_value
+        if not g.is_dimensional():
+            assert all(v == "" for v in g.dimension_to_value.values())
+
+
+@pytest.mark.parametrize(
+    "dimension_value",
+    [
+        "232>44",
+        "abc^def",
+        "<=2",
+    ],
+)
+def test_operator_position_not_correct(dimension_value):
+    with pytest.raises(ParseException):
+        Parser(
+            f"count(test_unit_type.global.conversion(x={dimension_value})",
+            "count(test_unit_type.unit.conversion)",
+        )
+
+
+@pytest.mark.parametrize(
+    "nominator",
+    [
+        "2 * count(test_unit_type.global.conversion)",
+        "-1 * count(test_unit_type.global.conversion)",
+    ],
+)
+def test_numbers(nominator):
+    assert isinstance(
+        Parser(nominator, "count(test_unit_type.unit.conversion)")._nominator_expr,
+        MultOp,
+    )
+
+
+def assert_count_value(evaluation, count, value, value_sqr, precision=5):
+    assert_almost_equal(evaluation[0], count, precision)
+    assert_almost_equal(evaluation[1], value, precision)
+    assert_almost_equal(evaluation[2], value_sqr, precision)
---
+++ packages/ep-stats/ep_stats-2.5.6.tar.gz/ep_stats-2.5.6/tests/epstats/toolkit/test_statistics.py
@@ -0,0 +1,261 @@
+import numpy as np
+import pytest
+from statsmodels.stats.power import TTestIndPower
+
+from src.epstats.toolkit.statistics import Statistics
+
+
+def _assert_sample_sizes_equal(x, y):
+    assert abs(x - y) <= 2
+
+
+def _assert_sample_sizes_equal_within_tolerance(x, y, n_variants):
+    # Booking calculator is using Sidak's correction instead of Bonferroni's,
+    # https://github.com/bookingcom/powercalculator/blob/master/src/js/math.js#L303
+    # it produces slightly different results in case of multiple variants.
+    # The difference is less than 0.5%.
+    if n_variants > 2:
+        rel_tol = 0.005
+        assert abs((x - y) / x) <= rel_tol
+    else:
+        _assert_sample_sizes_equal(x, y)
+
+
+@pytest.mark.parametrize(
+    "test_length, actual_day, expected",
+    [
+        (14, 0, 1.00),
+        (14, 1, 1.00),
+        (14, 2, 1.00),
+        (14, 3, 1.00),
+        (14, 14, 0.95),
+        (7, 1, 1.00),
+        (28, 4, 1.00),
+        (28, 8, 0.9998),
+        (28, 28, 0.95),
+    ],
+)
+def test_obf_alpha_spending_function(test_length, actual_day, expected):
+    alpha = Statistics.obf_alpha_spending_function(0.95, test_length, actual_day)
+    assert alpha == expected
+
+
+@pytest.mark.parametrize(
+    # expected from https://bookingcom.github.io/powercalculator
+    "n_variants, minimum_effect, mean, std, expected",
+    [
+        (2, 0.10, 0.2, 1.2, 56512),
+        (2, 0.10, 0.2, 2.0, 156978),
+        (2, 0.10, 0.3, 1.2, 25117),
+        (2, 0.10, 0.3, 2.0, 69768),
+        (2, 0.05, 0.2, 1.2, 226048),
+        (2, 0.05, 0.2, 2.0, 627911),
+        (2, 0.05, 0.3, 1.2, 100466),
+        (2, 0.05, 0.3, 2.0, 279072),
+        (3, 0.05, 0.3, 2.0, 336878),
+        (3, 0.10, 0.2, 1.2, 68218),
+        (4, 0.10, 0.2, 2.0, 208576),
+    ],
+)
+def test_required_sample_size_per_variant_equal_variance(n_variants, minimum_effect, mean, std, expected):
+    sample_size_per_variant = Statistics.required_sample_size_per_variant(
+        n_variants=n_variants,
+        minimum_effect=minimum_effect,
+        mean=mean,
+        std=std,
+    )
+    effect_size = (mean * (1 + minimum_effect) - mean) / std
+
+    # nobs1
+    expected_from_statsmodels = TTestIndPower().solve_power(
+        effect_size=effect_size,
+        ratio=1.0,  # N_A / N_B
+        alpha=0.05 / (n_variants - 1),
+        power=0.8,
+        nobs1=None,
+    )
+
+    _assert_sample_sizes_equal(sample_size_per_variant, round(expected_from_statsmodels))
+    _assert_sample_sizes_equal_within_tolerance(sample_size_per_variant, expected, n_variants)
+
+
+@pytest.mark.parametrize(
+    "minimum_effect, mean, std, std_2",
+    [
+        (0.10, 0.2, 1.2, 2.0),
+        (0.05, 0.3, 2.0, 2.5),
+    ],
+)
+def test_required_sample_size_per_variant_unequal_variance(minimum_effect, mean, std, std_2):
+    sample_size_per_variant = Statistics.required_sample_size_per_variant(
+        n_variants=2,
+        minimum_effect=minimum_effect,
+        mean=mean,
+        std=std,
+        std_2=std_2,
+    )
+
+    mean_2 = mean * (1 + minimum_effect)
+    var_ = (std**2 + std_2**2) / 2
+    std_ = np.sqrt(var_)
+    effect_size = (mean_2 - mean) / std_
+
+    # nobs1
+    expected_from_statsmodels = TTestIndPower().solve_power(
+        effect_size=effect_size,
+        ratio=1.0,  # N_A / N_B
+        alpha=0.05,
+        power=0.8,
+        nobs1=None,
+    )
+
+    _assert_sample_sizes_equal(sample_size_per_variant, round(expected_from_statsmodels))
+
+
+@pytest.mark.parametrize(
+    # expected from https://bookingcom.github.io/powercalculator
+    "n_variants, minimum_effect, mean, std, expected",
+    [
+        (2, 0.05, 0.4, None, 9490),
+        (2, 0.10, 0.1, None, 14749),
+        (3, 0.05, 0.4, None, 11455),
+        (4, 0.10, 0.1, None, 19596),
+    ],
+)
+def test_required_sample_size_per_variant_bernoulli(n_variants, minimum_effect, mean, std, expected):
+    sample_size_per_variant = Statistics.required_sample_size_per_variant_bernoulli(
+        n_variants=n_variants,
+        minimum_effect=minimum_effect,
+        mean=mean,
+    )
+
+    mean_2 = mean * (1 + minimum_effect)
+    var = mean * (1 - mean)
+    var_2 = mean_2 * (1 - mean_2)
+    std = np.sqrt((var + var_2) / 2)
+
+    effect_size = (mean_2 - mean) / std
+
+    # nobs1
+    expected_from_statsmodels = TTestIndPower().solve_power(
+        effect_size=effect_size,
+        ratio=1.0,
+        alpha=0.05 / (n_variants - 1),
+        power=0.8,
+        nobs1=None,
+    )
+
+    _assert_sample_sizes_equal(sample_size_per_variant, round(expected_from_statsmodels))
+    _assert_sample_sizes_equal_within_tolerance(sample_size_per_variant, expected, n_variants)
+
+
+@pytest.mark.parametrize(
+    "n_variants, minimum_effect, mean, std, f",
+    [
+        (2, -0.1, 0.2, 1.2, Statistics.required_sample_size_per_variant),
+        (1, -0.1, 0.2, 1.2, Statistics.required_sample_size_per_variant),
+        (2, 0.1, 10.1, None, Statistics.required_sample_size_per_variant_bernoulli),
+    ],
+)
+def test_required_sample_size_per_variant_raises_exception(n_variants, minimum_effect, mean, std, f):
+    args = {"minimum_effect": minimum_effect, "mean": mean, "n_variants": n_variants}
+
+    if std is not None:
+        args["std"] = std
+
+    with pytest.raises(ValueError):
+        f(**args)
+
+
+@pytest.mark.parametrize(
+    "minimum_effect, mean, std, expected",
+    [
+        (0.1, 0, 0, np.isnan),
+        (0.1, np.nan, np.nan, np.isnan),
+        (0.1, 0, np.nan, np.isnan),
+        (0.1, 0, 1, np.isinf),
+        (np.nan, np.nan, np.nan, np.isnan),
+    ],
+)
+def test_required_sample_size_per_variant_not_valid(minimum_effect, mean, std, expected):
+    assert expected(
+        Statistics.required_sample_size_per_variant(
+            minimum_effect=minimum_effect,
+            mean=mean,
+            std=std,
+            n_variants=2,
+        )
+    )
+
+
+@pytest.mark.parametrize(
+    "n_variants, sample_size_per_variant",
+    [
+        (2, 400000),
+        (2, 200000),
+        (2, 627911),
+        (3, 500000),
+        (4, 300000),
+    ],
+)
+def test_power_from_required_sample_size_per_variant(n_variants, sample_size_per_variant):
+    mean = 0.2
+    std = 2.0
+    minimum_effect = 0.05
+    required_sample_size_per_variant = Statistics.required_sample_size_per_variant(
+        n_variants=n_variants,
+        std=std,
+        mean=mean,
+        minimum_effect=minimum_effect,
+    )
+
+    expected = TTestIndPower().solve_power(
+        effect_size=(mean * (1 + minimum_effect) - mean) / std,
+        ratio=1.0,
+        alpha=0.05 / (n_variants - 1),
+        power=None,
+        nobs1=sample_size_per_variant,
+    )
+
+    power = Statistics.power_from_required_sample_size_per_variant(
+        n_variants=n_variants,
+        sample_size_per_variant=sample_size_per_variant,
+        required_sample_size_per_variant=required_sample_size_per_variant,
+    )
+
+    assert np.allclose(power, expected, atol=1e-3)
+
+
+def test_power_from_required_sample_size_per_variant_nan_params():
+    assert np.isnan(
+        Statistics.power_from_required_sample_size_per_variant(
+            n_variants=np.nan,
+            sample_size_per_variant=np.nan,
+            required_sample_size_per_variant=np.nan,
+        )
+    )
+
+
+@pytest.mark.parametrize(
+    "args",
+    [
+        {
+            "n_variants": 1,
+            "sample_size_per_variant": 100,
+            "required_sample_size_per_variant": 100,
+        },
+        {
+            "n_variants": 2,
+            "sample_size_per_variant": 0,
+            "required_sample_size_per_variant": 0,
+        },
+    ],
+)
+def test_power_from_required_sample_size_per_variant_is_nan(args):
+    assert np.isnan(Statistics.power_from_required_sample_size_per_variant(**args))
+
+
+def test_false_positive_risk():
+    false_positive_risk = Statistics.false_positive_risk(null_hypothesis_rate=1 - 0.33, power=0.8, p_value=0.025)
+
+    assert false_positive_risk == 0.05966162065894922
