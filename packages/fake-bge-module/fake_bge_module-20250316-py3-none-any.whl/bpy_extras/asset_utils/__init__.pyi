fddda7f818abafe6ffeca524bd744843db5ab7b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 06:03:47 +0000

    Add package fake-bge-module/20250316/fake_bge_module-20250316-py3-none-any.whl

---
+++ packages/fake-bge-module/fake_bge_module-20250316-py3-none-any.whl/bpy_extras/asset_utils/__init__.pyi
@@ -0,0 +1,12 @@
+"""
+Helpers for asset management tasks.
+
+"""
+
+import typing
+import collections.abc
+import typing_extensions
+
+class AssetBrowserPanel: ...
+class AssetMetaDataPanel: ...
+class SpaceAssetInfo: ...
