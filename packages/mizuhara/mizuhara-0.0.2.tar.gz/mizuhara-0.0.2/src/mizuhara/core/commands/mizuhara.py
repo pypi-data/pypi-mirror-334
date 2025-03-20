d37926ddaee02220095af11cc9db6cd07290c84a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 06:04:09 +0000

    Add package mizuhara/0.0.2/mizuhara-0.0.2.tar.gz

---
+++ packages/mizuhara/mizuhara-0.0.2.tar.gz/mizuhara-0.0.2/src/mizuhara/core/commands/mizuhara.py
@@ -0,0 +1,14 @@
+
+
+
+def main():
+    """
+    code for CLI command that create a telebot_framework project.
+
+    :return: None
+    """
+
+    print("test, Hello, Mizuhara")
+    return None
+
+
---
+++ packages/mizuhara/mizuhara-0.0.2.tar.gz/mizuhara-0.0.2/src/mizuhara/template/routes.py
@@ -0,0 +1,17 @@
+from core.routes import (connector_callback,
+                         connector_command,
+                         connector_message,)
+from project.views import *
+
+
+# Mapping handlers and views for Telegram Bot commands.
+COMMANDS: list = [
+]
+
+
+MESSAGES: list = [
+]
+
+
+CALLBACKS: list = [
+]
