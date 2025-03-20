b4b3bfd959bda584bb527e1781ab7c4a91c1f301
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 00:05:29 +0000

    Add package pytest-cocotb/0.1.0/pytest_cocotb-0.1.0.tar.gz

---
+++ packages/pytest-cocotb/pytest_cocotb-0.1.0.tar.gz/pytest_cocotb-0.1.0/tests/test_cocotb.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2025 Tymoteusz Blazejczyk <tymoteusz.blazejczyk@tymonx.com>
+# SPDX-License-Identifier: Apache-2.0
+
+"""Test Cocotb."""
+
+
+def test_cocotb() -> None:
+    """Test Cocotb."""
