f65feb500ea6667a0897c63f0280787baad3d348
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 09:04:28 +0000

    Add package dotflow-mongodb/0.1.2/dotflow_mongodb-0.1.2-py3-none-any.whl

---
+++ packages/dotflow-mongodb/dotflow_mongodb-0.1.2-py3-none-any.whl/dotflow_mongodb/__init__.py
@@ -0,0 +1,10 @@
+"""Dotflow MongoDB __init__ module."""
+
+__version__ = "0.1.2"
+__description__ = "Dotflow Storage MongoDB"
+
+from dotflow_mongodb.main import StorageMongoDB
+
+__all__ = [
+    "StorageMongoDB"
+]
