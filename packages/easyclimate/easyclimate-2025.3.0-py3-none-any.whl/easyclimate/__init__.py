239557eb070d7f7c28d56b8920909bac1ce293c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 05:03:52 +0000

    Add package easyclimate/2025.3.0/easyclimate-2025.3.0-py3-none-any.whl

---
+++ packages/easyclimate/easyclimate-2025.3.0-py3-none-any.whl/easyclimate/__init__.py
@@ -0,0 +1,11 @@
+from .core import *
+
+from . import filter
+from . import interp
+from . import plot
+from . import field
+from . import accessor
+from . import wrf
+
+# Version number
+__version__ = "2025.3.0"
