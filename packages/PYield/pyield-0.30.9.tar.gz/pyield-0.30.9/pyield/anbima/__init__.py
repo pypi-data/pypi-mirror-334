8621a912881170c76fff09a4374dc571fcad9a19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 19:06:18 +0000

    Add package PYield/0.30.9/pyield-0.30.9.tar.gz

---
+++ packages/PYield/pyield-0.30.9.tar.gz/pyield-0.30.9/pyield/anbima/__init__.py
@@ -0,0 +1,15 @@
+from pyield.anbima.ettj import last_ettj
+from pyield.anbima.ima import last_ima
+from pyield.anbima.imaq import imaq
+from pyield.anbima.ipca import ipca_projection
+from pyield.anbima.tpf import tpf_data, tpf_pre_maturities, tpf_rates
+
+__all__ = [
+    "last_ima",
+    "imaq",
+    "ipca_projection",
+    "tpf_data",
+    "tpf_rates",
+    "tpf_pre_maturities",
+    "last_ettj",
+]
