612aecf4c458cc62d51361ce87de7bc4f705e47c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 09:04:28 +0000

    Add package agentrpc/0.0.5/agentrpc-0.0.5.tar.gz

---
+++ packages/agentrpc/agentrpc-0.0.5.tar.gz/agentrpc-0.0.5/tests/test_client.py
@@ -0,0 +1,40 @@
+import os
+from pathlib import Path
+
+from openai.types.chat.chat_completion_message import FunctionCall
+
+from dotenv import load_dotenv
+from agentrpc import AgentRPC
+from agentrpc.openai import OpenAIIntegration
+
+# Load environment variables from .env file
+dotenv_path = Path(__file__).parent.parent / ".env"
+load_dotenv(dotenv_path=dotenv_path)
+api_secret = os.environ.get("INFERABLE_TEST_API_SECRET", "test_secret")
+api_endpoint = os.environ.get("INFERABLE_TEST_API_ENDPOINT", "https://api.agentrpc.com")
+
+
+def test_client_init():
+    """Test client initialization."""
+    client = AgentRPC(api_secret, api_endpoint)
+
+    # Check that properties are set correctly
+    assert client._AgentRPC__api_secret == api_secret
+    assert client._AgentRPC__endpoint == api_endpoint
+    assert client._AgentRPC__http_client is not None
+    assert isinstance(client.openai, OpenAIIntegration)
+
+
+def test_client_openai_get_tools():
+    """Test client initialization."""
+    client = AgentRPC(api_secret, api_endpoint)
+    tools = client.openai.get_tools()
+    print(tools)
+
+
+def test_client_openai_execute_tool():
+    """Test executing ."""
+    client = AgentRPC(api_secret, api_endpoint)
+    function_call = FunctionCall(name="hello", arguments='{"name": "agent"}')
+    result = client.openai.execute_tool(function_call)
+    print(result)
