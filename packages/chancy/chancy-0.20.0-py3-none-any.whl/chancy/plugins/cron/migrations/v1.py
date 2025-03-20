ed930b1116978412e3d80e0e88f04f1629f3dea9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 00:05:47 +0000

    Add package chancy/0.20.0/chancy-0.20.0-py3-none-any.whl

---
+++ packages/chancy/chancy-0.20.0-py3-none-any.whl/chancy/plugins/cron/migrations/v1.py
@@ -0,0 +1,28 @@
+from psycopg import AsyncCursor
+from psycopg import sql
+
+from chancy.migrate import Migration, Migrator
+
+
+class V1Migration(Migration):
+    async def up(self, migrator: Migrator, cursor: AsyncCursor):
+        await cursor.execute(
+            sql.SQL(
+                """
+                CREATE TABLE {table} (
+                    unique_key TEXT PRIMARY KEY,
+                    job JSON NOT NULL,
+                    cron TEXT NOT NULL,
+                    last_run TIMESTAMPTZ,
+                    next_run TIMESTAMPTZ NOT NULL
+                )
+                """
+            ).format(table=sql.Identifier(f"{migrator.prefix}cron"))
+        )
+
+    async def down(self, migrator: Migrator, cursor: AsyncCursor):
+        await cursor.execute(
+            sql.SQL("DROP TABLE {table}").format(
+                table=sql.Identifier(f"{migrator.prefix}cron")
+            )
+        )
