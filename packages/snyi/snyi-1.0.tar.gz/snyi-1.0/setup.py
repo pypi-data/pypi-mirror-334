e8128aa9d92480504264215243b8f2d29b830c6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 18:05:46 +0000

    Add package snyi/1.0/snyi-1.0.tar.gz

---
+++ packages/snyi/snyi-1.0.tar.gz/snyi-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="snyi",
+    version="1.0",
+    description="Uma biblioteca de utilitários para Python",
+    author="Seu Nome",
+    py_modules=["pyutils"],
+)
