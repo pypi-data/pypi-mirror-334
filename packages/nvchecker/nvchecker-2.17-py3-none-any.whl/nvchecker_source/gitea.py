eececd2f2b11bd331b96580d57b56155b68522fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 05:03:43 +0000

    Add package nvchecker/2.17/nvchecker-2.17-py3-none-any.whl

---
+++ packages/nvchecker/nvchecker-2.17-py3-none-any.whl/nvchecker_source/gitea.py
@@ -0,0 +1,57 @@
+# MIT licensed
+# Copyright (c) 2013-2020 lilydjwg <lilydjwg@gmail.com>, et al.
+
+from __future__ import annotations
+
+import urllib.parse
+
+GITEA_URL = 'https://%s/api/v1/repos/%s/commits'
+GITEA_MAX_TAG = 'https://%s/api/v1/repos/%s/tags'
+
+from nvchecker.api import (
+  VersionResult, RichResult, Entry,
+  AsyncCache, KeyManager,
+)
+
+async def get_version(
+  name: str, conf: Entry, *,
+  cache: AsyncCache, keymanager: KeyManager,
+) -> VersionResult:
+  repo = urllib.parse.quote(conf['gitea'])
+  br = conf.get('branch')
+  host = conf.get('host', 'gitea.com')
+  use_max_tag = conf.get('use_max_tag', False)
+
+  if use_max_tag:
+    url = GITEA_MAX_TAG % (host, repo)
+  else:
+    url = GITEA_URL % (host, repo)
+    if br:
+      url += '?sha=' + br
+
+  # Load token from config
+  token = conf.get('token')
+  # Load token from keyman
+  if token is None:
+    token = keymanager.get_key(host.lower(), 'gitea_' + host.lower())
+
+  # Set private token if token exists.
+  headers = {}
+  if token:
+    headers["Authorization"] = f'token {token}'
+
+  data = await cache.get_json(url, headers = headers)
+  if use_max_tag:
+    return [
+      RichResult(
+        version = tag['name'],
+        revision = tag['id'],
+        url = f'https://{host}/{conf["gitea"]}/releases/tag/{tag["name"]}',
+      ) for tag in data
+    ]
+  else:
+    return RichResult(
+      version = data[0]['commit']['committer']['date'],
+      revision = data[0]['sha'],
+      url = data[0]['html_url'],
+    )
---
+++ packages/nvchecker/nvchecker-2.17-py3-none-any.whl/nvchecker_source/htmlparser.py
@@ -0,0 +1,41 @@
+# MIT licensed
+# Copyright (c) 2020 Ypsilik <tt2laurent.maud@gmail.com>, et al.
+# Copyright (c) 2013-2020 lilydjwg <lilydjwg@gmail.com>, et al.
+
+from lxml import html, etree
+
+from nvchecker.api import session, GetVersionError
+
+async def get_version(name, conf, *, cache, **kwargs):
+  key = tuple(sorted(conf.items()))
+  return await cache.get(key, get_version_impl)
+
+async def get_version_impl(info):
+  conf = dict(info)
+
+  encoding = conf.get('encoding')
+  parser = html.HTMLParser(encoding=encoding)
+  data = conf.get('post_data')
+  if data is None:
+    res = await session.get(conf['url'])
+  else:
+    res = await session.post(conf['url'], body = data, headers = {
+        'Content-Type': conf.get('post_data_type', 'application/x-www-form-urlencoded')
+      })
+  doc = html.fromstring(res.body, base_url=conf['url'], parser=parser)
+
+  try:
+    els = doc.xpath(conf.get('xpath'))
+  except ValueError:
+    if not conf.get('missing_ok', False):
+      raise GetVersionError('version string not found.')
+  except etree.XPathEvalError as e:
+    raise GetVersionError('bad xpath', exc_info=e)
+
+  version = [
+    str(el)
+    if isinstance(el, str)
+    else str(el.text_content())
+    for el in els
+  ]
+  return version
