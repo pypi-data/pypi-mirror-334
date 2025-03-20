cd0c24ee0e666d9e7e97a73f62602a237ab45aab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 21:05:04 +0000

    Add package lip-generation/0.4.0/lip_generation-0.4.0.tar.gz

---
+++ packages/lip-generation/lip_generation-0.4.0.tar.gz/lip_generation-0.4.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+# Read requirements from requirements.txt
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='lip_generation',
+    version='0.4.0',
+    packages=find_packages(),
+    install_requires=required,
+)
