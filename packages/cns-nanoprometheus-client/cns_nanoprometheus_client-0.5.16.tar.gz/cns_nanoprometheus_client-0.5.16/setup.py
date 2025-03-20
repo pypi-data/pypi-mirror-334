7247665de644afe9c505943dfdf9c8628cd9658d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 13:11:22 +0000

    Add package cns-nanoprometheus-client/0.5.16/cns_nanoprometheus_client-0.5.16.tar.gz

---
+++ packages/cns-nanoprometheus-client/cns_nanoprometheus_client-0.5.16.tar.gz/cns_nanoprometheus_client-0.5.16/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cns-nanoprometheus-client",
+    version="0.5.16",
+    description="Python client for Nanoprometheus metrics",
+    author="Roman Skvara",
+    author_email="skvara.roman@gmail.com",
+    license="MIT",
+    license_files=["LICENSE"],
+    packages=find_packages(),
+    install_requires=["requests_html", "lxml_html_clean"],
+    entry_points={
+        "console_scripts": [
+            "cns-nano-init = cns_nanoprometheus_client:main"
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License",
+    ],
+)
