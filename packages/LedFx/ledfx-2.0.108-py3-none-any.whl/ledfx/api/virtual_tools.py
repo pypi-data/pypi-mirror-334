ba5f41855158f9ec466e05e197da2d38fbc26083
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 05:03:56 +0000

    Add package LedFx/2.0.108/ledfx-2.0.108-py3-none-any.whl

---
+++ packages/LedFx/ledfx-2.0.108-py3-none-any.whl/ledfx/api/virtual_tools.py
@@ -0,0 +1,72 @@
+import logging
+from json import JSONDecodeError
+
+from aiohttp import web
+
+from ledfx.api import RestEndpoint
+from ledfx.color import parse_color, validate_color
+
+_LOGGER = logging.getLogger(__name__)
+TOOLS = ["force_color", "oneshot"]
+
+
+class VirtualToolsEndpoint(RestEndpoint):
+    """api for all virtual manipulations"""
+
+    ENDPOINT_PATH = "/api/virtuals_tools"
+
+    async def get(self) -> web.Response:
+        return await self.request_success("info", f"Available tools: {TOOLS}")
+
+    async def put(self, request: web.Request) -> web.Response:
+        """Extensible tools support"""
+
+        try:
+            data = await request.json()
+        except JSONDecodeError:
+            return await self.json_decode_error()
+
+        tool = data.get("tool")
+
+        if tool is None:
+            return await self.invalid_request(
+                'Required attribute "tool" was not provided'
+            )
+
+        if tool not in TOOLS:
+            return await self.invalid_request(f"Tool {tool} is not in {TOOLS}")
+
+        if tool == "force_color":
+            color = data.get("color")
+            if color is None:
+                return await self.invalid_request(
+                    "Required attribute for force_color, color was not provided"
+                )
+            for virtual_id in self._ledfx.virtuals:
+                virtual = self._ledfx.virtuals.get(virtual_id)
+                if virtual.is_device == virtual.id:
+                    virtual.force_frame(parse_color(validate_color(color)))
+
+        if tool == "oneshot":
+            color = data.get("color", "white")
+
+            ramp = data.get("ramp", 0)
+            hold = data.get("hold", 0)
+            fade = data.get("fade", 0)
+
+            # if all values are zero, we will now just ensure any current
+            # oneshot are cancelled
+
+            # iterate through all virtuals and apply oneshot
+            for virtual_id in self._ledfx.virtuals:
+                virtual = self._ledfx.virtuals.get(virtual_id)
+                if virtual is not None:
+                    virtual.oneshot(
+                        parse_color(validate_color(color)), ramp, hold, fade
+                    )
+
+        effect_response = {}
+        effect_response["tool"] = tool
+
+        response = {"status": "success", "tool": tool}
+        return await self.bare_request_success(response)
