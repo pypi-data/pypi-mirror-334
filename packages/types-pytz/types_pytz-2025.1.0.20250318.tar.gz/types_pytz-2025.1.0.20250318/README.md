52d8070398b425ea022a9424b88be40ee97a3183
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 20:07:06 +0000

    Add package types-pytz/2025.1.0.20250318/types_pytz-2025.1.0.20250318.tar.gz

---
+++ packages/types-pytz/types_pytz-2025.1.0.20250318.tar.gz/types_pytz-2025.1.0.20250318/README.md
@@ -0,0 +1,26 @@
+## Typing stubs for pytz
+
+This is a [PEP 561](https://peps.python.org/pep-0561/)
+type stub package for the [`pytz`](https://github.com/stub42/pytz) package.
+It can be used by type-checking tools like
+[mypy](https://github.com/python/mypy/),
+[pyright](https://github.com/microsoft/pyright),
+[pytype](https://github.com/google/pytype/),
+[Pyre](https://pyre-check.org/),
+PyCharm, etc. to check code that uses `pytz`. This version of
+`types-pytz` aims to provide accurate annotations for
+`pytz==2025.1`.
+
+This package is part of the [typeshed project](https://github.com/python/typeshed).
+All fixes for types and metadata should be contributed there.
+See [the README](https://github.com/python/typeshed/blob/main/README.md)
+for more details. The source for this package can be found in the
+[`stubs/pytz`](https://github.com/python/typeshed/tree/main/stubs/pytz)
+directory.
+
+This package was tested with
+mypy 1.15.0,
+pyright 1.1.396,
+and pytype 2024.10.11.
+It was generated from typeshed commit
+[`b93eae52affaedfda79c00f662f50a2ef3b24c46`](https://github.com/python/typeshed/commit/b93eae52affaedfda79c00f662f50a2ef3b24c46).
---
+++ packages/types-pytz/types_pytz-2025.1.0.20250318.tar.gz/types_pytz-2025.1.0.20250318/setup.py
@@ -0,0 +1,56 @@
+from setuptools import setup
+
+name = "types-pytz"
+description = "Typing stubs for pytz"
+long_description = '''
+## Typing stubs for pytz
+
+This is a [PEP 561](https://peps.python.org/pep-0561/)
+type stub package for the [`pytz`](https://github.com/stub42/pytz) package.
+It can be used by type-checking tools like
+[mypy](https://github.com/python/mypy/),
+[pyright](https://github.com/microsoft/pyright),
+[pytype](https://github.com/google/pytype/),
+[Pyre](https://pyre-check.org/),
+PyCharm, etc. to check code that uses `pytz`. This version of
+`types-pytz` aims to provide accurate annotations for
+`pytz==2025.1`.
+
+This package is part of the [typeshed project](https://github.com/python/typeshed).
+All fixes for types and metadata should be contributed there.
+See [the README](https://github.com/python/typeshed/blob/main/README.md)
+for more details. The source for this package can be found in the
+[`stubs/pytz`](https://github.com/python/typeshed/tree/main/stubs/pytz)
+directory.
+
+This package was tested with
+mypy 1.15.0,
+pyright 1.1.396,
+and pytype 2024.10.11.
+It was generated from typeshed commit
+[`b93eae52affaedfda79c00f662f50a2ef3b24c46`](https://github.com/python/typeshed/commit/b93eae52affaedfda79c00f662f50a2ef3b24c46).
+'''.lstrip()
+
+setup(name=name,
+      version="2025.1.0.20250318",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      project_urls={
+          "GitHub": "https://github.com/python/typeshed",
+          "Changes": "https://github.com/typeshed-internal/stub_uploader/blob/main/data/changelogs/pytz.md",
+          "Issue tracker": "https://github.com/python/typeshed/issues",
+          "Chat": "https://gitter.im/python/typing",
+      },
+      install_requires=[],
+      packages=['pytz-stubs'],
+      package_data={'pytz-stubs': ['__init__.pyi', 'exceptions.pyi', 'lazy.pyi', 'reference.pyi', 'tzfile.pyi', 'tzinfo.pyi', 'METADATA.toml', 'py.typed']},
+      license="Apache-2.0",
+      python_requires=">=3.9",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Programming Language :: Python :: 3",
+          "Typing :: Stubs Only",
+      ]
+)
