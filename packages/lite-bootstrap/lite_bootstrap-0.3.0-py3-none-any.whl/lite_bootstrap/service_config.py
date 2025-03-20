fe5706380dc5f5b7e6a5d5a252ea591735b62be5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 17:05:34 +0000

    Add package lite-bootstrap/0.3.0/lite_bootstrap-0.3.0-py3-none-any.whl

---
+++ packages/lite-bootstrap/lite_bootstrap-0.3.0-py3-none-any.whl/lite_bootstrap/service_config.py
@@ -0,0 +1,8 @@
+import dataclasses
+
+
+@dataclasses.dataclass(kw_only=True, slots=True, frozen=True)
+class ServiceConfig:
+    service_name: str = "micro-service"
+    service_version: str = "1.0.0"
+    service_environment: str | None = None
