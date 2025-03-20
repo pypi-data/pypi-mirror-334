b01efc0b6150520f3616baa43f0832e4d43cf5f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 05:03:51 +0000

    Add package pdf-signer/0.1.2/pdf_signer-0.1.2-py3-none-any.whl

---
+++ packages/pdf-signer/pdf_signer-0.1.2-py3-none-any.whl/pdf_signer/__init__.py
@@ -0,0 +1,15 @@
+# pdf_signer/__init__.py
+from .core import PDFSigner
+from .config import SignerConfig, SignatureBoxStyle
+from .exceptions import PDFSignerError, ValidationError, TagNotFoundError, SignatureDataError
+
+__version__ = "0.1.2"
+__all__ = [
+    "PDFSigner", 
+    "SignerConfig", 
+    "SignatureBoxStyle", 
+    "PDFSignerError", 
+    "ValidationError", 
+    "TagNotFoundError", 
+    "SignatureDataError"
+]
