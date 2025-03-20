17023043769684ec3b7255975ae5332382ccd309
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 06:04:20 +0000

    Add package statsforegyptian/0.1.0/statsforegyptian-0.1.0.tar.gz

---
+++ packages/statsforegyptian/statsforegyptian-0.1.0.tar.gz/statsforegyptian-0.1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(author= "Mohamed Habashy Hussein", 
+      discription= "statsforegyptian model for Item Response Theory",
+      name= "statsforegyptian",
+      version="0.1.0",
+      packages=find_packages(include=['statsforegyptian', 'statsforegyptian.*']),
+      install_requires=[],
+)
