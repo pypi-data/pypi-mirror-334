e5ee43275b3a93eb51e77fb18ab116b5a4fa6823
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 19:06:00 +0000

    Add package openrouter-exception-handler/0.2.9/openrouter_exception_handler-0.2.9.tar.gz

---
+++ packages/openrouter-exception-handler/openrouter_exception_handler-0.2.9.tar.gz/openrouter_exception_handler-0.2.9/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='openrouter_exception_handler',
+    version='0.2.9',
+    packages=find_packages(where='src/'),  # ✅ Ahora encuentra el paquete correctamente
+    package_dir={'': 'src/openrouter_exception_handler/'},  # ✅ Mapea el paquete a la carpeta `src`
+    install_requires=['requests'],
+)
