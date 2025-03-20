172fd49c409e66ebfd5214cbb377c23157567ed6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 16:05:50 +0000

    Add package pygpsclient/1.5.4/pygpsclient-1.5.4-py3-none-any.whl

---
+++ packages/pygpsclient/pygpsclient-1.5.4-py3-none-any.whl/pygpsclient/__init__.py
@@ -0,0 +1,13 @@
+"""
+Created on 27 Sep 2020
+
+:author: semuadmin
+:copyright: 2020 SEMU Consulting
+:license: BSD 3-Clause
+"""
+
+# pylint: disable=invalid-name
+
+from pygpsclient._version import __version__
+
+version = __version__
