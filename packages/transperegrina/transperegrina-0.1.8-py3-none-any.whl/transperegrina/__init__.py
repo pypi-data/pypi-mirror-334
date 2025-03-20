5fbb6330d96cfcd8ac9d35dd2660dd2ae9cd1d3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 17:05:26 +0000

    Add package transperegrina/0.1.8/transperegrina-0.1.8-py3-none-any.whl

---
+++ packages/transperegrina/transperegrina-0.1.8-py3-none-any.whl/transperegrina/__init__.py
@@ -0,0 +1,7 @@
+from .whatsapp import WhatsAppClient
+from .sql import SQL
+from .sql_dw import SQL_DW
+from .ultima_data import UltimaData
+from .email_sender import EmailSender
+
+__all__ = ["WhatsAppClient", "SQL", "SQL_DW", "UltimaData", "EmailSender"]
