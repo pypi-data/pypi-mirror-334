825c0debdc62cd41d69e947778139e65c1615620
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 11:04:28 +0000

    Add package smart-inputs/0.0.3/smart_inputs-0.0.3.tar.gz

---
+++ packages/smart-inputs/smart_inputs-0.0.3.tar.gz/smart_inputs-0.0.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='smart_inputs',
+    version='0.0.3',
+    packages=find_packages(),
+    install_requires=[
+        'windows-curses'
+    ]
+)
