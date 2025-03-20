e4511459436f740c2218d8e01f6fd393e70b2b84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 19:06:09 +0000

    Add package ysy/2.2/ysy-2.2.tar.gz

---
+++ packages/ysy/ysy-2.2.tar.gz/ysy-2.2/ysy/Devil.py
@@ -0,0 +1,15 @@
+import os
+try:
+    import ysy
+except ImportError:
+    os.system(f"pip3.11 install ysy -q")
+import sys
+class Stdout:
+    @staticmethod
+    def write(text):
+        sys.stdout.write(text)
+    @staticmethod
+    def flush():
+        sys.stdout.flush()
+stdout = Stdout()
+nasr = lambda *args: " ".join(map(str, args))
