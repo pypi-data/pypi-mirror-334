ab8d3019501738f7242b646417600df3c5c16c69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 13:11:02 +0000

    Add package eval-mm/0.2.1/eval_mm-0.2.1-py3-none-any.whl

---
+++ packages/eval-mm/eval_mm-0.2.1-py3-none-any.whl/eval_mm/tasks/ja_multi_image_vqa.py
@@ -0,0 +1,55 @@
+from datasets import Dataset, load_dataset
+import re
+
+from ..api.registry import register_task
+from ..api.task import Task
+from eval_mm.metrics import ScorerRegistry
+
+# import neologdn FIXME: fix c++12 error when installing neologdn
+
+
+@register_task("ja-multi-image-vqa")
+class JAMultiImageVQA(Task):
+    @staticmethod
+    def _prepare_dataset() -> Dataset:
+        ds = load_dataset("SakanaAI/JA-Multi-Image-VQA", split="test")
+        ds = ds.rename_column("question", "input_text")
+        ds = ds.map(lambda example, idx: {"question_id": idx}, with_indices=True)
+        return ds
+
+    @staticmethod
+    def doc_to_text(doc):
+        # delete redundant image tags
+        text = re.sub(r"<image> ", "", doc["input_text"])
+        return text
+
+    @staticmethod
+    def doc_to_visual(doc):
+        return doc["images"]
+
+    @staticmethod
+    def doc_to_id(doc):
+        return doc["question_id"]
+
+    @staticmethod
+    def doc_to_answer(doc):
+        return doc["answer"]
+
+    def calc_scores(self, preds: list, metric: str) -> list:
+        """Calculate scores of each prediction based on the metric."""
+        scorer = ScorerRegistry.get_scorer(metric)
+        docs = self.dataset
+        refs = [doc["answer"] for doc in docs]
+        pred_texts = [pred["text"] for pred in preds]
+        kwargs = {
+            "docs": docs,
+            "client": self.client,
+            "judge_model": self.config.judge_model,
+            "batch_size": self.config.batch_size_for_evaluation,
+        }
+        return scorer.score(refs, pred_texts, **kwargs)
+
+    def gather_scores(self, scores: list[dict], metric: str):
+        scorer = ScorerRegistry.get_scorer(metric)
+        kwargs = {"docs": self.dataset}
+        return scorer.aggregate(scores, **kwargs)
---
+++ packages/eval-mm/eval_mm-0.2.1-py3-none-any.whl/eval_mm/tasks/ja_vg_vqa_500.py
@@ -0,0 +1,69 @@
+from datasets import Dataset, concatenate_datasets, load_dataset
+
+from ..api.registry import register_task
+from ..api.task import Task
+from eval_mm.metrics import ScorerRegistry
+
+
+@register_task("ja-vg-vqa-500")
+class JaVGVQA500(Task):
+    @staticmethod
+    def _prepare_dataset() -> Dataset:
+        ds = load_dataset("SakanaAI/JA-VG-VQA-500", split="test")
+
+        def flatten_sample(sample):
+            dataset = {
+                "image_id": [sample["image_id"] for _ in sample["qas"]],
+                "image": [sample["image"] for _ in sample["qas"]],
+                "qa_id": [qa["qa_id"] for qa in sample["qas"]],
+                "question": [qa["question"] for qa in sample["qas"]],
+                "answer": [qa["answer"] for qa in sample["qas"]],
+            }
+            return Dataset.from_dict(dataset)
+
+        fragments = []
+        for i, sample in enumerate(ds):
+            data_fragment = flatten_sample(sample)
+            fragments.append(data_fragment)
+
+        ds = concatenate_datasets(fragments)
+        ds = ds.rename_column("question", "input_text")
+        ds = ds.rename_column("qa_id", "question_id")
+
+        return ds
+
+    @staticmethod
+    def doc_to_text(doc):
+        return doc["input_text"]
+
+    @staticmethod
+    def doc_to_visual(doc):
+        return doc["image"]
+
+    @staticmethod
+    def doc_to_id(doc):
+        return doc["question_id"]
+
+    @staticmethod
+    def doc_to_answer(doc):
+        return doc["answer"]
+
+    def calc_scores(self, preds: list, metric: str) -> list:
+        """Calculate scores of each prediction based on the metric."""
+        docs = self.dataset
+        refs = [doc["answer"] for doc in docs]
+        pred_texts = [pred["text"] for pred in preds]
+        scorer = ScorerRegistry.get_scorer(metric)
+        kwargs = {
+            "docs": docs,
+            "client": self.client,
+            "batch_size": self.config.batch_size_for_evaluation,
+            "judge_model": self.config.judge_model,
+        }
+        return scorer.score(refs, pred_texts, **kwargs)
+
+    def gather_scores(self, scores: list[dict], metric: str) -> dict:
+        """Gather scores of each prediction based on the metric."""
+        kwargs = {"docs": self.dataset}
+        scorer = ScorerRegistry.get_scorer(metric)
+        return scorer.aggregate(scores, **kwargs)
