6e6d048f2e9501782b7ad89902f33fe1f32837f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 09:04:02 +0000

    Add package nshtrainer/1.0.0b54/nshtrainer-1.0.0b54-py3-none-any.whl

---
+++ packages/nshtrainer/nshtrainer-1.0.0b54-py3-none-any.whl/nshtrainer/configs/lr_scheduler/reduce_lr_on_plateau/__init__.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+__codegen__ = True
+
+from nshtrainer.lr_scheduler.reduce_lr_on_plateau import (
+    LRSchedulerConfigBase as LRSchedulerConfigBase,
+)
+from nshtrainer.lr_scheduler.reduce_lr_on_plateau import MetricConfig as MetricConfig
+from nshtrainer.lr_scheduler.reduce_lr_on_plateau import (
+    ReduceLROnPlateauConfig as ReduceLROnPlateauConfig,
+)
+from nshtrainer.lr_scheduler.reduce_lr_on_plateau import (
+    lr_scheduler_registry as lr_scheduler_registry,
+)
+
+__all__ = [
+    "LRSchedulerConfigBase",
+    "MetricConfig",
+    "ReduceLROnPlateauConfig",
+    "lr_scheduler_registry",
+]
