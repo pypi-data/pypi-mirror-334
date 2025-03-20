af80e391dae50964b5b2e4839a7f07e56c9ecb4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 16:06:14 +0000

    Add package cross-locs/0.1.9/cross_locs-0.1.9-py3-none-any.whl

---
+++ packages/cross-locs/cross_locs-0.1.9-py3-none-any.whl/cross_locs/models/elr.py
@@ -0,0 +1,47 @@
+import pandera as pa
+from pandera.typing import Series
+from pandera.typing.geopandas import Geometry
+
+
+class FullNetworkModel27700(pa.DataFrameModel):
+    """
+    - geometry: Geometry(crs=27700)
+    - OBJECTID: int
+    - ASSETID: int
+    - L_SYSTEM: str
+    - ELR: str
+    - TRID: int
+    - L_M_FROM: float
+    - L_M_TO: float
+    """
+
+    geometry: Geometry(crs=27700)  # type: ignore
+    OBJECTID: Series[int]
+    ASSETID: Series[int]
+    L_SYSTEM: Series[str]
+    ELR: Series[str]
+    TRID: Series[int]
+    L_M_FROM: Series[float]
+    L_M_TO: Series[float]
+
+
+class DataframeWithElrMileages27700(pa.DataFrameModel):
+    """
+    All the new columns added to the original dataframe.
+    ----------------------------------------------------
+    - _elr: str
+    - _mileage: float
+    - _relative_mileage: float
+    - _distance: float
+    - _elr_exception: str
+    - _message: str
+    - _error: bool
+    """
+
+    _elr: Series[str] = pa.Field(nullable=True)
+    _mileage: Series[float] = pa.Field(nullable=True)
+    _relative_mileage: Series[float] = pa.Field(nullable=True)
+    _distance: Series[float] = pa.Field(nullable=True)
+    _elr_exception: Series[str] = pa.Field(nullable=True)
+    _message: Series[str] = pa.Field(nullable=True)
+    _error: Series[bool] = pa.Field(nullable=True)
