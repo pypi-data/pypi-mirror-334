57f05790c4510d44867cfa07a36b687bb3fd6d42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 05:03:53 +0000

    Add package ambient-base-plugin/1.6.0/ambient_base_plugin-1.6.0.tar.gz

---
+++ packages/ambient-base-plugin/ambient_base_plugin-1.6.0.tar.gz/ambient_base_plugin-1.6.0/src/ambient_base_plugin/__init__.py
@@ -0,0 +1,3 @@
+from .base_plugin import BasePlugin, hookimpl, hookspec  # noqa: F401
+from .models.configuration import ConfigPayload, PluginDefinition  # noqa: F401
+from .models.message import Message  # noqa: F401
