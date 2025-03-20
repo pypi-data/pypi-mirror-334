80c7e59c8d2101d6cdc5a1c5d4194809b7d07d21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 09:03:59 +0000

    Add package mathxyz/0.1.0/mathxyz-0.1.0-py3-none-any.whl

---
+++ packages/mathxyz/mathxyz-0.1.0-py3-none-any.whl/mathxyz/__init__.py
@@ -0,0 +1,22 @@
+"""
+mathxyz package
+===============
+
+A state-of-the-art math solver library.
+"""
+
+from .core import (
+    multiply,
+    power,
+    power_scientific,
+    divide,
+    math_solver
+)
+
+__all__ = [
+    "multiply",
+    "power",
+    "power_scientific",
+    "divide",
+    "math_solver"
+]
