c2f0120245b5f46eac034e511e96e405989f5245
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 11:04:56 +0000

    Add package zenml-nightly/0.75.0.dev20250317/zenml_nightly-0.75.0.dev20250317-py3-none-any.whl

---
+++ packages/zenml-nightly/zenml_nightly-0.75.0.dev20250317-py3-none-any.whl/zenml/zen_server/dashboard/assets/ProBadge-ypma7R8i.js
@@ -0,0 +1,1 @@
+import{j as e}from"./@radix-AvWw-1nd.js";import{e as o}from"./index-DWoLoYDY.js";function a(){return e.jsx(o,{color:"yellow",rounded:!1,className:"rounded-md",children:e.jsx("span",{className:"font-semibold uppercase",children:"Pro"})})}export{a as P};
