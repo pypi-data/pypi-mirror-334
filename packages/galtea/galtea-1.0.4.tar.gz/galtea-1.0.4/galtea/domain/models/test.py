b83b12ac02ca0563a82cfe8a61e5817ae147814e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 15:05:08 +0000

    Add package galtea/1.0.4/galtea-1.0.4.tar.gz

---
+++ packages/galtea/galtea-1.0.4.tar.gz/galtea-1.0.4/galtea/domain/models/test.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+from ...utils.from_camel_case_base_model import FromCamelCaseBaseModel
+
+class TestBase(FromCamelCaseBaseModel):
+    name: str
+    type: str
+    product_id: str
+    ground_truth_uri: str | None = None
+    uri: str | None = None
+
+class Test(TestBase):
+    id: str
+    created_at: datetime | None = None
+    deleted_at: datetime | None = None
