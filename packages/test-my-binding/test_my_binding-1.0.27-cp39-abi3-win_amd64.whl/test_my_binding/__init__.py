826444462b76d6a2c1e683ffa8b84d2aa9da5e67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 16:06:10 +0000

    Add package test-my-binding/1.0.27/test_my_binding-1.0.27-cp39-abi3-win_amd64.whl

---
+++ packages/test-my-binding/test_my_binding-1.0.27-cp39-abi3-win_amd64.whl/test_my_binding/__init__.py
@@ -0,0 +1,59 @@
+"""""" # start delvewheel patch
+def _delvewheel_patch_1_10_0():
+    import ctypes
+    import os
+    import platform
+    import sys
+    libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'test_my_binding.libs'))
+    is_conda_cpython = platform.python_implementation() == 'CPython' and (hasattr(ctypes.pythonapi, 'Anaconda_GetVersion') or 'packaged by conda-forge' in sys.version)
+    if sys.version_info[:2] >= (3, 8) and not is_conda_cpython or sys.version_info[:2] >= (3, 10):
+        if os.path.isdir(libs_dir):
+            os.add_dll_directory(libs_dir)
+    else:
+        load_order_filepath = os.path.join(libs_dir, '.load-order-test_my_binding-1.0.27')
+        if os.path.isfile(load_order_filepath):
+            import ctypes.wintypes
+            with open(os.path.join(libs_dir, '.load-order-test_my_binding-1.0.27')) as file:
+                load_order = file.read().split()
+            kernel32 = ctypes.WinDLL('kernel32', use_last_error=True)
+            kernel32.LoadLibraryExW.restype = ctypes.wintypes.HMODULE
+            kernel32.LoadLibraryExW.argtypes = ctypes.wintypes.LPCWSTR, ctypes.wintypes.HANDLE, ctypes.wintypes.DWORD
+            for lib in load_order:
+                lib_path = os.path.join(os.path.join(libs_dir, lib))
+                if os.path.isfile(lib_path) and not kernel32.LoadLibraryExW(lib_path, None, 8):
+                    raise OSError('Error loading {}; {}'.format(lib, ctypes.FormatError(ctypes.get_last_error())))
+
+
+_delvewheel_patch_1_10_0()
+del _delvewheel_patch_1_10_0
+# end delvewheel patch
+
+import os
+
+
+__author__ = ["tom van mele", "petras vestartas"]
+__copyright__ = "Block Research Group - ETH Zurich"
+__license__ = "MIT License"
+__email__ = ["van.mele@arch.ethz.ch", "vestartas@arch.ethz.ch"]
+__version__ = "1.0.27"
+
+HERE = os.path.dirname(__file__)
+
+HOME = os.path.abspath(os.path.join(HERE, "../../"))
+DATA = os.path.abspath(os.path.join(HOME, "data"))
+DOCS = os.path.abspath(os.path.join(HOME, "docs"))
+TEMP = os.path.abspath(os.path.join(HOME, "temp"))
+
+
+__all_plugins__ = [
+    "test_my_binding.booleans",
+    "test_my_binding.intersections",
+    "test_my_binding.meshing",
+    "test_my_binding.measure",
+    "test_my_binding.reconstruction",
+    "test_my_binding.triangulation",
+    "test_my_binding.slicer",
+    "test_my_binding.subdivision",
+]
+
+__all__ = ["HOME", "DATA", "DOCS", "TEMP"]
