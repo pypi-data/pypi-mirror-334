dc972a8e47eaf6dac500f331bc5d212d66405842
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 06:04:09 +0000

    Add package gas-station-spain/1.0/gas_station_spain-1.0.tar.gz

---
+++ packages/gas-station-spain/gas_station_spain-1.0.tar.gz/gas_station_spain-1.0/gas_station_spain/__init__.py
@@ -0,0 +1,17 @@
+"""Gas Station Spain"""
+
+from .gas_station import (
+    get_gas_stations,
+    get_municipalities,
+    get_price,
+    get_products,
+    get_provinces,
+)
+
+__all__ = [
+    "get_gas_stations",
+    "get_municipalities",
+    "get_price",
+    "get_products",
+    "get_provinces",
+]
