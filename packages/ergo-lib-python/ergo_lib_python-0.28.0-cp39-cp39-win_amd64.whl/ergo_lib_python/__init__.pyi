38ce5a5ba9138421c31897e57b705752ec7bc045
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 20:06:59 +0000

    Add package ergo-lib-python/0.28.0/ergo_lib_python-0.28.0-cp39-cp39-win_amd64.whl

---
+++ packages/ergo-lib-python/ergo_lib_python-0.28.0-cp39-cp39-win_amd64.whl/ergo_lib_python/__init__.pyi
@@ -0,0 +1,17 @@
+# Stub typehints for ergo_lib_python. Documentation is generated from these (sphinx-autoapi)
+# Note that class constructors here are defined as __init__, even though pyo3 defines them as __new__. This is because sphinx-autoapi doesn't render __new__
+import typing
+from typing import Optional, List, Self, cast
+
+from ergo_lib_python import multi_sig
+from ergo_lib_python import transaction
+from ergo_lib_python import wallet
+from ergo_lib_python import chain
+from ergo_lib_python import verifier
+from ergo_lib_python import sigma_protocol
+from ergo_lib_python import exceptions
+from ergo_lib_python import ergo_tree
+from ergo_lib_python import nipopow
+
+__version__: str = ...
+__all__ = ['wallet', 'chain', 'transaction', 'sigma_protocol', 'multi_sig', 'verifier', 'exceptions', 'ergo_tree', 'nipopow', '__version__']
