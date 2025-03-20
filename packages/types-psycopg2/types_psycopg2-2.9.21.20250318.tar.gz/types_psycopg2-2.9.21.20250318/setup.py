76b8858d27a2798fffddffdbec57ac5cfd32d66e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 20:07:05 +0000

    Add package types-psycopg2/2.9.21.20250318/types_psycopg2-2.9.21.20250318.tar.gz

---
+++ packages/types-psycopg2/types_psycopg2-2.9.21.20250318.tar.gz/types_psycopg2-2.9.21.20250318/setup.py
@@ -0,0 +1,56 @@
+from setuptools import setup
+
+name = "types-psycopg2"
+description = "Typing stubs for psycopg2"
+long_description = '''
+## Typing stubs for psycopg2
+
+This is a [PEP 561](https://peps.python.org/pep-0561/)
+type stub package for the [`psycopg2`](https://github.com/psycopg/psycopg2) package.
+It can be used by type-checking tools like
+[mypy](https://github.com/python/mypy/),
+[pyright](https://github.com/microsoft/pyright),
+[pytype](https://github.com/google/pytype/),
+[Pyre](https://pyre-check.org/),
+PyCharm, etc. to check code that uses `psycopg2`. This version of
+`types-psycopg2` aims to provide accurate annotations for
+`psycopg2~=2.9.10`.
+
+This package is part of the [typeshed project](https://github.com/python/typeshed).
+All fixes for types and metadata should be contributed there.
+See [the README](https://github.com/python/typeshed/blob/main/README.md)
+for more details. The source for this package can be found in the
+[`stubs/psycopg2`](https://github.com/python/typeshed/tree/main/stubs/psycopg2)
+directory.
+
+This package was tested with
+mypy 1.15.0,
+pyright 1.1.396,
+and pytype 2024.10.11.
+It was generated from typeshed commit
+[`b93eae52affaedfda79c00f662f50a2ef3b24c46`](https://github.com/python/typeshed/commit/b93eae52affaedfda79c00f662f50a2ef3b24c46).
+'''.lstrip()
+
+setup(name=name,
+      version="2.9.21.20250318",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      project_urls={
+          "GitHub": "https://github.com/python/typeshed",
+          "Changes": "https://github.com/typeshed-internal/stub_uploader/blob/main/data/changelogs/psycopg2.md",
+          "Issue tracker": "https://github.com/python/typeshed/issues",
+          "Chat": "https://gitter.im/python/typing",
+      },
+      install_requires=[],
+      packages=['psycopg2-stubs'],
+      package_data={'psycopg2-stubs': ['__init__.pyi', '_ipaddress.pyi', '_json.pyi', '_psycopg.pyi', '_range.pyi', 'errorcodes.pyi', 'errors.pyi', 'extensions.pyi', 'extras.pyi', 'pool.pyi', 'sql.pyi', 'tz.pyi', 'METADATA.toml', 'py.typed']},
+      license="Apache-2.0",
+      python_requires=">=3.9",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Programming Language :: Python :: 3",
+          "Typing :: Stubs Only",
+      ]
+)
