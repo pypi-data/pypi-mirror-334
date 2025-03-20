cb154b764d133b6bb512b5d1ef74375889b534bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 09:04:35 +0000

    Add package luzitoolip/1.1/luzitoolip-1.1.tar.gz

---
+++ packages/luzitoolip/luzitoolip-1.1.tar.gz/luzitoolip-1.1/setup.py
@@ -0,0 +1,23 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='luzitoolip',
+    version='1.1',
+    packages=find_packages(),
+    install_requires=[
+        'requests',  
+    ],
+    description='A simple IP info fetcher',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='luzi inc',
+    author_email='help@luzitool.ct.ws',
+    url='https://luzitool.ct.ws/luziip.php',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
