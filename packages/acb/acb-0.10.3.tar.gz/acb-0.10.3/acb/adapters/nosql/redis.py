79831202f329ae305a7caacd71690b833528adf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 12:05:51 +0000

    Add package acb/0.10.3/acb-0.10.3.tar.gz

---
+++ packages/acb/acb-0.10.3.tar.gz/acb-0.10.3/acb/adapters/nosql/redis.py
@@ -0,0 +1,23 @@
+# from redis_om import get_redis_connection
+# from redis_om import Migrator
+
+from pydantic import field_validator
+
+from ._base import NosqlBase, NosqlBaseSettings
+
+
+class NosqlSettings(NosqlBaseSettings):
+    cache_db: int = 0
+
+    @field_validator("cache_db")
+    @classmethod
+    def cache_db_not_zero(cls, v: int) -> int:  # noqa: F841
+        if v < 3 and v != 0:
+            raise ValueError("must be > 3 (0-2 are reserved)")
+        return 0
+
+
+class Nosql(NosqlBase):
+    def __init__(self) -> None:
+        ...
+        # Migrator().run()
