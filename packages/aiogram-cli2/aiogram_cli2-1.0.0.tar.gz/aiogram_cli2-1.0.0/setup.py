51e6db77e53842cfe18e75ae205fafc2405d5f75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 14:05:12 +0000

    Add package aiogram-cli2/1.0.0/aiogram_cli2-1.0.0.tar.gz

---
+++ packages/aiogram-cli2/aiogram_cli2-1.0.0.tar.gz/aiogram_cli2-1.0.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="aiogram_cli2",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=[
+        "watchdog"
+    ],
+    url="https://github.com/sinofarmonovzfkrvjl/aiogram-cli",
+    author="https://t.me/python_dev323",
+    description="bu aiogram kutubxonasi uchun aiogram cli (command line tool)",
+    entry_points={
+        'console_scripts': [
+            'aiogram-cli = aiogram_cli.cli:main',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
