ee2b62b84b296637f4cc3cc2ef92a0d0b35ed301
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 22:05:25 +0000

    Add package madcubapy/0.3.1/madcubapy-0.3.1.tar.gz

---
+++ packages/madcubapy/madcubapy-0.3.1.tar.gz/madcubapy-0.3.1/madcubapy/utils/__init__.py
@@ -0,0 +1,9 @@
+# madcubapy/utils/__init__.py
+
+"""
+The `madcubapy.utils` package provides utility functions to help the user
+work with astronomical data.
+"""
+
+from .spectral import *
+from .geometry import *
