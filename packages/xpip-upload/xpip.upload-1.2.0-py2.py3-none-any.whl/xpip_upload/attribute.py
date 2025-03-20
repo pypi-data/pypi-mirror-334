3b7f6cca467d5a18edd8be98543e306e2d97ad5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 22:04:51 +0000

    Add package xpip-upload/1.2.0/xpip.upload-1.2.0-py2.py3-none-any.whl

---
+++ packages/xpip-upload/xpip.upload-1.2.0-py2.py3-none-any.whl/xpip_upload/attribute.py
@@ -0,0 +1,15 @@
+# coding:utf-8
+
+from urllib.parse import urljoin
+
+__project__ = "xpip.upload"
+__version__ = "1.2.0"
+__description__ = "upload python package via twine"
+__urlhome__ = "https://github.com/bondbox/xpip/"
+__urlcode__ = __urlhome__
+__urldocs__ = __urlhome__
+__urlbugs__ = urljoin(__urlhome__, "issues")
+
+# author
+__author__ = "Mingzhe Zou"
+__author_email__ = "zoumingzhe@outlook.com"
