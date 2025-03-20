248c0431ef6daf73570d441a948ae8f5c1c48794
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 00:06:08 +0000

    Add package check-spyder/0.15/check_spyder-0.15.tar.gz

---
+++ packages/check-spyder/check_spyder-0.15.tar.gz/check_spyder-0.15/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+# python setup.py sdist
+# twine upload dist/*
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+# open('Readme_check_spyder.md', encoding='utf-8').read(),
+#packages=find_packages(),
+#install_requires=['os'],
+#long_description=open('check_spyder/Readme.md', encoding='utf-8').read(),
+setup(
+    name='check_spyder',
+    version='0.15',
+    packages=['check_spyder'],
+    description='Library to detect if you are using Spyder IDE or not.',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
+    author='Javier S. Zurdo',
+    url='https://github.com/javierzurdo/Check_Spyder',
+    classifiers=[
+                 'Programming Language :: Python :: 3',
+                 'License :: OSI Approved :: MIT License',
+                 'Operating System :: OS Independent',
+                ],
+    python_requires='>=3.6',
+)
