96914a1ffc9dc8af5f509fc0c4e471f344da2505
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 22:05:08 +0000

    Add package arpakitlib/1.8.25/arpakitlib-1.8.25.tar.gz

---
+++ packages/arpakitlib/arpakitlib-1.8.25.tar.gz/arpakitlib-1.8.25/arpakitlib/_arpakit_project_template_v_5_1/project/api/schema/out/client/common.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+import datetime as dt
+
+from project.api.schema.common import BaseSO
+from project.sqlalchemy_db_.sqlalchemy_model import SimpleDBM
+
+
+class SimpleDBMClientSO(BaseSO):
+    id: int
+    long_id: str
+    slug: str | None
+    creation_dt: dt.datetime
+
+    @classmethod
+    def from_dbm(cls, *, simple_dbm: SimpleDBM) -> SimpleDBMClientSO:
+        return cls.model_validate(simple_dbm.simple_dict_with_sd_properties())
