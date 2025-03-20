fd62fa5d4a6e11a1f3f80249be4b5095ec9977de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 18:05:15 +0000

    Add package cuda-core/0.2.0/cuda_core-0.2.0-cp312-cp312-win_amd64.whl

---
+++ packages/cuda-core/cuda_core-0.2.0-cp312-cp312-win_amd64.whl/cuda/core/experimental/utils.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2024, NVIDIA CORPORATION & AFFILIATES. ALL RIGHTS RESERVED.
+#
+# SPDX-License-Identifier: LicenseRef-NVIDIA-SOFTWARE-LICENSE
+
+from cuda.core.experimental._memoryview import (
+    StridedMemoryView,  # noqa: F401
+    args_viewable_as_strided_memory,  # noqa: F401
+)
