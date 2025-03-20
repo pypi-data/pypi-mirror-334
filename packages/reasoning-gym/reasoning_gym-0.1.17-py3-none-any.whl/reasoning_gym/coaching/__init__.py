fd60c9392fc637e81ccc1a226ce879ff4a056518
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 07:04:12 +0000

    Add package reasoning-gym/0.1.17/reasoning_gym-0.1.17-py3-none-any.whl

---
+++ packages/reasoning-gym/reasoning_gym-0.1.17-py3-none-any.whl/reasoning_gym/coaching/__init__.py
@@ -0,0 +1,15 @@
+from .attributes import AttributeDefinition, RangeAttributeDefinition, ScalarAttributeDefinition
+from .base_curriculum import BaseCurriculum
+from .coach import Coach, GroupedScores, ScoreBoard, ScoreStats
+
+__all__ = [
+    "AttributeType",
+    "AttributeDefinition",
+    "ScalarAttributeDefinition",
+    "RangeAttributeDefinition",
+    "BaseCurriculum",
+    "Coach",
+    "ScoreBoard",
+    "GroupedScores",
+    "ScoreStats",
+]
---
+++ packages/reasoning-gym/reasoning_gym-0.1.17-py3-none-any.whl/reasoning_gym/code/__init__.py
@@ -0,0 +1,8 @@
+"""
+Code reasing tasks
+"""
+
+from .bf import BFConfig, BFDataset
+from .codeio import CodeIOConfig, CodeIODataset
+
+__all__ = ["BFConfig", "BFDataset", "CodeIOConfig", "CodeIODataset"]
