280662955cab46188142e8a3d4ed1e0ae667d862
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 07:03:46 +0000

    Add package dj-image-uploader-widget/0.4.3/dj_image_uploader_widget-0.4.3.tar.gz

---
+++ packages/dj-image-uploader-widget/dj_image_uploader_widget-0.4.3.tar.gz/dj_image_uploader_widget-0.4.3/dj_image_uploader_widget/views.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+from .oss import upload_to_oss
+
+
+@require_POST
+@login_required
+def upload_view(request):
+    try:
+        image_file = request.FILES["image"]
+        pathname = "User-" + request.user.id
+        oss_url = upload_to_oss(image_file, pathname)
+        return JsonResponse({"url": oss_url})
+    except Exception as e:
+        return JsonResponse({"error": str(e)}, status=400)
