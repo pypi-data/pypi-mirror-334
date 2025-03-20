93ea0488d27ffd6f4346747e634664726a9b293d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 11:04:32 +0000

    Add package XMAItest/0.1.0/xmaitest-0.1.0-py3-none-any.whl

---
+++ packages/XMAItest/xmaitest-0.1.0-py3-none-any.whl/XMAItest/__init__.py
@@ -0,0 +1,4 @@
+from .XMAItest import story,photo,reply,poem,get_access_token  # 从子模块导入函数到顶层
+from .MAGIC import birthday # 从子模块导入函数到顶层
+
+__all__ = ['story','photo','reply','poem','get_access_token','birthday']  # 可选：明确导出的内容
