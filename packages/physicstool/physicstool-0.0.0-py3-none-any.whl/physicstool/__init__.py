f52be4a0e0d27f04a50901c419b3cedfae0fadc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 06:03:48 +0000

    Add package physicstool/0.0.0/physicstool-0.0.0-py3-none-any.whl

---
+++ packages/physicstool/physicstool-0.0.0-py3-none-any.whl/physicstool/__init__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# SPDX-License-Identifier: EUPL-1.2
+#  
+#  Copyright (c) 2025  Marc van der Sluys - Nikhef/Utrecht University - marc.vandersluys.nl
+#   
+#  This file is part of the physicstool Python package:
+#  A simple Python package to do basic physics calculations.
+#  See: https://github.com/MarcvdSluys/physicstool
+#   
+#  This is free software: you can redistribute it and/or modify it under the terms of the European Union
+#  Public Licence 1.2 (EUPL 1.2).  This software is distributed in the hope that it will be useful, but
+#  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+#  PURPOSE.  See the EU Public Licence for more details.  You should have received a copy of the European
+#  Union Public Licence along with this code.  If not, see <https://www.eupl.eu/1.2/en/>.
+
+
+"""physicstool package
+
+A simple Python package to do basic physics calculations.
+
+The code is being developed by `Marc van der Sluys <https://marc.vandersluys.nl>`_ of the Department of Physics
+at Utrecht University in the Netherlands, and the Netherlands Institute of Nuclear and High-Energy Physics
+(Nikhef) in Amsterdam.  The evTools package can be used under the conditions of the EUPL 1.2 licence.  These
+pages contain the API documentation.  For more information on the Python package, licence and source code, see
+the `physicstool GitHub page <https://github.com/MarcvdSluys/physicstool>`_.
+
+"""
+
+
+name = 'physicstool'
+__author__ = 'Marc van der Sluys - Nikhef/Utrecht University - marc.vandersluys.nl'
+
