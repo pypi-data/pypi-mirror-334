2d1045100f970aba4d63132b4f10e7297f873fb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 06:04:17 +0000

    Add package aitop/0.5.9/aitop-0.5.9-py3-none-any.whl

---
+++ packages/aitop/aitop-0.5.9-py3-none-any.whl/aitop/ui/components/__init__.py
@@ -0,0 +1,21 @@
+"""UI component modules."""
+
+from .header import HeaderComponent
+from .footer import FooterComponent
+from .tabs import TabsComponent
+from .overview import OverviewPanel
+from .gpu_panel import GPUPanel
+from .process_panel import ProcessPanel
+from .memory_panel import MemoryPanel
+from .cpu_panel import CPUPanel
+
+__all__ = [
+    'HeaderComponent',
+    'FooterComponent',
+    'TabsComponent',
+    'OverviewPanel',
+    'GPUPanel',
+    'ProcessPanel',
+    'MemoryPanel',
+    'CPUPanel'
+]
