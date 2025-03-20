1508fc79584190cc69f60c0fe42d80881ce929b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 12:05:00 +0000

    Add package zkeymap/0.1.3/zkeymap-0.1.3-py3-none-any.whl

---
+++ packages/zkeymap/zkeymap-0.1.3-py3-none-any.whl/zkeymap/layouts/__init__.py
@@ -0,0 +1,10 @@
+# Copyright (c) 2025 Frank David Martínez Muñoz <mnesarco>
+# SPDX-License-Identifier: MIT
+
+# ruff: noqa: F401
+
+"""
+Builtin layouts for common keyboards
+"""
+
+from .utils import import_layout_json, import_all_layouts_json
