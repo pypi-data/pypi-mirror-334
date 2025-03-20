172aa1ed030c8060a410afa7e89b9b2e35176dc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 18:05:41 +0000

    Add package mitol-django-google-sheets/2025.3.17/mitol_django_google_sheets-2025.3.17.tar.gz

---
+++ packages/mitol-django-google-sheets/mitol_django_google_sheets-2025.3.17.tar.gz/mitol_django_google_sheets-2025.3.17/mitol/google_sheets/apps.py
@@ -0,0 +1,18 @@
+"""Google Sheets app AppConfigs"""
+
+import os
+
+from mitol.common.apps import BaseApp
+
+
+class GoogleSheetsApp(BaseApp):
+    """Default configuration for the Google Sheets app"""
+
+    name = "mitol.google_sheets"
+    label = "google_sheets"
+    verbose_name = "Google Sheets"
+
+    required_settings = []
+
+    # necessary because this is a namespaced app
+    path = os.path.dirname(os.path.abspath(__file__))  # noqa: PTH100, PTH120
