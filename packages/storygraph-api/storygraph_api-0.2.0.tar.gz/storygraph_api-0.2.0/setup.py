f4002bf517ac661b057549b02b77ad1dd1da07d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 11:04:38 +0000

    Add package storygraph-api/0.2.0/storygraph_api-0.2.0.tar.gz

---
+++ packages/storygraph-api/storygraph_api-0.2.0.tar.gz/storygraph_api-0.2.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="storygraph-api",
+    version="0.2.0",
+    description="This package allows you to interact with and fetch data from the StoryGraph website.",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author="ym496",
+    author_email="yogendramishra2004@gmail.com",
+    url="https://github.com/ym496/storygraph-api",
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'beautifulsoup4',
+        'selenium',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords=["storygraph", "storygraph api", "api storygraph", "storygraph scraper"],
+    python_requires='>=3.10',
+)
