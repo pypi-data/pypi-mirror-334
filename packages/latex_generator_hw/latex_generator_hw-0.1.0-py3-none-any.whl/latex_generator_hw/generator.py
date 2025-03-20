ee9b22ffe7127f3d40305f645ee4ff8759acb0a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-20 16:05:24 +0000

    Add package latex_generator_hw/0.1.0/latex_generator_hw-0.1.0-py3-none-any.whl

---
+++ packages/latex_generator_hw/latex_generator_hw-0.1.0-py3-none-any.whl/latex_generator_hw/generator.py
@@ -0,0 +1,37 @@
+from functools import reduce
+
+def document_generator(*args: str) -> str:
+    return _document(*args)
+
+def _document(*args: str) -> str:
+    l = reduce(lambda acc, s: acc + [s], args, [_begin_doc()])
+    l = l + [_end_doc()]
+    return "\n".join(l)
+
+def _begin_doc() -> str:
+    return "\n".join(["\\documentclass{article}", "\\usepackage{graphicx}", "\\begin{document}"])
+
+def _end_doc() -> str:
+    return "\\end{document}"
+
+
+def table_generator(data: list[list[str]]) -> str:
+    new_data = list(map(_row, data))
+    column_number = len(data[0])
+    return "\n".join([_begin_table(column_number), "\n".join(new_data), _end_table()])
+
+def _begin_table(column_number: int) -> str:
+    return f"\\begin{{center}}\n\\begin{{tabular}}{_schema(column_number)}"
+
+def _end_table() -> str:
+    return "\\hline" + "\n" + "\\end{tabular}" + "\n" + "\\end{center}"
+
+def _schema(column_number: int) -> str:
+    return "{|" + "|".join(map(lambda _: "c", range(column_number))) + "|}"
+
+def _row(data: list[str]) -> str:
+    return "\\hline\n" + " & ".join(data) + " \\\\"
+
+
+def image_generator(path: str, scale: float) -> str:
+    return f"$$\\includegraphics[scale={scale}]{{{path}}}$$"
