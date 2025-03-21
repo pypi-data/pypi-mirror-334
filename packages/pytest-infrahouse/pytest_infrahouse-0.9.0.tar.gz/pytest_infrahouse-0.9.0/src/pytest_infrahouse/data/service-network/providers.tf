894871671d180d7d6dd0334cf6a452f9f3690d38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 03:16:01 +0000

    Add package pytest-infrahouse/0.9.0/pytest_infrahouse-0.9.0.tar.gz

---
+++ packages/pytest-infrahouse/pytest_infrahouse-0.9.0.tar.gz/pytest_infrahouse-0.9.0/src/pytest_infrahouse/data/service-network/providers.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  dynamic "assume_role" {
+    for_each = var.role_arn != null ? [1] : []
+    content {
+      role_arn = var.role_arn
+    }
+  }
+  region = var.region
+  default_tags {
+    tags = {
+      "created_by" : "infrahouse/pytest-infrahouse/service-network"
+    }
+  }
+}
