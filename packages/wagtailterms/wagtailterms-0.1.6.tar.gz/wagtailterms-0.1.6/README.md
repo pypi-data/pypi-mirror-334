9809b3c1261d5076528166dd561984c013cf9f07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 01:33:03 +0000

    Add package wagtailterms/0.1.6/wagtailterms-0.1.6.tar.gz

---
+++ packages/wagtailterms/wagtailterms-0.1.6.tar.gz/wagtailterms-0.1.6/README.md
@@ -0,0 +1,227 @@
+# Wagtail Terms
+A Wagtail package to add support for a glossary terms entity to Draftail
+
+## Installation
+```bash
+pip install wagtailterms
+```
+Add `wagtailterms` to your `INSTALLED_APPS` in your Django settings file.
+Make sure you have rest_framework in your installed apps as well.
+
+Migrate the database
+```bash
+python manage.py migrate
+```
+
+
+Add `wagtailterms.urls` to your `urlpatterns` in your Django urls file.
+the url should look like this:
+```python
+path('api/terms/', include('wagtailterms.urls')),
+```
+
+---
+#### ⚠️ Note
+
+The url path can be anything you want. This is the url that will be used to access the terms on the frontend
+
+---
+
+## Settings
+All configurations for wagtailterms are done in the settings.py file in a dictionary called WAGTAILTERMS.
+
+-  icon - The icon for the terms. It is used in the draftail editor and for the viewset. All the icons available for [wagtail](https://docs.wagtail.org/en/latest/advanced_topics/icons.html) are valid options
+- menu_order - Change the position of the terms snippet in the menu.
+- style - Change the default css inline-style of the term
+
+```python
+    WAGTAILTERMS = {
+        'icon': 'snippet',
+        'menu_order': 200,
+        'style': "text-decoration-line: underline; text-decoration-color: green;text-decoration-thickness: 3px;color:green;",
+    }
+```
+
+## Usage
+This wagtail package adds a Draftail entity to create a term that is mapped to a definition. The most common use case would be for the user to hover over a word/phrase on a page and a definition would appear next to the word/phrase.
+It allows you to Highlight a word/phrase in the Draftail/richtext editor and search for a definition that was created as a TermSnippet. In the editor the term name and definition will appear on top of the phrase when hovering over the phrase.
+
+### Creating new terms
+Click in the admin side bar Terms -> Add New
+<p align="center" width="100%">
+<img width="50%" src="./example/images/create_term_1.png">
+</p>
+<p align="center" width="100%">
+<img width="50%" src="./example/images/create_term_2.png">
+</p>
+
+### Use term entity in editor
+
+<p align="center" width="100%">
+<img width="50%" src="./example/images/add_term_1.png">
+</p>
+
+Search for term
+
+
+<p align="center" width="100%">
+<img width="50%" src="./example/images/add_term_2.png">
+</p>
+<p align="center" width="100%">
+<img width="50%" src="./example/images/search_term.png">
+</p>
+
+Select term
+
+<p align="center" width="100%">
+<img width="50%" src="./example/images/add_term_3.png">
+</p>
+
+Entity displayed in editor
+
+<p align="center" width="100%">
+<img width="50%" src="./example/images/add_term_4.png">
+</p>
+
+<p align="center" width="100%">
+<img width="50%" src="./example/images/basic_term_editor_1.png">
+</p>
+
+<p align="center" width="100%">
+<img width="50%" src="./example/images/basic_term_editor_2.png">
+</p>
+
+
+### Display in template
+To display the terms on the frontend the term shows up as a `<span>` element 
+tag with a green underline and green text. In a future update this will be customizable. 
+The element has a data-term  attribute that contains the term id. It is up to the developer to fetch
+the term and definition to display it. To get the term by id fetch the term by id using the term API.
+
+Rendered HTML of the term in the frontend:
+```html
+<span style="text-decoration-line: underline; text-decoration-color: green;text-decoration-thickness: 3px;color:green;" data-term="1">term 1</span>
+```
+ℹ️ Above is the default style but this can be modified in the settings.
+
+#### Quick implementation: ([See full example](./example/home/templates/home/quick_start.html))
+
+Include in the template at the bottom of the page template.
+```html
+...
+{% include 'wagtailterms/wagtailterms.html' %}
+```
+ℹ️  This loads the advanced implementation in page template.
+
+#### The most basic implementation: ([See full example](./example/home/templates/home/basic_page.html))
+```javascript
+function showterm(e){
+    const termid = e.target.dataset.term
+    fetch(`/api/terms/${termid}/`)
+        .then(response=> response.json())
+        .then(data => {
+            alert(`term = ${data.term} \ndefinition = ${data.definition}`)
+        })
+}
+
+for(const term of document.querySelectorAll('[data-term]')){
+    term.onmouseover=showterm;
+}
+```
+The page would look like this:
+
+<p align="center" width="100%">
+<img width="50%" src="./example/images/view_basic_1.png">
+</p>
+
+On hover
+<p align="center" width="100%">
+<img width="50%" src="./example/images/view_basic_2.png">
+</p>
+
+#### A more advanced way would be to use a library like [tippy.js](https://atomiks.github.io/tippyjs/) to 
+create a tooltip that appears when hovering over the term. ([See full example](./example/home/templates/home/advanced_page.html))
+```javascript
+function add_tooltips(){
+    const tips = tippy('[data-term]', {
+        content: 'Loading...',
+        allowHTML:true,
+        interactive:true,
+        theme:'light',
+        animation: 'scale-subtle',
+        onCreate(instance) {
+            // Setup our own custom state properties
+            // set if was loaded
+            instance._fetchInitualized = false;
+            instance._error = null;
+        },
+        onShow(instance) {
+            if (instance._fetchInitualized || instance._error) {
+                return;
+            }
+
+            instance._fetchInitualized = true;
+            fetch(`/api/terms/${instance.reference.dataset.term}/`)
+                .then(response => response.json())
+                .then(data => {
+                    if (data.term){
+                        instance.setContent(`<h4>${data.term}</h4><p>${data.definition}</p>`);
+                    }else{
+                        instance.setContent("<p style='color: red'>Could not find definition</p>");
+                    }
+                })
+                .catch(error => {
+                    instance._error = error;
+                    instance.setContent(`Request failed. ${error}`);
+                });
+        },
+    });
+}
+add_tooltips();
+```
+The page would look like this:
+<p align="center" width="100%">
+<img width="50%" src="./example/images/view_advanced_1.png">
+</p>
+
+On hover
+<p align="center" width="100%">
+<img width="50%" src="./example/images/view_advanced_2.png">
+</p>
+
+
+## REST API
+`/api/terms/` will return:
+
+    [
+      {
+          "term": "term 1",
+          "definition": "<p data-block-key=\"51h82\">this is term 1</p>",
+          "id": 1
+      },
+        {
+          "term": "example 2",
+          "definition": "<p data-block-key=\"83b17\">this is another example</p>",
+          "id": 2
+      }
+    ]
+
+/api/terms/1/ will return:
+
+    {
+        "term": "term 1",
+        "definition": "<p data-block-key=\"51h82\">this is term 1</p>",
+        "id": 1
+    }
+
+## Changelog
+### 0.1.1
+- added settings to change the icon and menu order.
+
+### 0.1.2
+- fixed term search form wider than modal
+- Add dark mode support
+
+### 0.1.3
+- Added setting to change frontend styles
+- Added quick start template for default frontend implementation
---
+++ packages/wagtailterms/wagtailterms-0.1.6.tar.gz/wagtailterms-0.1.6/wagtailterms/static/wagtailterms/term.js
@@ -0,0 +1,207 @@
+// create popup to choose the term. The entire page uses the same popup. This makes it easier to select by id
+const termPopup = new Popup({
+    id: "term-selector-popup",
+    title: "Choose term",
+    allowClose: false,
+    backgroundColor:'var(--w-color-surface-field)',
+    textColor:'var(--w-text-context)',
+    titleColor:'var(--w-text-label)',
+    borderColor:'var(--w-color-border-furniture)',
+    content: `<div style="position: relative;">
+    <button style="position: absolute;top:-80px;right:0;background-color: var(--w-color-surface-field)" id="term-selector-popup-close">X</button>
+    <label for="term-selector-popup-search-box">Find Term</label><br>
+    <input type="search" name="fname" id="term-selector-popup-search-box" style="width: 100%; width: -moz-available; width: -webkit-fill-available; width: fill-available;">
+    <div id="term-selector-popup-search-buttons-frame"></div>
+    </div>
+    `,
+});
+
+
+
+// Not a real React component – just creates the entities as soon as it is rendered.
+class TermSource extends window.React.Component {
+    state = {terms: []}
+
+    // close window and cancel user adding a term
+    handleClose = (e) => {
+        const {onComplete, editorState} = this.props;
+        termPopup.hide()
+
+        // Draftail requires that must set editor state
+        onComplete(editorState);
+    }
+    handleSetTerm = (e) => {
+        // get the term id
+        const termId = parseInt(e.target.dataset.termId)
+        // get the term from the id
+        const term = this.state.terms.find((term) => term.id === termId)
+
+        // close the term popup once term is found
+        termPopup.hide();
+
+        const {editorState, entityType, onComplete} = this.props;
+        const content = editorState.getCurrentContent();
+        const selection = editorState.getSelection();
+
+        // get currently selected text
+        const anchorKey = selection.getAnchorKey();
+        const start = selection.getStartOffset();
+        const end = selection.getEndOffset()
+        const current_selected_text = content.getBlockForKey(anchorKey).getText().slice(start, end)
+
+        // get the searchbox and insert the value of the highlighted text
+        const searchBox = document.getElementById("term-selector-popup-search-box");
+        searchBox.value = current_selected_text;
+
+        // Uses the Draft.js API to create a new entity with the right data.
+        const contentWithEntity = content.createEntity(
+            entityType.type,
+            'MUTABLE',
+            {text: current_selected_text, term: term, id: this.state.id},
+        );
+
+        // add text in position of the old text. If no text was selected put the text of the term.
+        const newContent = window.DraftJS.Modifier.replaceText(
+            content,
+            selection,
+            current_selected_text.length === 0 ? term.term : current_selected_text,
+            null,
+            contentWithEntity.getLastCreatedEntityKey(),
+        );
+
+        // create the new editor state with the new entity
+        const nextState = window.DraftJS.EditorState.push(
+            editorState,
+            newContent,
+            'insert-characters',
+        );
+
+        // update the editor to show the changes
+        onComplete(nextState);
+    }
+
+    componentDidMount() {
+        // open the term selector popup
+        termPopup.show();
+
+        // make editor not freeze on close - use custom close button so have to set on click method for it
+        const searchClose = document.getElementById("term-selector-popup-close");
+        searchClose.onclick = this.handleClose;
+
+        // get the selected text
+        const searchBox = document.getElementById("term-selector-popup-search-box");
+        const frame = document.getElementById("term-selector-popup-search-buttons-frame");
+        const {editorState, entityType, onComplete} = this.props;
+        const content = editorState.getCurrentContent();
+        const selection = editorState.getSelection();
+        const anchorKey = selection.getAnchorKey();
+        const start = selection.getStartOffset();
+        const end = selection.getEndOffset()
+        const current_selected_text = content.getBlockForKey(anchorKey).getText().slice(start, end)
+
+        // function to search for terms that match the search text. All matches get added as a button in popup.
+        // if button is clicked will set the term to button
+        const getSearchTerms = () => {
+            fetch(`${WAGTAIL_TERM_PATH}?q=${searchBox.value}`)
+                .then(response => response.json())
+                .then(data => {
+                    this.setState({terms: data})
+                    frame.innerHTML = "";
+                    for (const item of data) {
+                        const button_style = 'background-color: var(--w-color-surface-button-default); color:var(--w-color-text-button); margin: 5px;'
+                        const update_hover_colors = "onMouseOver=\"this.style.backgroundColor='var(--w-color-surface-button-hover)'\" onMouseOut=\"this.style.backgroundColor='var(--w-color-surface-button-default)'"
+                        frame.innerHTML += `<button data-term-id="${item.id}" style="${button_style}" ${update_hover_colors}">${item.term}</button>`
+                    }
+                    for (const button of frame.children) {
+                        button.onclick = this.handleSetTerm
+                    }
+                })
+        };
+
+        // set the searchbox text to selected text
+        searchBox.value = current_selected_text;
+
+        // update search while the user types
+        searchBox.onkeyup = (e) => {
+            getSearchTerms();
+        }
+
+        // run the search initially
+        getSearchTerms();
+    }
+
+    render() {
+        // always returns null
+        return null
+    }
+}
+
+
+// function based components don't work
+// this is a term item that shows up in the editor
+class Term extends window.React.Component{
+    constructor(props) {
+        super(props);
+        // get the data and the term
+        const {entityKey, contentState} = props;
+        const data = contentState.getEntity(entityKey).getData();
+        this.state = {
+            // generate a unique string to this component so the tool tip will be applied to only this term
+            randomString:(Math.random() + 1).toString(36).substring(7),
+            term:data.term
+        }
+    }
+
+    componentDidMount(){
+        tippy(document.getElementById(`term_${this.state.term.id}_${this.state.randomString}`),
+        {
+            content: `<h4 style="color: white">${this.state.term.term}</h4><p>${this.state.term.definition}</p>`,
+            allowHTML: true,
+            interactive: true,
+            appendTo: () => document.body, theme: 'light-border'
+        });
+    }
+
+    render(){
+        // this is the jsx equivalent. jsx is not available since this is using the build system of wagtail which only gives access to React.
+        // cannot use the build system of the rest of the project since it doesn't load at
+        // the correct time and makes the editor crash when using this entity
+        // return <span id={`term_${data.term.id}_${randomString}`} style={
+        //         {
+        //             textDecorationLine: 'underline',
+        //             textDecorationColor: "green",
+        //             textDecorationThickness: 3,
+        //             color: 'green'
+        //         }
+        //     }>
+        //     {props.children}
+        // </span>
+
+        // converting a string to camel case so that the same styles can be used in the editor and the frontend with no duplication.
+        // this helps when changing the style setting so that it looks the same in the admin and frontend.
+        // https://stackoverflow.com/a/42124821
+        const convertToCamel=(string)=>{
+            const camelize = (string) =>  string.replace(/-([a-z])/gi,(s, group) =>  group.toUpperCase());
+            const style2object = (style) => style.split(';').filter(s => s.length)
+                .reduce((a, b) => {
+                    const keyValue = b.split(':');
+                    a[camelize(keyValue[0])] = keyValue[1] ;
+                    return a;
+                } ,{});
+            return style2object(string)
+        };
+
+        return window.React.createElement("span", {
+            id: `term_${this.state.term.id}_${this.state.randomString}`,
+            // WAGTAIL_TERM_STYLE is injected in wagtail_hooks file. This is the style set for the term as a string.
+            style:convertToCamel(WAGTAIL_TERM_STYLE),
+            children: this.props.children
+    })
+    }
+}
+
+window.draftail.registerPlugin({
+    type: 'TERM',
+    source: TermSource,
+    decorator: Term,
+}, 'entityTypes');
