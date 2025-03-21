0fc70018cee1b3134a774fb8c3452c4e4939a6dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 03:16:05 +0000

    Add package flux0-server/0.1.0b1/flux0_server-0.1.0b1.tar.gz

---
+++ packages/flux0-server/flux0_server-0.1.0b1.tar.gz/flux0_server-0.1.0b1/static/assets/_sessionId-BnIEB3bm.js
@@ -0,0 +1,1 @@
+import{j as r,E as n}from"./index-4HqW0s2V.js";const s=function({error:o}){return r.jsx(n,{error:o})};export{s as errorComponent};
---
+++ packages/flux0-server/flux0_server-0.1.0b1.tar.gz/flux0_server-0.1.0b1/static/assets/index-hnSE82OW.js
@@ -0,0 +1,1 @@
+import{E as o}from"./index-4HqW0s2V.js";const n=o;export{n as errorComponent};
