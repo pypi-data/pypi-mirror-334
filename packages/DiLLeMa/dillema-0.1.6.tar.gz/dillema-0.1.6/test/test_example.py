60d82862988c81a0a2136bd965ebbbd11ad57a80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 02:31:23 +0000

    Add package DiLLeMa/0.1.6/dillema-0.1.6.tar.gz

---
+++ packages/DiLLeMa/dillema-0.1.6.tar.gz/dillema-0.1.6/test/test_example.py
@@ -0,0 +1,7 @@
+from dillema import example
+
+
+def test_example():
+    assert example.say_hello("Hello") == "Hello, Hello!"
+    assert example.say_hello("World") == "Hello, World!"
+    assert example.say_hello("!@#") == "Hello, !@#!"
