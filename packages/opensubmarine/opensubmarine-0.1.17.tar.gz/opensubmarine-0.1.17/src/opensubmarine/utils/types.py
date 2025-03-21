cc49929bb33436e0f0e05e0bbb02c62f0d802248
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 00:07:12 +0000

    Add package opensubmarine/0.1.17/opensubmarine-0.1.17.tar.gz

---
+++ packages/opensubmarine/opensubmarine-0.1.17.tar.gz/opensubmarine-0.1.17/src/opensubmarine/utils/types.py
@@ -0,0 +1,7 @@
+import typing
+from algopy import arc4
+
+Bytes8: typing.TypeAlias = arc4.StaticArray[arc4.Byte, typing.Literal[8]]
+Bytes32: typing.TypeAlias = arc4.StaticArray[arc4.Byte, typing.Literal[32]]
+Bytes64: typing.TypeAlias = arc4.StaticArray[arc4.Byte, typing.Literal[64]]
+Bytes256: typing.TypeAlias = arc4.StaticArray[arc4.Byte, typing.Literal[256]]
