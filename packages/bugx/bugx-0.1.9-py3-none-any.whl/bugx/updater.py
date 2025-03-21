a2dae9c8a7f89e25ea6a8764d594321becd6e4fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 03:15:41 +0000

    Add package bugx/0.1.9/bugx-0.1.9-py3-none-any.whl

---
+++ packages/bugx/bugx-0.1.9-py3-none-any.whl/bugx/updater.py
@@ -0,0 +1,76 @@
+"""updater"""
+
+import subprocess
+import sys
+import time
+from typing import LiteralString
+
+import requests
+from flagsmith import Flagsmith
+from flagsmith.exceptions import FlagsmithAPIError
+
+from bugx.__about__ import __version__, app_name
+from bugx.utils import Command, Display, Network, Prettier, console
+
+display = Display()
+
+APP_NAME: LiteralString = app_name.title()
+
+
+def get_changelog(app_exit: bool = True) -> None:
+    """gives the changelog"""
+    try:
+        Network(url=None).check(to_exit=app_exit)
+        flagsmith = Flagsmith(
+            environment_key="ser.NJ2Sh3B2NJGLC9hYsKYrJx",
+        )
+        changelog = flagsmith.get_environment_flags().get_feature_value(feature_name="changelog")
+        console.print(Prettier(message="\nHere is the changelog:").bold_green)
+        console.print(changelog)
+    except FlagsmithAPIError as api_error:
+        display.connection_error(message=str(object=api_error), app_exit=app_exit)
+
+
+def get_latest_version() -> str:
+    """gives the latest version."""
+    try:
+        url: LiteralString = f"https://pypi.python.org/pypi/{app_name}/json"
+        r: requests.Response = requests.get(url=url, timeout=5)
+        r.raise_for_status()
+        return r.json()["info"]["version"]
+    except requests.exceptions.ConnectionError as connection_error:
+        raise ConnectionError(
+            f"Failed to fetch the latest {APP_NAME} version."
+        ) from connection_error
+
+
+def update():
+    """update"""
+    try:
+        with console.status(status="Checking for Updates...") as update_status:
+            update_status.start()
+            latest_version = get_latest_version()
+            current_version = __version__
+
+            if latest_version > current_version:
+                update_status.update(
+                    status=f"Got a new version {latest_version}. Kicking myself to update."
+                )
+                Command(
+                    command=[sys.executable, "-m", "pip", "install", "--upgrade", app_name]
+                ).run(app_exit=True, error_msg="internal error.")
+                update_status.stop()
+                display.success(f"Updated to v{latest_version}.")
+                get_changelog()
+            else:
+                console.print(":sparkles: Hola... Im already updated.")
+    except IOError as e:
+        display.connection_error(e)
+    except KeyboardInterrupt as _:
+        display.interrupt(f"While updating {APP_NAME}.")
+    except subprocess.CalledProcessError as update_error:
+        display.error(f"Failed to update {app_name} due to {update_error.stderr}")
+
+
+if __name__ == "__main__":
+    update()
---
+++ packages/bugx/bugx-0.1.9-py3-none-any.whl/bugx/utils.py
@@ -0,0 +1,166 @@
+"""utils"""
+
+import subprocess
+import sys
+from typing import Literal, Optional, Union
+
+import requests
+
+from bugx import console
+
+
+class Prettier:
+    """prettier"""
+
+    def __init__(self, message: str):
+        self.message: str = message
+
+    @property
+    def bold_yellow(self) -> str:
+        """bold yellow"""
+        return f"[bold yellow]{self.message}[/bold yellow]"
+
+    @property
+    def bold_green(self) -> str:
+        """bold green"""
+        return f"[bold green]{self.message}[/bold green]"
+
+    @property
+    def bold_red(self) -> str:
+        """bold red"""
+        return f"[bold red]{self.message}[/bold red]"
+
+    def apply(self, weight: Literal["bold", "italic"], color: str) -> str:
+        """apply"""
+        return f"[{weight} {color}]{self.message}[/{weight} {color}]"
+
+
+class Display:
+    """Display"""
+
+    def __init__(self):
+        self.print = console.print
+
+    def interrupt(self, message: str, app_exit: bool = True) -> None:
+        """interrupt"""
+        self.print(
+            f":unamused: :{Prettier(message=f"Dude you Interrupted me. {message}").bold_yellow}"
+        )
+        if app_exit:
+            sys.exit(2)
+
+    def success(self, message: str) -> None:
+        """success"""
+        self.print(f":white_check_mark: :{Prettier(message=message).bold_green}")
+
+    def error(self, message: str, app_exit: bool = False, new_line: bool = False) -> None:
+        """error"""
+        self.print(
+            f"{"\n" if new_line else ""}:x: :{Prettier(message=message).bold_red}",
+            new_line_start=new_line,
+        )
+        if app_exit:
+            sys.exit(1)
+
+    def connection_error(
+        self, message: str, app_exit: bool = False, new_line: bool = False
+    ) -> None:
+        """connection error"""
+        self.print(
+            f"{"\n" if new_line else ""}:sleeping: :{Prettier(message=message).bold_red}",
+            new_line_start=new_line,
+        )
+        if app_exit:
+            sys.exit(1)
+
+
+class Network(Display):
+    """Network"""
+
+    def __init__(self, url: Optional[str]):
+        super().__init__()
+        self.timeout = 5
+        self.url = "https://www.google.com" if not url else url
+
+    def check(self, to_exit: bool) -> None:
+        """checks for network"""
+        try:
+            r = requests.get(self.url, timeout=self.timeout)
+            r.raise_for_status()
+        except requests.exceptions.ConnectionError:
+            self.connection_error(
+                message="No Network. Please connect to Internet :signal_strength:.",
+                app_exit=to_exit,
+            )
+
+
+class Command(Display):
+    """command runner"""
+
+    def __init__(
+        self,
+        command: Union[str, list[str]],
+        verbose: bool = False,
+    ):
+        super().__init__()
+        self.command: str | list[str] = command
+        self.verbose: bool = verbose
+
+    def run(self, error_msg: Optional[str] = None, app_exit: bool = True) -> str:
+        """runs the command"""
+        formatted_command: str = ""
+        cmd_list: list = []
+
+        if isinstance(self.command, str):
+            cmd_list = self.command.split()
+            formatted_command = self.command
+        if isinstance(self.command, list):
+            cmd_list = self.command
+            formatted_command = " ".join(self.command)
+        if self.verbose:
+            console.print(f"Running command: {formatted_command}")
+        try:
+            with subprocess.Popen(
+                args=cmd_list,
+                stderr=subprocess.PIPE,
+                stdout=subprocess.PIPE,
+                text=True,
+            ) as run_command:
+                stdout, stderr = run_command.communicate()
+                if self.verbose:
+                    console.print(
+                        f"Output: {stdout if stdout else "Empty"}\nError: {stderr if stderr else "Empty"}"
+                    )
+                if run_command.returncode != 0:
+                    raise subprocess.CalledProcessError(
+                        returncode=run_command.returncode,
+                        cmd=cmd_list,
+                        stderr=(
+                            f"Failed to run the command {formatted_command}. {stderr}"
+                            if not error_msg
+                            else error_msg
+                        ),
+                        output=stdout,
+                    )
+            return stdout.strip()
+        except FileNotFoundError as _:
+            self.error(message=f"{formatted_command} command not found.", app_exit=app_exit)
+            return "empty"
+        except KeyboardInterrupt as _:
+            self.interrupt(message="While running the command.", app_exit=app_exit)
+            return "empty"
+
+
+if __name__ == "__main__":
+    # Display().error("mess")
+    # Network(url=None).check(to_exit=True)
+    try:
+        print(
+            Command(command=[sys.executable, "-m", "uv", "sync", "-P", "bugx"], verbose=False).run(
+                app_exit=False
+            )
+        )
+    except KeyboardInterrupt as e:
+        print(e)
+    except subprocess.CalledProcessError as err:
+        print(err.stderr, err.returncode)
