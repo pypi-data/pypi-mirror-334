b35f4539dcd9005929b6bf347e371ff3d29980b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 03:15:22 +0000

    Add package geopapyrus/0.1.1/geopapyrus-0.1.1-pp310-pypy310_pp73-manylinux_2_17_s390x.manylinux2014_s390x.whl

---
+++ packages/geopapyrus/geopapyrus-0.1.1-pp310-pypy310_pp73-manylinux_2_17_s390x.manylinux2014_s390x.whl/geopapyrus/__init__.pyi
@@ -0,0 +1,15 @@
+def distance_haversine_m(lat1: float, lon1: float, lat2: float, lon2: float) -> float:
+    """Calculates haversine distance in meters for given 2 points
+    Uses this function: https://docs.rs/geo/latest/geo/algorithm/haversine_distance/trait.HaversineDistance.html
+
+    Returns:
+        float: distance in meters
+    """
+
+def distance_geodesic_m(lat1: float, lon1: float, lat2: float, lon2: float) -> float:
+    """Calculates geodesic distance in meters for given 2 points
+    Uses this function: https://docs.rs/geo/latest/geo/algorithm/geodesic_distance/trait.GeodesicDistance.html
+
+    Returns:
+        float: distance in meters
+    """
