bfbaa2ff56184c32184957b93eff0d45db306faa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 01:32:48 +0000

    Add package talentro-definitions/0.1.9/talentro_definitions-0.1.9.tar.gz

---
+++ packages/talentro-definitions/talentro_definitions-0.1.9.tar.gz/talentro_definitions-0.1.9/src/talentro/integrations/models.py
@@ -0,0 +1,28 @@
+import uuid
+
+from sqlalchemy import Column, JSON
+from sqlmodel import Field
+from sqlalchemy.orm import registry
+
+from ..general.models import BaseModel, ClintSpecificModel
+
+
+mapper_registry = registry()
+
+@mapper_registry.mapped
+class Integration(ClintSpecificModel, table=True):
+    name: str = Field(index=True)
+    icon: str = Field(index=True)
+    type: str = Field(index=True)
+    tag: str = Field(index=True, nullable=True)
+    enabled: bool = Field(default=True, nullable=True)
+    description: str = Field(index=True, nullable=True)
+    code_reference: str = Field(index=True)
+    setup_config: dict = Field(sa_column=Column(JSON))
+
+
+@mapper_registry.mapped
+class Link(BaseModel, table=True):
+    company: uuid.UUID = Field(index=True)
+    auth_config: dict = Field(sa_column=Column(JSON))
+    integration_id: uuid.UUID = Field(foreign_key="integration.id")
