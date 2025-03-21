a6470bf246794fe85e0517c7c0b331ae9f45f2cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 02:31:06 +0000

    Add package zenml/0.75.1/zenml-0.75.1.tar.gz

---
+++ packages/zenml/zenml-0.75.1.tar.gz/zenml-0.75.1/src/zenml/zen_server/dashboard/assets/ProviderIcon-DKN3Gdcg.js
@@ -0,0 +1,1 @@
+import{j as r}from"./@radix-AvWw-1nd.js";import{S as o,a as n,b as m}from"./gcp-0u4le6mC.js";import{n as t}from"./index-DWoLoYDY.js";function x({provider:c,className:e,...s}){const a=t("w-5 h-5 shrink-0",e);switch(c){case"aws":return r.jsx(m,{...s,className:a});case"azure":return r.jsx(n,{...s,className:a});case"gcp":return r.jsx(o,{...s,className:a})}}export{x as C};
