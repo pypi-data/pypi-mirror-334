219e31a9bb5ae6a0f2b74d969bd5bddeb3a1da35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 02:31:04 +0000

    Add package smas/1.0.0/smas-1.0.0.tar.gz

---
+++ packages/smas/smas-1.0.0.tar.gz/smas-1.0.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="smas",
+    version="1.0.0",
+    description="This package is reserved.",
+    long_description="This package is reserved.",
+    long_description_content_type="text/plain",
+    author="Praveen K",
+    author_email="cio.kyras@gmail.com",
+    packages=["smas"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires=">=3.6",
+)
