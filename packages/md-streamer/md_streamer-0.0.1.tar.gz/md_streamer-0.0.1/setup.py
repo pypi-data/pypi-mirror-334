13681270017563c08dc439f91ecac8160bd14520
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 02:31:13 +0000

    Add package md-streamer/0.0.1/md_streamer-0.0.1.tar.gz

---
+++ packages/md-streamer/md_streamer-0.0.1.tar.gz/md_streamer-0.0.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='md_streamer',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+        'mistune',
+    ],
+    author='Gangchain',
+    author_email='gaganjot1872@gmail.com',
+    description='streaming markdown to html converter',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/GangChain/md_streamer-py',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ],
+    python_requires='>=3.9',
+)
