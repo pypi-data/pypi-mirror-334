68c3ec4728be40722970ef5fdbf30722f05c5ed2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 02:31:51 +0000

    Add package bgm-net/0.1.0/bgm_net-0.1.0-py3-none-any.whl

---
+++ packages/bgm-net/bgm_net-0.1.0-py3-none-any.whl/package/server.py
@@ -0,0 +1,49 @@
+import socket
+
+class Packets:
+    @staticmethod
+    def Constructor(data, type: str):
+        return f"{type}|".encode() + "|".join(data).encode()
+    
+    @staticmethod
+    def Deconstructor(data: str):
+        splitted = data.decode().split("|")
+        return (splitted[0], splitted[1:])
+
+class BGM_Server:
+    def __init__(self, host, port):
+        self.Address = (host, port)
+        self.Sock = None
+        
+        self.Clients = []
+        
+        self.ReciveCallBacks = []
+    
+    def recive_event(self, callback):
+        self.ReciveCallBacks.append(callback)
+    
+    def start_server(self):
+        self.Sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.Sock.bind(self.Address)
+        
+        print(f"Server on {self.Address[0]}:{self.Address[1]}")
+        
+        while True:
+            try:
+                message, client_address = self.Sock.recvfrom(1024)
+                
+                message = Packets.Deconstructor(message)
+
+                if message[0] == "CONNECT":
+                    self.Clients.append(client_address)
+                elif message[0] == "DISCONNECT":
+                    self.Clients.remove(client_address)
+                else:
+                    for callback in self.ReciveCallBacks:
+                        callback(message, client_address)
+            except:
+                pass
+                
+    def broadcast(self, message):
+        for client in self.Clients:
+            self.Sock.sendto(message, client)
