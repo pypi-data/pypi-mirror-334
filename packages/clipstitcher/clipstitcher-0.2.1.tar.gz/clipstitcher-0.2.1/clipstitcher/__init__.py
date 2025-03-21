b02525717ca66b501aeaaaa830df67604769dfd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 01:33:15 +0000

    Add package clipstitcher/0.2.1/clipstitcher-0.2.1.tar.gz

---
+++ packages/clipstitcher/clipstitcher-0.2.1.tar.gz/clipstitcher-0.2.1/clipstitcher/__init__.py
@@ -0,0 +1,14 @@
+from .scene_objects import (
+    default_options,
+    Image, Html_page, Tweet, 
+    Video, Overlay, Scene_sequence,
+    LinearTransform, LinearTransition,
+    )
+
+from .client_sync import (
+    ClientPlayer,
+    )
+
+from .host_sync import (
+    Uploader,
+    )
