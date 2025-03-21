859f8233d3b78cb2fde5da22049d8e56460c2f80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 01:33:48 +0000

    Add package usdm4/0.1.0/usdm4-0.1.0-py3-none-any.whl

---
+++ packages/usdm4/usdm4-0.1.0-py3-none-any.whl/usdm4/api/population_definition.py
@@ -0,0 +1,28 @@
+from typing import Union, List, Literal, Annotated
+from annotated_types import Len
+from .api_base_model import ApiBaseModelWithIdNameLabelAndDesc
+from .code import Code
+from .range import Range
+from .characteristic import Characteristic
+from .comment_annotation import CommentAnnotation
+
+
+class PopulationDefinition(ApiBaseModelWithIdNameLabelAndDesc):
+    includesHealthySubjects: bool
+    plannedEnrollmentNumber: Union[Range, None] = None
+    plannedCompletionNumber: Union[Range, None] = None
+    plannedSex: Annotated[list[Code], Len(min_length=0, max_length=2)] = []
+    criterionIds: List[str] = []
+    plannedAge: Union[Range, None] = None
+    notes: List[CommentAnnotation] = []
+    instanceType: Literal["PopulationDefinition"]
+
+
+class StudyCohort(PopulationDefinition):
+    characteristics: List[Characteristic] = []
+    instanceType: Literal["StudyCohort"]
+
+
+class StudyDesignPopulation(PopulationDefinition):
+    cohorts: List[StudyCohort] = []
+    instanceType: Literal["StudyDesignPopulation"]
---
+++ packages/usdm4/usdm4-0.1.0-py3-none-any.whl/usdm4/api/range.py
@@ -0,0 +1,11 @@
+from typing import Literal, Union
+from .api_base_model import ApiBaseModelWithId
+from .code import Code
+
+
+class Range(ApiBaseModelWithId):
+    minValue: float
+    maxValue: float
+    unit: Union[Code, None] = None
+    isApproximate: bool
+    instanceType: Literal["Range"]
