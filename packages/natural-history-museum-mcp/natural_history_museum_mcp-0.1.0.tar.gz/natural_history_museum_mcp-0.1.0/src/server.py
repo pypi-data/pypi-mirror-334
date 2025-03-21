f6245689ed699ceb2a38d6ff3e533658d44c5ef8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 00:07:29 +0000

    Add package natural-history-museum-mcp/0.1.0/natural_history_museum_mcp-0.1.0.tar.gz

---
+++ packages/natural-history-museum-mcp/natural_history_museum_mcp-0.1.0.tar.gz/natural_history_museum_mcp-0.1.0/src/server.py
@@ -0,0 +1,13 @@
+from mcp.server.fastmcp import FastMCP
+import nhm_api
+import json
+
+# Create an MCP server
+mcp = FastMCP("Natural History Museum Data API")
+
+
+@mcp.tool()
+def search_specimens(query: str, limit: int) -> str:
+    nhm_api_result = nhm_api.get_by_query(query, limit)
+
+    return json.dumps(nhm_api_result)
