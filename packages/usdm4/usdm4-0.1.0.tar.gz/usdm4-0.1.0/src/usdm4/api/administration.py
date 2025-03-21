fff142f786636b4702e51517118d3fe7a8cede8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 01:33:48 +0000

    Add package usdm4/0.1.0/usdm4-0.1.0.tar.gz

---
+++ packages/usdm4/usdm4-0.1.0.tar.gz/usdm4-0.1.0/src/usdm4/api/administration.py
@@ -0,0 +1,16 @@
+from typing import List, Union, Literal
+from .api_base_model import ApiBaseModelWithIdNameLabelAndDesc
+from .quantity import Quantity
+from .administration_duration import AdministrationDuration
+from .alias_code import AliasCode
+from .comment_annotation import CommentAnnotation
+
+
+class Administration(ApiBaseModelWithIdNameLabelAndDesc):
+    duration: AdministrationDuration
+    dose: Quantity
+    route: AliasCode
+    frequency: AliasCode
+    administrableProductId: Union[str, None] = None
+    notes: List[CommentAnnotation] = []
+    instanceType: Literal["Administration"]
