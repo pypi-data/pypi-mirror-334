04a983928b9613b7c9db1b19947ec84909196a1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 00:07:19 +0000

    Add package guppylang/0.17.0/guppylang-0.17.0.tar.gz

---
+++ packages/guppylang/guppylang-0.17.0.tar.gz/guppylang-0.17.0/devenv.nix
@@ -0,0 +1,44 @@
+{ pkgs, lib, ... }:
+
+{
+  # for building optional tket2 dependency
+  # see https://github.com/CQCL/tket2/blob/main/devenv.nix
+  packages = [
+    pkgs.just
+    pkgs.llvmPackages_14.libllvm
+    pkgs.libffi
+    pkgs.libxml2
+    pkgs.graphviz
+  ]
+  ++ lib.optionals pkgs.stdenv.isLinux [
+    pkgs.stdenv.cc.cc.lib
+  ]
+  ++ lib.optionals pkgs.stdenv.isDarwin (
+    with pkgs.darwin.apple_sdk; [
+      frameworks.CoreServices
+      frameworks.CoreFoundation
+    ]
+  );
+
+  enterShell = ''
+    export PATH="$UV_PROJECT_ENVIRONMENT/bin:$PATH"
+  '';
+
+  languages.python = {
+    enable = true;
+    uv = {
+      enable = true;
+      sync.enable = true;
+    };
+    venv.enable = true;
+  };
+
+  env.LLVM_SYS_140_PREFIX = pkgs.llvmPackages_14.libllvm.dev;
+
+  env.LD_LIBRARY_PATH = "${lib.makeLibraryPath [ pkgs.stdenv.cc.cc.lib ]}";
+
+  languages.rust = {
+    enable = true;
+    channel = "stable";
+  };
+}
