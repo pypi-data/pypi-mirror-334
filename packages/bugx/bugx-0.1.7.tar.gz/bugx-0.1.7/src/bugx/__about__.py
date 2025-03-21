82ab7e0fea3a66c41ce7cd0960041a7beb6ec131
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 02:31:14 +0000

    Add package bugx/0.1.7/bugx-0.1.7.tar.gz

---
+++ packages/bugx/bugx-0.1.7.tar.gz/bugx-0.1.7/src/bugx/__about__.py
@@ -0,0 +1,63 @@
+"""About"""
+
+from typing import LiteralString
+
+from bugx import console
+
+__all__: list[str] = ["__version__", "author", "app_name", "description"]
+
+__version__ = "0.1.7"
+author: LiteralString = "Govardhan Ummadisetty"
+app_name: LiteralString = "bugx"
+description: LiteralString = """
+╔════════════════════════════════════════════════════════════════════════════╗
+║██████╗ ██╗   ██╗ ██████╗ ██╗  ██╗████████╗██████╗  █████╗  ██████╗████████╗║
+║██╔══██╗██║   ██║██╔════╝ ╚██╗██╔╝╚══██╔══╝██╔══██╗██╔══██╗██╔════╝╚══██╔══╝║
+║██████╔╝██║   ██║██║  ███╗ ╚███╔╝    ██║   ██████╔╝███████║██║        ██║   ║
+║██╔══██╗██║   ██║██║   ██║ ██╔██╗    ██║   ██╔══██╗██╔══██║██║        ██║   ║
+║██████╔╝╚██████╔╝╚██████╔╝██╔╝ ██╗   ██║   ██║  ██║██║  ██║╚██████╗   ██║   ║
+║╚═════╝  ╚═════╝  ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝   ╚═╝   ║
+╚════════════════════════════════════════════════════════════════════════════╝
+                 A simple CLI tool for Bug report Extraction"""
+
+
+description_color: LiteralString = """
+[bold blue]╔════════════════════════════════════════════════════════════════════════════╗
+║[bold green]██████╗ ██╗   ██╗ ██████╗ ██╗  ██╗████████╗██████╗  █████╗  ██████╗████████╗[/bold green]║
+║[bold green]██╔══██╗██║   ██║██╔════╝ ╚██╗██╔╝╚══██╔══╝██╔══██╗██╔══██╗██╔════╝╚══██╔══╝[/bold green]║
+║[bold yellow]██████╔╝██║   ██║██║  ███╗ ╚███╔╝    ██║   ██████╔╝███████║██║        ██║   [/bold yellow]║
+║[bold yellow]██╔══██╗██║   ██║██║   ██║ ██╔██╗    ██║   ██╔══██╗██╔══██║██║        ██║[/bold yellow]   ║
+║[bold red]██████╔╝╚██████╔╝╚██████╔╝██╔╝ ██╗   ██║   ██║  ██║██║  ██║╚██████╗   ██║[/bold red]   ║
+║[bold red]╚═════╝  ╚═════╝  ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝   ╚═╝   [/bold red]║
+╚════════════════════════════════════════════════════════════════════════════╝[/bold blue]
+[bold bright_white]        A simple CLI tool for Bug report Extraction[/bold bright_white]"""
+
+short_description: LiteralString = r"""
+   ___            _  ____               __ 
+  / _ )__ _____ _| |/_/ /________ _____/ /_
+ / _  / // / _ `_>  </ __/ __/ _ `/ __/ __/
+/____/\_,_/\_, /_/|_|\__/_/  \_,_/\__/\__/ 
+          /___/                            
+A simple CLI tool for Bug report Extraction
+"""
+
+short_description_color: LiteralString = r"""
+[bold italic red]   ___            _  ____               __ [/bold italic red]
+[bold italic yellow]  / _ )__ _____ _| |/_/ /________ _____/ /_ [/bold italic yellow]
+[bold italic green] / _  / // / _ `_>  </ __/ __/ _ `/ __/ __/ [/bold italic green]
+[bold italic blue]/____/\_,_/\_, /_/|_|\__/_/  \_,_/\__/\__/ [/bold italic blue]
+[bold italic magenta]          /___/                         [/bold italic magenta]   
+[bold white]A simple CLI tool for Bug report Extraction [/bold white]
+"""
+
+
+def version():
+    from bugx.cli_constants import CliApplication
+
+    console.print(f"{CliApplication.DESCRIPTION}\n", justify=CliApplication.JUSTIFY)
+    console.print(f":sparkles: {CliApplication.APP_NAME} version {CliApplication.VERSION}")
+    console.print(CliApplication.EPILOG)
+
+
+if __name__ == "__main__":
+    console.print(short_description_color)
