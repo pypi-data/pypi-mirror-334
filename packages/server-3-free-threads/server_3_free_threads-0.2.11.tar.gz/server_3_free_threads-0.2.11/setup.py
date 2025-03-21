d9017c59f68045cc0f18ba09e121ffac8c670d95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 01:32:56 +0000

    Add package server-3-free-threads/0.2.11/server_3_free_threads-0.2.11.tar.gz

---
+++ packages/server-3-free-threads/server_3_free_threads-0.2.11.tar.gz/server_3_free_threads-0.2.11/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open('README.md', 'r') as f:
+        return f.read()
+
+
+setup(
+    name='server_3_free_threads',
+    version='0.2.11',
+    author='Vadim Kazakov',
+    author_email='skolkovoinovations@gmail.com',
+    description='WSGI server based on threads',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    # url='your_url',
+    packages=find_packages(),
+    # install_requires=['python-dotenv==1.0.1',],
+    classifiers=[
+        'Programming Language :: Python :: 3.11',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+    ],
+    keywords=['server', 'WSGI'],
+    # project_urls={
+    #     'GitHub': 'your_github'
+    # },
+    python_requires='>=3.10'
+)
