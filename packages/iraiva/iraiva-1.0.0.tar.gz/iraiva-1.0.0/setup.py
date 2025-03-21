b01e2c917bf4205984360c3a98bf0624c3713ffa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 02:31:20 +0000

    Add package iraiva/1.0.0/iraiva-1.0.0.tar.gz

---
+++ packages/iraiva/iraiva-1.0.0.tar.gz/iraiva-1.0.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="iraiva",
+    version="1.0.0",
+    description="This package is reserved.",
+    long_description="This package is reserved.",
+    long_description_content_type="text/plain",
+    author="Praveen K",
+    author_email="iraiva.co@gmail.com",
+    packages=["iraiva"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires=">=3.6",
+)
