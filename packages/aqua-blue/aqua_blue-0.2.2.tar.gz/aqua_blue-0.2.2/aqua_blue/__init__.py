af5fc016661cac10ba96aa886d80e5a3efbb66fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 02:31:26 +0000

    Add package aqua-blue/0.2.2/aqua_blue-0.2.2.tar.gz

---
+++ packages/aqua-blue/aqua_blue-0.2.2.tar.gz/aqua_blue-0.2.2/aqua_blue/__init__.py
@@ -0,0 +1,23 @@
+__version__ = "0.2.2"
+__authors__ = [
+    "Jacob Jeffries",
+    "Hrishikesh Belagali",
+    "Avik Thumati",
+    "Ameen Mahmood",
+    "Samuel Josephs",
+]
+
+__author_emails__ = [
+    "jacob.jeffries@ccmc.global",
+    "ameen.mahmood@ccmc.global",
+    "avik.thumati@ccmc.global",
+    "samuel.josephs@ccmc.global",
+    "hrishikesh.belagali@ccmc.global"
+]
+__url__ = "https://github.com/Chicago-Club-Management-Company/aqua-blue"
+
+from . import utilities as utilities
+from . import reservoirs as reservoirs
+from . import readouts as readouts
+from . import models as models
+from . import time_series as time_series
