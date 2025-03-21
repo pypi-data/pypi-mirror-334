eb161c9f53c55281168bfe6cf4c4f501d7d269e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 02:31:19 +0000

    Add package python-woc/0.3.0/python_woc-0.3.0-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/python-woc/python_woc-0.3.0-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/woc/__init__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# SPDX-License-Identifier: GPL-3.0-or-later
+# @authors: Runzhi He <rzhe@pku.edu.cn>
+# @date: 2024-01-17
+
+"""
+# To Use
+.. include:: ../README.md
+   :start-line: 4
+   :end-before: ## Contributing
+# To Contribute
+.. include:: ../docs/contributing.md
+# World of Code Tutorial
+.. include:: ../docs/tutorial.md
+# World of Code DataFormat
+.. include:: ../docs/DataFormat.md
+
+"""  # noqa: D205
+
+__all__ = ["local", "tch", "detect", "objects"]
+
+import importlib.metadata
+
+__version__ = importlib.metadata.version("python-woc")
