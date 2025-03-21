ee2ff8e5c644c4ee304fe25222e70b7d62763b70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 04:05:47 +0000

    Add package SmartAITool/0.2.0/smartaitool-0.2.0-py3-none-any.whl

---
+++ packages/SmartAITool/smartaitool-0.2.0-py3-none-any.whl/tests/local_test.py
@@ -0,0 +1,17 @@
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
+
+from SmartAITool import core
+from SmartAITool.video import get_video_info, extract_frames, create_video
+
+core.cprint("Hello World", "green")
+core.cprint("Hello World", "blue")
+core.cprint("Hello World", "yellow")
+core.cprint("Hello World", "magenta")
+core.cprint("Hello World", "cyan")
+core.cprint("Hello World", "black")
+core.cprint("Hello World", "white")
+core.cprint("Hello World", "cyan")
+core.cprint("Hello World")
+
