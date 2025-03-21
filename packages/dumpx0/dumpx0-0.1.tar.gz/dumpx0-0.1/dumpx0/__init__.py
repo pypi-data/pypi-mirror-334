7751cdd01d4119e06147451f203e86e8aeb51eaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 01:32:46 +0000

    Add package dumpx0/0.1/dumpx0-0.1.tar.gz

---
+++ packages/dumpx0/dumpx0-0.1.tar.gz/dumpx0-0.1/dumpx0/__init__.py
@@ -0,0 +1,4 @@
+# __init__.py
+from .index import X3DH, Encryption, HMAC, SecureSocket
+
+__all__ = ["X3DH", "Encryption", "HMAC", "SecureSocket"]
---
+++ packages/dumpx0/dumpx0-0.1.tar.gz/dumpx0-0.1/setup.py
@@ -0,0 +1,16 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="dumpx0",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        'cryptography',
+        'pycryptodome',
+    ],
+    description="...",
+    author="ALfisene Keita",
+    author_email="info@infopeklo.cz",
+    url="https://github.com/certikpolik1/thecryptoxprotox",
+)
