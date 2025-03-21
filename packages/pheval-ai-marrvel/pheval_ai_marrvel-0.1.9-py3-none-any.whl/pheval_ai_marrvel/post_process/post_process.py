a63c3e928c2f5e5aceec18a32f25a1166f7539e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 02:31:44 +0000

    Add package pheval-ai-marrvel/0.1.9/pheval_ai_marrvel-0.1.9-py3-none-any.whl

---
+++ packages/pheval-ai-marrvel/pheval_ai_marrvel-0.1.9-py3-none-any.whl/pheval_ai_marrvel/post_process/post_process.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+
+import click
+
+from pheval_ai_marrvel.post_process.post_process_results_format import create_standardised_results
+
+
+def post_process_results(raw_results_dir: Path, output_dir: Path) -> None:
+    """
+    Post-process AI-MARRVEL raw results and create standardised PhEval TSV results.
+
+    Args:
+        raw_results_dir (Path): Path to the raw results directory.
+        output_dir (Path): Path to the output directory.
+    """
+    create_standardised_results(raw_results_dir, output_dir)
+
+
+@click.command()
+@click.option(
+    "--raw-results-dir",
+    "-r",
+    type=Path,
+)
+@click.option(
+    "--output-dir",
+    "-o",
+    type=Path,
+)
+def post_process(raw_results_dir: Path, output_dir: Path) -> None:
+    """
+    Post-process AI-MARRVEL raw results and create standardised PhEval TSV results.
+
+    Args:
+        raw_results_dir (Path): Path to the raw results directory.
+        output_dir (Path): Path to the output directory.
+    """
+    output_dir.joinpath("pheval_gene_results").mkdir(exist_ok=True)
+    output_dir.joinpath("pheval_variant_results").mkdir(exist_ok=True)
+    post_process_results(raw_results_dir, output_dir)
