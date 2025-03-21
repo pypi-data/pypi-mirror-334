04cfaa8f0ee18a69edc7837a82840dc45508781f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 00:07:06 +0000

    Add package pulp-container/2.16.11/pulp_container-2.16.11-py3-none-any.whl

---
+++ packages/pulp-container/pulp_container-2.16.11-py3-none-any.whl/pulp_container/app/__init__.py
@@ -0,0 +1,22 @@
+from pulpcore.plugin import PulpPluginAppConfig
+
+
+class PulpContainerPluginAppConfig(PulpPluginAppConfig):
+    """Entry point for the container plugin."""
+
+    name = "pulp_container.app"
+    label = "container"
+    version = "2.16.11"
+    python_package_name = "pulp-container"
+
+    def ready(self):
+        super().ready()
+        self.register_registry_types()
+
+    def register_registry_types(self):
+        # circular import avoidance
+        from pulp_container import constants
+        from django.conf import settings
+
+        for media_type, layer_types in settings.ADDITIONAL_OCI_ARTIFACT_TYPES.items():
+            constants.register_well_known_types(media_type, layer_types)
