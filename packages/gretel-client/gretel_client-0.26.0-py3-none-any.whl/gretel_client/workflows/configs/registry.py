25dc3a6ac89dfd6228c6b71a4fe3a66e52b1d2ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 03:16:06 +0000

    Add package gretel-client/0.26.0/gretel_client-0.26.0-py3-none-any.whl

---
+++ packages/gretel-client/gretel_client-0.26.0-py3-none-any.whl/gretel_client/workflows/configs/registry.py
@@ -0,0 +1,49 @@
+# AUTO-GENERATED FILE, DO NOT EDIT DIRECTLY
+
+from typing import Any
+
+from gretel_client.workflows.configs import tasks
+
+
+class RegistryMeta(type):
+    def __setattr__(cls, name: str, value: Any) -> None:
+        super().__setattr__(name, value)
+
+
+class Registry(metaclass=RegistryMeta):
+
+    ConcatDatasets = tasks.ConcatDatasets
+    ExtractDataSeedsFromSampleRecords = tasks.ExtractDataSeedsFromSampleRecords
+    IdGenerator = tasks.IdGenerator
+    LoadDataSeeds = tasks.LoadDataSeeds
+    DropColumns = tasks.DropColumns
+    NameGenerator = tasks.NameGenerator
+    GenerateDatasetFromSampleRecords = tasks.GenerateDatasetFromSampleRecords
+    SampleDataSeeds = tasks.SampleDataSeeds
+    GetGretelDataset = tasks.GetGretelDataset
+    Combiner = tasks.Combiner
+    DummyTaskWithInputs = tasks.DummyTaskWithInputs
+    DummyTaskWithListOfInputs = tasks.DummyTaskWithListOfInputs
+    TestFailingTask = tasks.TestFailingTask
+    TestOptionalArgTask = tasks.TestOptionalArgTask
+    TestRequiredAndOptionalArgsTask = tasks.TestRequiredAndOptionalArgsTask
+    TestTaskCallingTask = tasks.TestTaskCallingTask
+    EvaluateSsDataset = tasks.EvaluateSsDataset
+    Holdout = tasks.Holdout
+    GenerateColumnConfigFromInstruction = tasks.GenerateColumnConfigFromInstruction
+    SampleFromDataset = tasks.SampleFromDataset
+    JudgeWithLlm = tasks.JudgeWithLlm
+    GenerateColumnsUsingSamplers = tasks.GenerateColumnsUsingSamplers
+    ValidateCode = tasks.ValidateCode
+    SeedFromRecords = tasks.SeedFromRecords
+    EvaluateDataset = tasks.EvaluateDataset
+    GenerateColumnFromTemplate = tasks.GenerateColumnFromTemplate
+    GenerateSeedCategoryValues = tasks.GenerateSeedCategoryValues
+    TabularGan = tasks.TabularGan
+    TabularFt = tasks.TabularFt
+    Transform = tasks.Transform
+    GenerateFromTextFt = tasks.GenerateFromTextFt
+    TextFt = tasks.TextFt
+    TrainTextFt = tasks.TrainTextFt
+    PromptPretrainedModel = tasks.PromptPretrainedModel
+
