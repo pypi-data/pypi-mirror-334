4fbe1a57ac38211f0abeb9e074ea667aab70f1b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 02:31:05 +0000

    Add package djangoiransms/1.0.0/djangoiransms-1.0.0.tar.gz

---
+++ packages/djangoiransms/djangoiransms-1.0.0.tar.gz/djangoiransms-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="djangoiransms",
+    version="1.0.0",
+    install_requires=["django-iran-sms"],
+    description="Alias package for django-iran-sms",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
