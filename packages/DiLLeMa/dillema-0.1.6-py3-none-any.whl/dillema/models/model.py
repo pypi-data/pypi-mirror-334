b5b009424069e03561a9df1a1566d86e2fec6640
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 02:31:23 +0000

    Add package DiLLeMa/0.1.6/dillema-0.1.6-py3-none-any.whl

---
+++ packages/DiLLeMa/dillema-0.1.6-py3-none-any.whl/dillema/models/model.py
@@ -0,0 +1,8 @@
+"""MODELS FOR DILLEMA APP"""
+
+MODELS = {
+    "llama-3.1-8B-Instruct": {
+        "model": "llama-3.1-8B-Instruct",
+        "model_path": "dillema/model/llama-3.1-8B-Instruct",
+    }
+}
