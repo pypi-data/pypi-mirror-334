e1419555eba97db395fb9c89924c45e1684c2ffc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 02:31:18 +0000

    Add package usdm4/0.3.1/usdm4-0.3.1-py3-none-any.whl

---
+++ packages/usdm4/usdm4-0.3.1-py3-none-any.whl/usdm4/api/masking.py
@@ -0,0 +1,6 @@
+from .api_base_model import ApiBaseModelWithIdAndDesc
+from typing import Literal
+
+
+class Masking(ApiBaseModelWithIdAndDesc):
+    instanceType: Literal["Masking"]
---
+++ packages/usdm4/usdm4-0.3.1-py3-none-any.whl/usdm4/api/study_intervention.py
@@ -0,0 +1,17 @@
+from typing import List, Literal, Union
+from .api_base_model import ApiBaseModelWithIdNameLabelAndDesc
+from .quantity import Quantity
+from .administration import Administration
+from .code import Code
+from .comment_annotation import CommentAnnotation
+
+
+class StudyIntervention(ApiBaseModelWithIdNameLabelAndDesc):
+    role: Code
+    type: Code
+    minimumResponseDuration: Union[Quantity, None] = None
+    codes: List[Code] = []
+    administrations: List[Administration] = []
+    productDesignation: Code
+    notes: List[CommentAnnotation] = []
+    instanceType: Literal["StudyIntervention"]
