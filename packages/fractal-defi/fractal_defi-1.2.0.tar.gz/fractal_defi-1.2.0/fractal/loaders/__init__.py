ea81168f7db2a848b591c19059152450f4efc31f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 01:33:49 +0000

    Add package fractal-defi/1.2.0/fractal_defi-1.2.0.tar.gz

---
+++ packages/fractal-defi/fractal_defi-1.2.0.tar.gz/fractal_defi-1.2.0/fractal/loaders/__init__.py
@@ -0,0 +1,56 @@
+from fractal.loaders.aave import AaveV2EthereumLoader, AaveV3ArbitrumLoader
+from fractal.loaders.base_loader import Loader, LoaderType
+from fractal.loaders.binance import (BinanceDayPriceLoader,
+                                     BinanceFundingLoader,
+                                     BinanceHourPriceLoader)
+from fractal.loaders.gmx_v1 import GMXV1FundingLoader
+from fractal.loaders.hyperliquid import (HyperliquidFundingRatesLoader,
+                                         HyperLiquidPerpsPricesLoader)
+from fractal.loaders.simulations import (ConstantFundingsLoader,
+                                         LPMLSimulatedStatesLoader,
+                                         LPSimulatedStates,
+                                         MonteCarloHourPriceLoader)
+from fractal.loaders.structs import (FundingHistory, LendingHistory,
+                                     PoolHistory, PriceHistory, RateHistory)
+from fractal.loaders.thegraph import (ArbitrumGraphLoader, BaseGraphLoader,
+                                      EthereumUniswapV2PoolDataLoader,
+                                      GraphLoaderException, StETHLoader,
+                                      UniswapV3ArbitrumPoolDayDataLoader,
+                                      UniswapV3ArbitrumPoolHourDataLoader,
+                                      UniswapV3ArbitrumPricesLoader,
+                                      UniswapV3EthereumPoolDayDataLoader,
+                                      UniswapV3EthereumPoolHourDataLoader)
+
+__all__ = [
+    "Loader",
+    "LoaderType",
+    "FundingHistory",
+    "PoolHistory",
+    "PriceHistory",
+    "RateHistory",
+    "AaveV2EthereumLoader",
+    "AaveV3ArbitrumLoader",
+    "BinanceDayPriceLoader",
+    "BinanceFundingLoader",
+    "BinanceHourPriceLoader",
+    "GMXV1FundingLoader",
+    "MonteCarloHourPriceLoader",
+    "UniswapV3ArbitrumPoolDayDataLoader",
+    "UniswapV3ArbitrumPoolHourDataLoader",
+    "UniswapV3EthereumPoolDayDataLoader",
+    "UniswapV3EthereumPoolHourDataLoader",
+    "StETHLoader",
+    "ConstantFundingsLoader",
+    "LPMLSimulatedStatesLoader",
+    "LPSimulatedStates",
+    "EthereumUniswapV2PoolDataLoader",
+    "BaseGraphLoader",
+    "GraphLoaderException",
+    "ArbitrumGraphLoader",
+    "LendingHistory",
+    "UniswapV3ArbitrumPricesLoader"
+    "HyperliquidFundingRatesLoader",
+    "HyperLiquidPerpsPricesLoader",
+    "HyperliquidFundingRatesLoader",
+    "UniswapV3ArbitrumPricesLoader",
+]
