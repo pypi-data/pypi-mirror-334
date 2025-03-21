2247f0b9e637fdd63f3fc085bda4e4bb5b9fc53c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 01:33:16 +0000

    Add package scaleway-core/2.9.0/scaleway_core-2.9.0-py3-none-any.whl

---
+++ packages/scaleway-core/scaleway_core-2.9.0-py3-none-any.whl/scaleway_core/bridge/__init__.py
@@ -0,0 +1,50 @@
+from .money import Money
+from .money import unmarshal_Money
+from .money import marshal_Money
+
+from .region import Region
+from .region import ALL_REGIONS
+from .zone import Zone
+from .zone import ALL_ZONES
+
+from .scwfile import ScwFile
+from .scwfile import unmarshal_ScwFile
+from .scwfile import marshal_ScwFile
+
+from .serviceinfo import ServiceInfo
+from .serviceinfo import unmarshal_ServiceInfo
+from .serviceinfo import marshal_ServiceInfo
+
+from .timeseries import TimeSeriesPoint
+from .timeseries import TimeSeries
+from .timeseries import unmarshal_TimeSeries
+from .timeseries import marshal_TimeSeries
+from .timeseries import unmarshal_TimeSeriesPoint
+from .timeseries import marshal_TimeSeriesPoint
+
+from .decimal import unmarshal_Decimal
+from .decimal import marshal_Decimal
+
+__all__ = [
+    "Money",
+    "unmarshal_Money",
+    "marshal_Money",
+    "Region",
+    "ALL_REGIONS",
+    "Zone",
+    "ALL_ZONES",
+    "ScwFile",
+    "unmarshal_ScwFile",
+    "marshal_ScwFile",
+    "ServiceInfo",
+    "unmarshal_ServiceInfo",
+    "marshal_ServiceInfo",
+    "TimeSeriesPoint",
+    "TimeSeries",
+    "unmarshal_TimeSeries",
+    "marshal_TimeSeries",
+    "unmarshal_TimeSeriesPoint",
+    "marshal_TimeSeriesPoint",
+    "unmarshal_Decimal",
+    "marshal_Decimal",
+]
