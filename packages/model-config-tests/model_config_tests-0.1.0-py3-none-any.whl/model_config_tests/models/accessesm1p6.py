9da6fad4934e53176e5aa5e1c1aba6a1f5c900fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 05:05:44 +0000

    Add package model-config-tests/0.1.0/model_config_tests-0.1.0-py3-none-any.whl

---
+++ packages/model-config-tests/model_config_tests-0.1.0-py3-none-any.whl/model_config_tests/models/accessesm1p6.py
@@ -0,0 +1,11 @@
+"""Specific ACCESS-ESM1.6 Model setup and post-processing"""
+
+from model_config_tests.models.accessesm1p5 import AccessEsm1p5
+
+
+class AccessEsm1p6(AccessEsm1p5):
+    def __init__(self, experiment):
+        super().__init__(experiment)
+
+        self.output_filename = "access-esm1.6.out"
+        self.output_file = self.output_0 / self.output_filename
