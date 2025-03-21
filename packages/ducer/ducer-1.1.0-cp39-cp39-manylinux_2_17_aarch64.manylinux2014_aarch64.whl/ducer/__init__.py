cffd757e6731e6d0844b4fcb594305da776e6952
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 04:06:00 +0000

    Add package ducer/1.1.0/ducer-1.1.0-cp39-cp39-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/ducer/ducer-1.1.0-cp39-cp39-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/ducer/__init__.py
@@ -0,0 +1,17 @@
+from importlib import metadata
+
+from ._fst import *
+
+__all__ = (
+    "Automaton",
+    "Buffer",
+    "Map",
+    "Op",
+    "Set",
+)
+
+try:
+    __version__ = metadata.version("ducer")
+except metadata.PackageNotFoundError:
+    __version__ = "1.1.0"
+del metadata
