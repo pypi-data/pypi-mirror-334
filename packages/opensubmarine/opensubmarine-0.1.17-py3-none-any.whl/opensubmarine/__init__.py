cdbd25cf157141fb61104c0fc9be11d68ea308fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 00:07:12 +0000

    Add package opensubmarine/0.1.17/opensubmarine-0.1.17-py3-none-any.whl

---
+++ packages/opensubmarine/opensubmarine-0.1.17-py3-none-any.whl/opensubmarine/__init__.py
@@ -0,0 +1,28 @@
+from opensubmarine.contracts.access.Ownable.contract import Ownable, OwnableInterface
+from opensubmarine.contracts.participation.Stakable.contract import Stakeable
+from opensubmarine.contracts.update.Upgradeable.contract import Upgradeable
+from opensubmarine.contracts.factory.Deployable.contract import Deployable
+from opensubmarine.contracts.factory.Factory.contract import BaseFactory, FactoryCreated
+from opensubmarine.contracts.token.ARC200.src.contract import (
+    ARC200Token,
+    ARC200TokenInterface,
+    arc200_Transfer,
+)
+
+
+__version__ = "0.1.17"
+
+__all__ = [
+    "Ownable",
+    "OwnableInterface",
+    "Stakeable",
+    "Upgradeable",
+    "Deployable",
+    "BaseFactory",
+    "FactoryCreated",
+    "ARC200Token",
+    "ARC200TokenInterface",
+    "arc200_Transfer",
+]
+
+OpenSubmarine_version = __version__
