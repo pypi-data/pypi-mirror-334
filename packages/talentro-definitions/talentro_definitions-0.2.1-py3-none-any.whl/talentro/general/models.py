9273ef2a66df4f214d9efe496b11923cad7b8f67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-03-21 01:33:14 +0000

    Add package talentro-definitions/0.2.1/talentro_definitions-0.2.1-py3-none-any.whl

---
+++ packages/talentro-definitions/talentro_definitions-0.2.1-py3-none-any.whl/talentro/general/models.py
@@ -0,0 +1,14 @@
+import uuid
+from datetime import datetime
+
+from sqlmodel import SQLModel, Field
+
+
+class BaseModel(SQLModel):
+    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    created_at: datetime = Field(default=datetime.now())
+    updated_at: datetime = Field(nullable=True)
+
+
+class ClintBaseModel(BaseModel):
+    company_id: str = Field(index=True)
